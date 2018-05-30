cab8ff9949f0e1b928ff3ce0a352541815fcc0b7
Zach Latta <zach@zachlatta.com>
2018-05-29 20:48:39 -0700

    Add root page & convenient link to it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
+  get '/', to: 'static_pages#index'
+
   resources :events
   resources :bank_accounts, only: [ :new, :create, :show ]
   resources :transactions, only: [ :show, :edit, :update ]
