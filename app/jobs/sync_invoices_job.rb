2b53a704f965f28a26b907f974926e2b114420eb
Zach Latta <zach@zachlatta.com>
2018-05-30 13:50:37 -0700

    Sync invoices w/ Stripe

---
+++ app/jobs/sync_invoices_job.rb
@@ -0,0 +1,19 @@
+class SyncInvoicesJob < ApplicationJob
+  RUN_EVERY = 1.hour
+
+  def perform(repeat = false)
+    ActiveRecord::Base.transaction do
+      Invoice.find_each do |i|
+        inv = StripeService::Invoice.retrieve(i.stripe_invoice_id)
+
+        i.set_fields_from_stripe_invoice(inv)
+
+        i.save!
+      end
+    end
+
+    if repeat
+      self.class.set(wait: RUN_EVERY).perform_later
+    end
+  end
+end
