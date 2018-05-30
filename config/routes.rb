ee43248ad71e38ead16281da250e38371c4d0995
Zach Latta <zach@zachlatta.com>
2018-05-29 17:23:53 -0700

    Set up Sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :events
   resources :bank_accounts, only: [ :new, :create, :show ]
   resources :transactions, only: [ :show, :edit, :update ]
