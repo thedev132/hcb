c79e4883be0c46307571c7704a00e8a628f7741a
Zach Latta <zach@zachlatta.com>
2018-06-17 17:53:52 -0700

    Add HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def self.auth
+    if Rails.env.production?
+      Rails.application.credentials.auth[:live]
+    else
+      Rails.application.credentials.auth[:test]
+    end
+  end
+
+  http_basic_authenticate_with name: auth[:username], password: auth[:password]
 end
