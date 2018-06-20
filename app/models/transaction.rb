745d53bfdc67e7e91edfd17fde606ec45cb1f2e1
Zach Latta <zach@zachlatta.com>
2018-06-20 15:21:48 -0700

    Improve validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,11 +6,14 @@ class Transaction < ApplicationRecord
   belongs_to :bank_account
 
   belongs_to :fee_relationship, inverse_of: :t_transaction, required: false
-  has_one :event, through: :fee_relationship
+  has_one :event, through: :fee_relationship, required: false
 
   accepts_nested_attributes_for :fee_relationship
 
-  validates :fee_relationship,
+  validates :fee_relationship, :event,
+    presence: true,
+    if: -> { self.is_event_related == true }
+  validates :fee_relationship, :event
     absence: true,
     if: -> { self.is_event_related == false }
 
