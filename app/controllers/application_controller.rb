781961e91300275755ff84f6786a1ef0e420a58a
Zach Latta <zach@zachlatta.com>
2018-06-22 02:46:01 -0700

    Remove HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   include SessionsHelper
 
-  def self.auth
-    if Rails.env.production?
-      Rails.application.credentials.auth[:live]
-    else
-      Rails.application.credentials.auth[:test]
-    end
-  end
-
   protect_from_forgery
-
-  http_basic_authenticate_with name: auth[:username], password: auth[:password]
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
