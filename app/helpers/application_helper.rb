8f88c33b61128a4916e4db6ab4bae63d484c9bbc
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-25 10:17:50 -0700

    Blankslate view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,10 @@ module ApplicationHelper
 
   def render_percentage(decimal, params={})
     precision = params[:precision] || 2
-
     number_to_percentage(decimal * 100, precision: precision)
+  end
+
+  def blankslate(text, options={})
+    content_tag :p, text, class: "center my0 slate bold h3 #{options.class}"
   end
 end
