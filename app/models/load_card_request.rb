1667d4451107102374c2108f0a70658c2d12b397
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-25 10:21:29 -0700

    Add outstanding scope to LoadCardRequest

--- app/models/load_card_request.rb
+++ app/models/load_card_request.rb
@@ -3,6 +3,8 @@ class LoadCardRequest < ApplicationRecord
   belongs_to :fulfilled_by, class_name: 'User', required: false
   belongs_to :creator, class_name: 'User'
 
+  scope :outstanding, -> { where.not(fulfilled_by_id: nil) }
+
   def status
     return 'rejected' if rejected_at.present?
     return 'canceled' if canceled_at.present?
