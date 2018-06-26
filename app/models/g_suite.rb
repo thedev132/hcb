ad6f6d96afd2dc36b675212c08e790d8e604d20e
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-26 01:29:54 -0700

    verified?

--- app/models/g_suite.rb
+++ app/models/g_suite.rb
@@ -1,7 +1,12 @@
 class GSuite < ApplicationRecord
   has_one :g_suite_application
+  has_many :g_suite_accounts
   belongs_to :event
 
   validates_presence_of :domain, :dns_verification_key
   validates :domain, format: { with: URI.regexp }, if: 'domain.present?'
+
+  def verified?
+    # one of the accounts is verified
+  end
 end
--- app/models/g_suite_account.rb
+++ app/models/g_suite_account.rb
@@ -2,4 +2,8 @@ class GSuiteAccount < ApplicationRecord
   belongs_to :g_suite
 
   validates :address, presence: true
+
+  def verified?
+    verified_at.present?
+  end
 end
