0b6e1b6d94fe90f1155a8526d896e647b3bb1ee8
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-25 22:27:28 -0700

    Fix showing homepage blankslate with invites

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    if signed_in?
+      @events = current_user.events
+      @invites = current_user.organizer_position_invites.pending
+    end
   end
 end
