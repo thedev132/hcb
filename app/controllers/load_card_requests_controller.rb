f2f5c0f40ca68e2bc59781529dcee1fd8b9ea54e
Max Wofford <max@maxwofford.com>
2018-06-25 17:57:36 -0700

    Add Pundit policies for load card requests

--- app/controllers/load_card_requests_controller.rb
+++ app/controllers/load_card_requests_controller.rb
@@ -3,28 +3,38 @@ class LoadCardRequestsController < ApplicationController
 
   def index
     @load_card_requests = LoadCardRequest.all
+    authorize @load_card_requests
   end
 
   def show
     @card = @load_card_request.card
+    authorize @load_card_requests
   end
 
   def accept
     @load_card_request.fulfiled_at = Time.now
     @load_card_request.fulfiled_by = current_user
+
+    authorize @load_card_requests
+
     @load_card_request.save
   end
 
   def new
     @card = Card.includes(:event).find(params[:card_id])
     @load_card_request = LoadCardRequest.new
+
+    authorize @load_card_request
   end
 
   def edit
+    authorize @load_card_requests
   end
 
   def create
     @load_card_request = LoadCardRequest.new(load_card_request_params)
+
+    authorize @load_card_requests
 
     if @load_card_request.save
       redirect_to @load_card_request, notice: 'Load card request was successfully created.'
@@ -34,6 +44,8 @@ class LoadCardRequestsController < ApplicationController
   end
 
   def update
+    authorize @load_card_requests
+
     if @load_card_request.update(load_card_request_params)
       flash[:success] = 'Load card request was successfully updated.'
       redirect_to load_card_requests
@@ -43,7 +55,9 @@ class LoadCardRequestsController < ApplicationController
   end
 
   def destroy
-    @load_card_request.destroy
+    authorize @load_card_request
+
+    @load_card_request.cancelled_at = Time.now
     flash[:success] = 'Load card request cancelled.'
     redirect_to @load_card_request.event
   end
---
+++ app/policies/load_card_request_policy.rb
@@ -0,0 +1,37 @@
+class CardRequestPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def new?
+    record.user == user || user.admin?
+  end
+
+  def create?
+    record.user == user || user.admin?
+  end
+
+  def show?
+    user.admin?
+  end
+
+  def edit?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    record.user == user || user.admin?
+  end
+
+  def accept?
+    user.admin?
+  end
+
+  def reject?
+    user.admin?
+  end
+end
