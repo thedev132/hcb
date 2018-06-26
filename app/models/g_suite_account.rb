28f6c644674885bd7e2177062e4cd6ad13fe7367
Max Wofford <max@maxwofford.com>
2018-06-26 04:23:14 -0700

    Ensure uniqueness of G Suite email addresses

--- app/models/g_suite_account.rb
+++ app/models/g_suite_account.rb
@@ -3,7 +3,17 @@ class GSuiteAccount < ApplicationRecord
 
   validates_presence_of :address
 
+  validate uniqueness_of_address_in_domain
+
   def verified?
     verified_at.present?
+  end
+
+  private
+
+  def uniqueness_of_address_in_domain
+    g_suite.g_suite_accounts.where(address: address).each do |account|
+      errors.add(:unique_error, 'Address already in use') if id != account.id
+    end
   end
 end
