a02b07837f7345afa38090c67980d7f0b74f4fdd
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-25 17:18:17 -0700

    Formalize status indicators on card requests

--- app/models/card_request.rb
+++ app/models/card_request.rb
@@ -9,12 +9,17 @@ class CardRequest < ApplicationRecord
   validate :single_status
 
   scope :outstanding, -> { where(accepted_at: nil) }
+  scope :under_review, -> { where(rejected_at: nil, canceled_at: nil, accepted_at: nil) }
 
   def status
     return 'rejected' if rejected_at.present?
     return 'canceled' if canceled_at.present?
     return 'accepted' if accepted_at.present?
     'under review'
+  end
+
+  def under_review?
+    rejected_at.nil? && canceled_at.nil? && accepted_at.nil?
   end
 
   def send_accept_email
