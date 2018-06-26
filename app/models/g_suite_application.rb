284dd39d04abdb5709dd3cbc6df0c4b78584db5c
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-25 23:46:40 -0700

    Add scopes to applications

--- app/models/g_suite_application.rb
+++ app/models/g_suite_application.rb
@@ -4,4 +4,22 @@ class GSuiteApplication < ApplicationRecord
   belongs_to :fulfilled_by, class_name: 'User'
 
   validates :creator, :event, :domain, :fulfilled_by, presence: true
+
+  scope :outstanding, -> { where(accepted_at: nil) }
+  scope :under_review, -> { where(rejected_at: nil, canceled_at: nil, accepted_at: nil) }
+
+  def status
+    return 'rejected' if rejected_at.present?
+    return 'canceled' if canceled_at.present?
+    return 'accepted' if accepted_at.present?
+    'under review'
+  end
+
+  def under_review?
+    rejected_at.nil? && canceled_at.nil? && accepted_at.nil?
+  end
+
+  def rejected?
+    rejected_at.present?
+  end
 end
