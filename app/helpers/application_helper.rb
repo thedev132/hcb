5e5af4845a338042c075ac3e83d5a05fe4795ceb
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-26 23:59:34 -0700

    Patch render_money for 0

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def render_money(amount, unit = '$')
-    number_to_currency(BigDecimal.new(amount) / 100, unit: unit)
+    number_to_currency(BigDecimal.new(amount || 0) / 100, unit: unit)
   end
 
   def render_percentage(decimal, params={})
