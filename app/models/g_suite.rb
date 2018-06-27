83c47d0f0cf949151a36967c85d5744b486121e7
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-06-27 00:23:42 -0700

    Fix GSuite validations

--- app/models/g_suite.rb
+++ app/models/g_suite.rb
@@ -3,7 +3,7 @@ class GSuite < ApplicationRecord
   has_many :g_suite_accounts
   belongs_to :event
 
-  validates_presence_of :domain, :dns_verification_key
+  validates_presence_of :domain, :verification_key
   validates_uniqueness_of :domain
   validate :domain_without_protocol
 
@@ -13,8 +13,8 @@ class GSuite < ApplicationRecord
 
   private
 
-  def domain_without_protocol(domain)
-    uri = URI.parse(domain)
-    uri.scheme.nil?
+  def domain_without_protocol
+    bad = ['http', ':', '/'].any? { |s| domain.include? s }
+    errors.add(:domain, 'shouldn’t include http(s):// or ending /') if bad
   end
 end
