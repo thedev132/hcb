21fa589d823ea1edab9be540acc8dc1eb16cef00
Zach Latta <zach@zachlatta.com>
2018-07-14 15:44:42 +0900

    Ensure files are attached to documents. Closes #7.

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,5 +4,15 @@ class Document < ApplicationRecord
 
   has_one_attached :file
 
-  validates_presence_of :event, :user, :file
+  validates_presence_of :event, :user
+  validate :ensure_file_attached
+
+  private
+
+  # ActiveStorage doesn't yet support attachment validation (how dumb... see
+  # https://github.com/rails/rails/issues/31656). This manually checks if a
+  # file is attached for the time being.
+  def ensure_file_attached
+    errors.add(:file, 'must be attached') unless file.attached?
+  end
 end
