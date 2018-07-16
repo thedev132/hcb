ffb9bda59e782d0a6d88673e90c0cdc7c337dd89
Zach Latta <zach@zachlatta.com>
2018-07-16 13:46:23 +0900

    Add comment w/ background on CreateInvoicePayoutJob

--- app/jobs/create_invoice_payout_job.rb
+++ app/jobs/create_invoice_payout_job.rb
@@ -1,3 +1,6 @@
+# CreateInvoicePayoutJob is used to queue creation of InvoicePayouts after
+# balances become available in Stripe for withdrawal. Please see
+# Invoice#queue_payout!.
 class CreateInvoicePayoutJob < ApplicationJob
   def perform(invoice)
     invoice.create_payout!
