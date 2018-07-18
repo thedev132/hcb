248bb47203365e8d1299e27c38057c8e1464900d
Max Wofford <max@maxwofford.com>
2018-07-18 15:05:40 -0700

    Transactions can also be linked to an event through their associated card

--- app/jobs/sync_emburse_transactions_job.rb
+++ app/jobs/sync_emburse_transactions_job.rb
@@ -7,8 +7,14 @@ class SyncEmburseTransactionsJob < ApplicationJob
       et = EmburseTransaction.find_by(emburse_id: trn[:id])
       et ||= EmburseTransaction.new(emburse_id: trn[:id])
 
-      amount = trn[:amount] * 100
       department_id = trn[:department] && trn[:department][:id]
+      # If the transaction isn't assigned to a department directly, we'll use the card's department
+      if department_id.nil? && trn[:card]
+        card = EmburseClient::Card.get(trn[:card][:id])
+        department_id = card[:department][:id]
+      end
+
+      amount = trn[:amount] * 100
       related_event = Event.find_by(emburse_department_id: department_id)
 
       et.update!(
