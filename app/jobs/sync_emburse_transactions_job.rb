268896819ca8e655a31e3f88b95bf43e2807521a
Max Wofford <max@maxwofford.com>
2018-07-18 15:21:36 -0700

    SyncEmburseTransactionsJob queues itself

--- app/jobs/sync_emburse_transactions_job.rb
+++ app/jobs/sync_emburse_transactions_job.rb
@@ -1,28 +1,31 @@
 class SyncEmburseTransactionsJob < ApplicationJob
   RUN_EVERY = 10.minutes
 
-  def perform(*args)
-    transactions = EmburseClient::Transaction.list
-    transactions.each do |trn|
-      et = EmburseTransaction.find_by(emburse_id: trn[:id])
-      et ||= EmburseTransaction.new(emburse_id: trn[:id])
+  def perform(repeat = false)
+    ActiveRecord::Base.transaction do
+      EmburseClient::Transaction.list.each do |trn|
+        et = EmburseTransaction.find_by(emburse_id: trn[:id])
+        et ||= EmburseTransaction.new(emburse_id: trn[:id])
 
-      department_id = trn[:department] && trn[:department][:id]
-      # If the transaction isn't assigned to a department directly, we'll use the card's department
-      if department_id.nil? && trn[:card]
-        card = EmburseClient::Card.get(trn[:card][:id])
-        department_id = card[:department][:id]
+        department_id = trn[:department] && trn[:department][:id]
+        # If the transaction isn't assigned to a department directly, we'll use the card's department
+        if department_id.nil? && trn[:card]
+          card = EmburseClient::Card.get(trn[:card][:id])
+          department_id = card[:department][:id]
+        end
+
+        amount = trn[:amount] * 100
+        related_event = Event.find_by(emburse_department_id: department_id)
+
+        et.update!(
+          amount: amount,
+          state: trn[:state],
+          emburse_department_id: department_id,
+          event: related_event
+        )
       end
-
-      amount = trn[:amount] * 100
-      related_event = Event.find_by(emburse_department_id: department_id)
-
-      et.update!(
-        amount: amount,
-        state: trn[:state],
-        emburse_department_id: department_id,
-        event: related_event
-      )
     end
+
+    self.class.set(wait: RUN_EVERY).perform_later(true) if repeat
   end
 end
