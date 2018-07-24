a5ca116c6ab94b540a602e4974f3dab2e35eda59
Zach Latta <zach@zachlatta.com>
2018-07-23 19:32:29 -0700

    Fix syntax error

--- app/jobs/sync_invoices_job.rb
+++ app/jobs/sync_invoices_job.rb
@@ -13,15 +13,17 @@ class SyncInvoicesJob < ApplicationJob
         now_paid = i.paid
 
         if !was_paid && now_paid
-          logger.debug("Queueing payout for invoice: #{i.attributes.inspect}}")
-          i.queue_payout!
+          begin
+            logger.debug("Queueing payout for invoice: #{i.attributes.inspect}}")
+            i.queue_payout!
 
-        # This happens in the case where the invoice was for such a low amount,
-        # like $0.10, that Stripe didn't bother creating a charge and instead
-        # credited a balance to the invoice. See in_1CppXuFSaumjmb9rrEUviPYy
-        # in live mode for an example.
-        rescue Invoice::NoAssociatedStripeCharge
-          logger.debug("Payout aborted due to no associated Stripe charge existing")
+          # This happens in the case where the invoice was for such a low amount,
+          # like $0.10, that Stripe didn't bother creating a charge and instead
+          # credited a balance to the invoice. See in_1CppXuFSaumjmb9rrEUviPYy
+          # in live mode for an example.
+          rescue Invoice::NoAssociatedStripeCharge
+            logger.debug("Payout aborted due to no associated Stripe charge existing")
+          end
         end
       end
     end
