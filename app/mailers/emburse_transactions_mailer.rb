757218207636644a9998ebf5842b8bf9531dbad3
Max Wofford <max@maxwofford.com>
2018-07-26 02:33:00 -0700

    Don't thread emburse transaction emails in gmail

--- app/mailers/emburse_transactions_mailer.rb
+++ app/mailers/emburse_transactions_mailer.rb
@@ -1,9 +1,9 @@
 class EmburseTransactionsMailer < ApplicationMailer
   def notify(params)
     @recipient = 'team@hackclub.com'
-    @emburse_transaction_path = params[:emburse_transaction].emburse_path
+    @emburse_transaction = params[:emburse_transaction]
 
     mail to: @recipient,
-      subject: "[Action Requested] Triage unassociated Emburse transaction"
+      subject: "[Action Requested] Triage unassociated Emburse transaction ##{@emburse_transaction.id}"
   end
 end
