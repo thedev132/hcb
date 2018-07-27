8c76b4c04198214de6f8e1656e67763dbb387f95
Zach Latta <zach@zachlatta.com>
2018-07-27 00:26:55 -0700

    Fix transaction mailer syntax error

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -1,7 +1,7 @@
 class TransactionMailer < ApplicationMailer
   def notify_admin
     @transaction = params[:transaction]
-    env = Rails.environment.production? ? :prod : :dev
+    env = Rails.env.production? ? :prod : :dev
 
     mail to: Rails.application.credentials.admin_email[env],
       subject: "[Bank] New Transaction: #{@transaction.name}"
