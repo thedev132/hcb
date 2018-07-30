8a58a6187fd3cfdff627e303d31630e7d50bb3e2
Zach Latta <zach@zachlatta.com>
2018-07-30 11:12:03 -0700

    Use Util module's unixtime parser in Invoice#queue_payout!

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -80,12 +80,7 @@ class Invoice < ApplicationRecord
 
     b_tnx = inv.charge.balance_transaction
 
-    funds_available_at_unixtime = b_tnx.available_on
-
-    # really dumb... but this seems to be the best way to convert unixtime to
-    # datetime w/o timezone issues. see https://stackoverflow.com/a/7819254.
-    funds_available_at = DateTime.strptime(funds_available_at_unixtime.to_s, '%s')
-
+    funds_available_at = Util.unixtime(b_tnx.available_on)
     create_payout_at = funds_available_at + 1.day
 
     job = CreateInvoicePayoutJob.set(wait_until: create_payout_at).perform_later(self)
