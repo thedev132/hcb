4b7704d6b689d9d21fc4d6cd37e6132fe05b1972
Max Wofford <max@maxwofford.com>
2018-07-30 19:58:35 -0700

    CORS

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,24 @@ module Bank
 
     config.autoload_paths << "#{config.root}/lib"
     config.eager_load_paths << "#{config.root}/lib"
+
+    # Allow whitelist of origins through CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      DOMAINS = %w{https://hackclub.com localhost}
+
+      allow do
+        origins do |source, _env|
+          DOMAINS.each do |domain|
+            parsed = URI.parse(source)
+            domain == source || domain == parsed.host
+          end
+        end
+
+        resource '*',
+                 headers: :any,
+                 methods: %i[get post put patch delete options head]
+      end
+
+    end
   end
 end
