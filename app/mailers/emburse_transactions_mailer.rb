4604eba64386781a415bdd4187a9f8da4a8822b2
Max Wofford <max@maxwofford.com>
2018-08-09 05:55:43 -0700

    Fix env always being set to dev in EmburseTransactionMailer

--- app/mailers/emburse_transactions_mailer.rb
+++ app/mailers/emburse_transactions_mailer.rb
@@ -1,7 +1,7 @@
 class EmburseTransactionsMailer < ApplicationMailer
   def notify(params)
     @emburse_transaction = params[:emburse_transaction]
-    env = Rails.environment.production? ? :prod : :dev
+    env = Rails.env.production? ? :prod : :dev
 
     mail to: Rails.application.credentials.admin_email[env],
       subject: "[Action Requested] No department linked to Emburse transaction ##{@emburse_transaction.id}"
