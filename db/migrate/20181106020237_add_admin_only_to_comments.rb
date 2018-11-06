79747de48d34b7ad1647f749ae0a8fe69c62c83b
Max Wofford <max@maxwofford.com>
2018-11-05 18:26:16 -0800

    Add admin-only comments

---
+++ db/migrate/20181106020237_add_admin_only_to_comments.rb
@@ -0,0 +1,5 @@
+class AddAdminOnlyToComments < ActiveRecord::Migration[5.2]
+  def change
+    add_column :comments, :admin_only, :boolean, null: false, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_012708) do
+ActiveRecord::Schema.define(version: 2018_11_06_020237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2018_10_22_012708) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin_only", default: false, null: false
     t.index ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
