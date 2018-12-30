db1ae9f3179dd5ce0bde5bf30e93ff8cb8b250ab
Lachlan Campbell <lachlan@hackclub.com>
2018-12-30 17:16:28 -0500

    Add automatic rounding + Retina to avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,17 +2,17 @@ require 'digest/md5'
 require 'uri'
 
 module UsersHelper
-  def gravatar_url(email, name, size = 64)
+  def gravatar_url(email, name, size)
     hex = Digest::MD5.hexdigest(email.downcase.strip)
     "https://gravatar.com/avatar/#{hex}?s=#{size}&d=https%3A%2F%2Fui-avatars.com%2Fapi%2F/#{URI.encode(name)}/#{size}/e42d42/fff"
   end
 
-  def gravatar_for(user, size, options = {})
-    image_tag gravatar_url(user.email, user.name, size), options
+  def avatar_for(user, size = 32, options = {})
+    image_tag gravatar_url(user.email, user.name, size * 2), options.merge({ alt: user.name, width: size, class: "circle #{options[:class]}" })
   end
 
   def user_mention(user)
-    avi = gravatar_for user, 48
+    avi = avatar_for user, 24
     name = content_tag :span, user.name
     content_tag :span, avi + name, class: 'mention'
   end
