db01ae3ca21c747924001b762b879d101e9bd7fd
Lachlan Campbell <lachlan@hackclub.com>
2019-01-31 02:59:00 -0500

    Add icons to dock cards

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+  def dock_card(name, icon, color, url)
+    link_to url, class: 'card' do
+      inline_icon(icon, size: 32, class: color) + content_tag(:span, name)
+    end
+  end
 end
