a4c745aac6f1d7fc60216de9d8f7b16404687938
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2019-01-31 20:46:05 -0500

    Update app/mailers/money_received_mailer.rb
    Co-Authored-By: tmb <me@theos.space>

--- app/mailers/money_received_mailer.rb
+++ app/mailers/money_received_mailer.rb
@@ -3,7 +3,6 @@ class MoneyReceivedMailer < ApplicationMailer
     @transaction = params[:transaction]
     @emails = @transaction.event.users.pluck(:email)
     
-
     mail to: @emails, subject: "Money from #{@transaction.invoice_payout.invoice.sponsor.name} is in Bank"
   end
 end
