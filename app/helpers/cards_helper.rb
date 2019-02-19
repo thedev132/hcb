367c8f9b7d19c2b9540fa236be561920a4d696bd
Lachlan Campbell <lachlan@hackclub.com>
2019-02-19 03:24:39 -0800

    Add card_mention helper

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,4 +1,10 @@
 module CardsHelper
+  def card_mention(card)
+    icon = inline_icon 'card', size: 24, class: 'accent'
+    text = content_tag :span, card.last_four
+    link_to(card, class: 'mention') { icon + text }
+  end
+
   def one_isnt_completed?(emburse_transactions) 
     emburse_transactions.collect { |a| !a.completed? }.include?(true)
   end
