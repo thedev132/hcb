653c1de4199619ffb6ed1e642233e67d3f900164
Lachlan Campbell <lachlan@hackclub.com>
2019-02-18 20:11:20 -0800

    Add tooltips to dock

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
-  def dock_card(name, icon, color, url)
-    link_to url, class: 'card' do
+  def dock_card(name, tooltip, icon, color, url)
+    link_to url, class: 'card dock__card tooltipped tooltipped--s', 'aria-label': tooltip do
       inline_icon(icon, size: 32, class: color) + content_tag(:span, name)
     end
   end
