2b000949da91128c85b22b02c33c011b45bacab4
Max Wofford <max@maxwofford.com>
2019-02-25 22:09:25 -0800

    Add parents to application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -17,14 +17,14 @@ class ApplicationsController < ApplicationController
       "Tell us about your history": params[:application][:about_history],
       "Have you already opened sign-ups for the event?": params[:application][:opened_sign_ups] ? "No" : "Yes",
       "Expected Attendees": params[:application][:expected_attendees].to_i,
-      "Application": [app.id],
+      "Application": [ app.id ],
       "Expected Budget": params[:application][:expected_budget].to_i
     })
     events.create(event)
 
     team = Airtable::Record.new(
       "Has your team organized events before? If so, which ones, what were the budgets, and how many people came?": params[:application][:organizer_history],
-      "Application": [app.id]
+      "Application": [ app.id ]
     )
     teams.create(team)
 
@@ -36,9 +36,19 @@ class ApplicationsController < ApplicationController
         "Phone": member_params[:phone_number],
         "Date of birth": birthdate,
         "Title": member_params[:title],
-        "Team": [team.id]
+        "Team": [ team.id ]
       })
       team_members.create(team_member)
+
+      if Date.today - 18.years <= Date.parse(birthdate)
+        parent_birthdate = "#{member_params['parent_birthdate(1i)']}-#{member_params['parent_birthdate(2i)']}-#{member_params['parent_birthdate(3i)']}"
+        parent = Airtable::Record.new({
+          "Name (First / Last)": member_params[:parent_name],
+          "Child": [ team_member.id ],
+          "Email": member_params[:parent_email],
+          "Phone": member_params[:parent_phone_number],
+          "Date of birth": parent_birthdate
+        })
     end
 
     render :submit
