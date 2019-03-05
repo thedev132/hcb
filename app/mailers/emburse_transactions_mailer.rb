c88f253a5f4a66be439ccfbb78f8978b9fc5b259
Theo B <me@theos.space>
2019-03-05 17:13:45 -0500

    add emburse transaction notification

--- app/mailers/emburse_transactions_mailer.rb
+++ app/mailers/emburse_transactions_mailer.rb
@@ -5,4 +5,13 @@ class EmburseTransactionsMailer < ApplicationMailer
     mail to: admin_email,
       subject: "[Action Requested] No department linked to Emburse transaction ##{@emburse_transaction.id}"
   end
+
+  def organizer_notify(params)
+    @event = params[:event]
+    @emburse_transaction = params[:emburse_transaction]
+
+    emails = @event.users.map { |u| u.email }
+    mail to: emails,
+      subject: "#{@emburse_transaction.card.user.name} spent #{@emburse_transaction.amount} at #{@emburse_transaction.merchant_name}"
+  end
 end
