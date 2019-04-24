6a267bb8e5cb3f524d3b6bcb435ed11438793599
Theo B <me@theos.space>
2019-04-24 17:34:00 -0400

    remove status from fee reimbursements

--- db/migrate/20190424211751_create_fee_reimbursements.rb
+++ db/migrate/20190424211751_create_fee_reimbursements.rb
@@ -2,7 +2,6 @@ class CreateFeeReimbursements < ActiveRecord::Migration[5.2]
   def change
     create_table :fee_reimbursements do |t|
       t.bigint :amount
-      t.integer :status
       t.string :transaction_memo
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_211850) do
+ActiveRecord::Schema.define(version: 2019_04_24_211751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -320,10 +320,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_211850) do
     t.text "status"
     t.integer "payout_creation_balance_net"
     t.integer "payout_creation_balance_stripe_fee"
-    t.boolean "reimbursable"
-    t.bigint "fee_reimbursements_id"
     t.index ["creator_id"], name: "index_invoices_on_creator_id"
-    t.index ["fee_reimbursements_id"], name: "index_invoices_on_fee_reimbursements_id"
     t.index ["item_stripe_id"], name: "index_invoices_on_item_stripe_id", unique: true
     t.index ["manually_marked_as_paid_user_id"], name: "index_invoices_on_manually_marked_as_paid_user_id"
     t.index ["payout_creation_queued_job_id"], name: "index_invoices_on_payout_creation_queued_job_id", unique: true
@@ -495,7 +492,6 @@ ActiveRecord::Schema.define(version: 2019_04_24_211850) do
   add_foreign_key "g_suite_applications", "users", column: "creator_id"
   add_foreign_key "g_suite_applications", "users", column: "fulfilled_by_id"
   add_foreign_key "g_suites", "events"
-  add_foreign_key "invoices", "fee_reimbursements", column: "fee_reimbursements_id"
   add_foreign_key "invoices", "invoice_payouts", column: "payout_id"
   add_foreign_key "invoices", "sponsors"
   add_foreign_key "invoices", "users", column: "creator_id"
