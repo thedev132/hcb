911956b11cc81c3efc546052b3890cb82c536faf
Theo B <me@theos.space>
2019-05-06 21:34:49 -0400

    Add comments to document policy

--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -38,10 +38,14 @@ class DocumentPolicy < ApplicationPolicy
 
   def fiscal_sponsorship_letter?
     return true if user.admin?
-    return true if record.blank?
+    # if there are no documents then user can't access fiscal sponsorship doc
+    return false if record.blank?
     
+    # get all event ids in document collection
     event_ids = record.map(&:event).pluck(:id)
+    # boolean for if all of them are the same
     same_event = event_ids.uniq.size == 1
+    # if all the event ids are the same and the user accessing owns that event, give access.
     return true if same_event && user.events.pluck(:id).include?(event_ids.first)
   end
 end
