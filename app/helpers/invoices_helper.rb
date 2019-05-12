843182f665f1d1f2212e6743a111691ad447d649
Max Wofford <max@maxwofford.com>
2019-05-11 19:14:14 -0700

    Add invoice badge helpers

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -68,3 +68,64 @@ module InvoicesHelper
     render_money profit
   end
 end
+
+def invoice_payment_method_mention(invoice = @invoice)
+  return nil unless invoice.manually_marked_as_paid? || invoice&.payment_method_type
+
+  if invoice.manually_marked_as_paid?
+    icon_name = 'post-fill'
+    description_text = 'Manually marked as paid'
+  elsif invoice&.payment_method_card_brand
+    icon_name = {
+      'amex' => 'card-amex',
+      'mastercard' => 'card-mastercard',
+      'visa' => 'card-visa'
+    }[invoice&.payment_method_card_brand] || 'card-other'
+    description_text = "••••#{invoice&.payment_method_card_last4}"
+  else
+    icon_name = 'bank-account'
+    description_text = invoice.payment_method_type.humanize
+  end
+
+  description = content_tag :span, description_text, class: 'ml1'
+  icon = inline_icon icon_name, size: 24, class: 'slate'
+  content_tag(:span, class: 'inline-flex items-center') { icon + description }
+end
+
+def invoice_card_country_mention(invoice = @invoice)
+  country_code = invoice&.payment_method_card_country
+
+  return nil unless country_code
+  
+  # Hack to turn country code into the country's flag
+  # https://stackoverflow.com/a/50859942
+  emoji = country_code.tr('A-Z', "\u{1F1E6}-\u{1F1FF}")
+
+  "#{country_code} #{emoji}"
+end
+
+def invoice_card_check_badge(check, invoice = @invoice)
+  case invoice["payment_method_card_checks_#{check}_check"]
+  when 'pass'
+    background = 'success'
+    icon_name = 'checkmark'
+    text = 'Passed'
+  when 'failed'
+    background = 'warning'
+    icon_name = 'checkbox'
+    text = 'Failed'
+  when 'unchecked'
+    background = 'info'
+    icon_name = 'checkbox'
+    text = 'Unchecked'
+  else
+    background = 'info'
+    icon_name = 'checkbox'
+    text = 'Unavailable'
+  end
+
+  icon_tag = inline_icon icon_name, size: 20
+  description_tag = content_tag :span, text
+
+  content_tag('span', class: "badge h4 medium ml0 bg-#{background}"){ description_tag + icon_tag }
+end
