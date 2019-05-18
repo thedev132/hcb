afee4228c6ac6fb2c51c76487d379d1ca6ab464d
Theo Bleier <me@theos.space>
2019-05-18 14:01:53 -0400

    Revert "Revert "Fix user slugs in URLs""

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 
     sign_in u
     if u.full_name.blank? || u.phone_number.blank?
-      redirect_to edit_user_path(u)
+      redirect_to edit_user_path(u.slug)
     else
       redirect_back_or root_path
     end
