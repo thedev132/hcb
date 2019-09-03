bac2a59a1c0d9ca073c57a3b1cc9e96db387e6fc
Theo Bleier <me@theos.space>
2019-09-03 00:10:07 -0400

    (bug fix) allow plaid to work in development w/o changes (#222)

--- app/services/plaid_service.rb
+++ app/services/plaid_service.rb
@@ -26,12 +26,8 @@ class PlaidService
   def env
     # Since we're only using one account & Plaid's development plan supports up
     # to 100 accounts, we're just going to stick with the development key in
-    # production for now.
-    if Rails.env.production?
-      'development'
-    else
-      'sandbox'
-    end
+    # production for now. Plaid is essentially read-only, so this also works in development.
+    'development'
   end
 
   def client
