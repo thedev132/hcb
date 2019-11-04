261656c309a69121c1a1606fdfd6dde6548bd99b
Linus Lee <linus@thesephist.com>
2019-11-03 18:21:06 -0800

    Remove the ugly ios notification dot things on admin page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
   def card_to(name, path, options = {})
-    badge = options[:badge].to_i > 0 ? badge_for(options[:badge], {class: 'badge--notification'}) : ''
+    badge = options[:badge].to_i > 0 ? badge_for(options[:badge]) : ''
     link_to content_tag(:li,
                         [content_tag(:strong, name), badge].join.html_safe,
                         class: 'card card--item card--hover relative overflow-visible line-height-3'),
