2b690f04542ff6a4ee90b27dd0079eb9709bcd48
Linus Lee <linus@thesephist.com>
2019-11-25 02:56:26 -0800

    When removing organizer, also remove any pending invites they sent (#343)
    * When removing organizer, also remove any pending invites they sent
    
    * Only cancel OP invites for the event from which user is being removed

--- app/controllers/organizer_positions_controller.rb
+++ app/controllers/organizer_positions_controller.rb
@@ -3,6 +3,14 @@ class OrganizerPositionsController < ApplicationController
     @organizer_position = OrganizerPosition.find(params[:id])
     authorize @organizer_position
     @organizer_position.destroy
+
+
+    # also remove all organizer invites from the organizer that are still pending
+    invites = @organizer_position.event.organizer_position_invites.pending.where(sender: @organizer_position.user)
+    invites.each do |ivt|
+      ivt.cancel
+    end
+
     flash[:success] = "Removed #{@organizer_position.user.email} from the team!"
     redirect_to event_team_path(@organizer_position.event)
   end
