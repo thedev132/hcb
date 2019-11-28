34b31f52764b80addb0cfac610a53ba858dde30a
Linus Lee <linus@thesephist.com>
2019-11-28 00:37:01 -0500

    Fix card request email subject lines

--- app/mailers/card_request_mailer.rb
+++ app/mailers/card_request_mailer.rb
@@ -4,7 +4,7 @@ class CardRequestMailer < ApplicationMailer
     @creator = @request.creator
     @event = @request.event
 
-    mail to: admin_email, subject: "New card request for #{@event}"
+    mail to: admin_email, subject: "New card request for #{@event.name}"
   end
 
   def accepted
@@ -17,6 +17,6 @@ class CardRequestMailer < ApplicationMailer
     @activation_link = @card.emburse_path
 
     mail to: @recipient,
-         subject: "Your #{@event} credit card from Hack Club Bank is on the way!"
+      subject: "Your #{@event.name} credit card from Hack Club Bank is on the way!"
   end
 end
