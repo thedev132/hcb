bbed8622438018b3b89767bfcf5ed0822f2064f0
Linus Lee <linus@thesephist.com>
2019-11-27 22:41:51 -0500

    Show nicknames in the badge under the header in index page (#348)

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,6 +4,31 @@ module StaticPagesHelper
     link_to content_tag(:li,
                         [content_tag(:strong, name), badge].join.html_safe,
                         class: 'card card--item card--hover relative overflow-visible line-height-3'),
-            path, method: options[:method]
+                        path, method: options[:method]
+  end
+
+  def random_nickname()
+    if Rails.env.development?
+      'Development Mode'
+    else
+      [
+        'The hivemind known as Bank',
+        'A cloud full of money',
+        "Hack Club's pot of gold",
+        'A sentient stack of dollars',
+        'The Hack Club Federal Reserve',
+        'money money money money money',
+        'A money-crazed virus 🤑',
+        'A cloud raining money',
+        "A pile of money in the cloud that isn't Bitcoin",
+        'Hack Club Smoothmunny',
+        'Hack Club ezBUCKS',
+        'Hack Club Money Bucket',
+        'A mattress stuffed with 100 dollar bills',
+        'Hack Club Dollaringos',
+        'The Hack Foundation dba The Dolla Store',
+        'Hack on.',
+      ].sample
+    end
   end
 end
