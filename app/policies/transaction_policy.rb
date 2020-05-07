4bec0e371a809e1aa63b10fa04063842162e023b
Max Wofford <max@maxwofford.com>
2020-05-07 15:55:27 -0400

    Changes for making hq public (#596)
    * Hide note attachments
    
    * Disable check buttons
    
    * Fix indentation & hide donor emails
    
    * Fix outdated usage of comment partial
    
    * Hide POC email on reimbursement page
    
    * Hide source transaction memo in edit page
    
    * Redact contact info of POC in pending fees
    
    * Redact check memos

--- app/policies/transaction_policy.rb
+++ app/policies/transaction_policy.rb
@@ -13,12 +13,14 @@ class TransactionPolicy < ApplicationPolicy
   end
 
   def edit?
-    is_public || admin_or_teammember
+    admin_or_teammember
   end
 
   def update?
     admin_or_teammember
   end
+
+  private
 
   def admin_or_teammember
     user&.admin? || record&.event&.users&.include?(user)
