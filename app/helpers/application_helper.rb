593a305ad7e66ab1bfe3b5ae346da3e10f6a9a7f
Max Wofford <max@maxwofford.com>
2020-05-07 22:01:16 -0400

    Add build lifetime to footer (#599)
    * Add build lifetime to footer
    
    * Bank logo in development is visually distinct
    
    * Add flavor to development mode tooltip
    
    * Graciously fail if no commit info is found
    
    * Handle empty commit hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -138,4 +138,40 @@ module ApplicationHelper
   def title(text)
     content_for :title, text
   end
+
+  def commit_hash
+    @commit_hash ||= begin
+      hash = ENV['HEROKU_SLUG_COMMIT'] || `git show --pretty=%H -q`&.chomp
+
+      hash[0...7]
+    end
+
+    @commit_hash
+  end
+
+  def commit_time
+    @commit_time ||= begin
+      ENV['HEROKU_RELEASE_CREATED_AT'] || `git log -1 --format=%at`&.chomp&.to_i
+    end
+
+    @commit_time
+  end
+
+
+  def commit_duration
+    @commit_duration ||= begin
+      distance_of_time_in_words Time.at(commit_time), Time.now
+    end
+  end
+
+  def development_mode_flavor
+    [
+      "Drop the tables for all I care.",
+      "More, tonight at 9.",
+      "Go wild.",
+      "But, the night is still young ;P",
+      "You pansy.",
+      "Now go write some code?"
+    ].sample
+  end
 end
