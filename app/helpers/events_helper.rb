1bbeba1e993c87e88d00a87bb39285c49ab4ac8d
Lachlan Campbell <lachlan@hackclub.com>
2020-05-20 18:26:16 -0400

    Revert "Use tab bar on mobile"
    This reverts commit c7749b7ff7278b4035869767ebd3f158abc8e818.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   def dock_card(name, tooltip, icon, color, url)
-    link_to url, class: 'dock__card tooltipped tooltipped--n', 'aria-label': tooltip do
+    link_to url, class: 'card dock__card tooltipped tooltipped--n', 'aria-label': tooltip do
       inline_icon(icon, size: 32, class: color) + content_tag(:span, name)
     end
   end
