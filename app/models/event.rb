5b2aa23b95f0307418185b8d6f402ffa5e58c9bb
Lachlan Campbell <lachlan@hackclub.com>
2020-07-01 11:35:23 -0400

    Remove validations of start, end, & address (#703)

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,7 +37,7 @@ class Event < ApplicationRecord
 
   validate :point_of_contact_is_admin
 
-  validates :name, :start, :end, :address, :sponsorship_fee, presence: true
+  validates :name, :sponsorship_fee, presence: true
   validates :slug, uniqueness: true, presence: true, format: { without: /\s/ }
 
   before_create :default_values
