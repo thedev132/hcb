c9faef85e43619ccc5e00ae5147798d0b8fda0a4
Max Wofford <max@maxwofford.com>
2020-07-02 18:44:17 +0000

    Start g_suite_status if no g_suite

--- app/models/event.rb
+++ app/models/event.rb
@@ -168,11 +168,11 @@ class Event < ApplicationRecord
   end
 
   def g_suite_status
-    return :start if g_suite_application.nil?
+    return :start if g_suite_application.nil? || g_suite.nil?
     return :under_review if g_suite_application.under_review?
     return :app_accepted if g_suite_application.accepted? && g_suite.present?
     return :app_rejected if g_suite_application.rejected?
-    return :verify_setup if !g_suite.verified?
+    return :verify_setup unless g_suite.verified?
     return :done if g_suite.verified?
 
     :start
