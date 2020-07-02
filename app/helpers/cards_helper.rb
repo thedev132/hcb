3480f8650a98c15dd72e43f954978ab3ec8fe951
Lachlan Campbell <lachlan@hackclub.com>
2020-07-02 14:55:48 +0000

    Redesign card page

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,6 +1,6 @@
 module CardsHelper
   def card_mention(card)
-    icon = inline_icon 'card', size: 24, class: 'purple'
+    icon = inline_icon 'card', size: 24, class: 'purple pr1'
     if organizer_signed_in?
       text = content_tag :span, card.last_four
       return link_to(card, class: 'mention') { icon + text }
