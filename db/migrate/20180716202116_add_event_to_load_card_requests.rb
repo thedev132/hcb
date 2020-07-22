ce7eabeafc49473b99106740b4615eba38328c4c
Max Wofford <max@maxwofford.com>
2020-07-22 15:27:32 +0000

    Fix historic lcr migration using a model name

--- db/migrate/20180716202116_add_event_to_load_card_requests.rb
+++ db/migrate/20180716202116_add_event_to_load_card_requests.rb
@@ -1,10 +1,16 @@
 class AddEventToLoadCardRequests < ActiveRecord::Migration[5.2]
+  class MigrationLoadCardRequest < ActiveRecord::Base
+    self.table_name = :load_card_requests
+  end
+  class MigrationCard < ActiveRecord::Base
+    self.table_name = :cards
+  end
   def change
     add_reference :load_card_requests, :event, foreign_key: true
 
-    LoadCardRequest.all.each do |lcr|
-      lcr.event_id = lcr.card.event_id
-      lcr.save!
+    MigrationLoadCardRequest.find_each do |lcr|
+      card = MigrationCard.find_by(card_id: lcr.card_id)
+      lcr.update!(event_id: card.event_id)
     end
   end
 end
