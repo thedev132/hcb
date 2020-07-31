15e6a1060c5ecc0bc78542ae4f078eabdcb47893
motdotla <mot@mot.la>
2020-07-31 11:32:07 -0700

    [TXT bug] Add future TODO

--- app/models/shared/domain.rb
+++ app/models/shared/domain.rb
@@ -2,6 +2,8 @@ module Shared
   module Domain
     private
 
+    # TODO: these 3 methods can probably be combined into 1 domain validation or a more user friendly input that gets cleaned up in the model
+
     def domain_without_protocol
       bad = ['http', ':', '/'].any? { |s| domain.to_s.include? s }
       errors.add(:domain, 'shouldn’t include http(s):// or ending /') if bad
