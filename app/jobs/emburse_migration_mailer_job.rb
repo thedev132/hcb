0293f200df61a5de510f34f23ac4b55edd9b5571
Max Wofford <max@maxwofford.com>
2020-09-02 23:38:30 +0000

    Emburse migration mailing job runs for real

--- app/jobs/emburse_migration_mailer_job.rb
+++ app/jobs/emburse_migration_mailer_job.rb
@@ -2,7 +2,7 @@ class EmburseMigrationMailerJob < ApplicationJob
   def perform
     users = EmburseCardRequest.where('fulfilled_by_id IS NOT NULL').where('is_virtual IS NOT TRUE').map(&:creator).uniq
 
-    users.select{|u| u.email == 'max@maxwofford.com'}.each do |user|
+    users.each do |user|
       puts "Emailing #{user.email}"
       CardMailer.with(user: user).emburse_migration.deliver_later
     end
