6cc368cb9d307a6bbad37b77264bd250401d2cdd
Max Wofford <max@maxwofford.com>
2020-09-02 16:50:15 +0000

    Improve Emburse migration mailer copy

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -1,12 +1,17 @@
 class CardMailer < ApplicationMailer
   def emburse_migration
     @user = params[:user]
-    @events_with_cards = @user.emburse_cards.map(&:event)
-    @event_names = @event.pluck(:name).join(', ')
-    @event_code = @events.pluck(:id)('|')
-    @form_link = "https://hack.af/emburse_migration?Prefill_Account=#{@user.email}&Prefill_Events=#{@eventCode}-#{@event_names}"
+    @events_with_cards = @user.emburse_cards.map(&:event).uniq
+    @event_names = @events_with_cards.pluck(:name)
+    @event_code = @events_with_cards.pluck(:id)
+    @form_links = @events_with_cards.map do |event|
+      {
+        name: event.name,
+        url: "https://hack.af/emburse_migration?prefill_Account=#{@user.email}|#{@user.id}&prefill_Events=#{event.id}-#{event.name}"
+      }
+    end
 
-    mail to: @user.email
-         subject: "[Action Requested] Your Hack Club Bank card expires by Sept 15th"
+    mail to: @user.email,
+         subject: "[Action Requested] Your Hack Club Bank card expires Sept 15th"
   end
 end
