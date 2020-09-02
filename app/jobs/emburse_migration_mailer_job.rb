68783a1bab11e65b22fae0943ee33b4c055d2839
Max Wofford <max@maxwofford.com>
2020-09-02 23:23:18 +0000

    Only run emburse mailer for admins

--- app/jobs/emburse_migration_mailer_job.rb
+++ app/jobs/emburse_migration_mailer_job.rb
@@ -2,7 +2,7 @@ class EmburseMigrationMailerJob < ApplicationJob
   def perform
     users = EmburseCardRequest.where('fulfilled_by_id IS NOT NULL').where('is_virtual IS NOT TRUE').map(&:creator).uniq
 
-    users.each do |user|
+    users.where('admin_at IS NOT NULL').each do |user|
       puts "Emailing #{user.email}"
       CardMailer.with(user: user).emburse_migration.deliver_later
     end
