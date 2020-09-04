369d42d0bfe63bb065287c65c768e5f81ddf3a8b
Max Wofford <max@maxwofford.com>
2020-09-04 15:41:31 +0000

    Fix multiple events method in stripe card mailer

--- app/mailers/stripe_card_mailer.rb
+++ app/mailers/stripe_card_mailer.rb
@@ -3,7 +3,9 @@ class StripeCardMailer < ApplicationMailer
     @card = params[:card]
     @user = @card.user
     @event = @card.event
+    @has_multiple_events = @user.events.size > 1
     @recipient = @user.email
+
     mail to: @recipient,
          subject: "Your new Hack Club Bank card for #{@event.name} is on its way"
   end
