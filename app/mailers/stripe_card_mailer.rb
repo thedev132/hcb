5e4dfa0504ca7e67a9d0b8ace83d6ee90d568a34
Max Wofford <max@maxwofford.com>
2020-09-06 21:35:55 +0000

    Attempt to fix Stripe physical card ordered mailer not going out

--- app/mailers/stripe_card_mailer.rb
+++ app/mailers/stripe_card_mailer.rb
@@ -1,10 +1,11 @@
 class StripeCardMailer < ApplicationMailer
   def physical_card_ordered
-    @card = Card.find params[:card_id]
+    @card = StripeCard.find params[:card_id]
     @user = @card.user
     @event = @card.event
     @has_multiple_events = @user.events.size > 1
     @recipient = @user.email
+    @eta = @card.stripe_obj.to_hash[:shipping][:eta]
 
     mail to: @recipient,
          subject: "Your new Hack Club Bank card for #{@event.name} is on its way"
