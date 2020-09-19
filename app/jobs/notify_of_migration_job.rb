c601e5bf8f91c3eb76cccd9045acafc8a7b9357c
Max Wofford <max@maxwofford.com>
2020-09-19 22:11:33 +0000

    Don't send migration email to users without organizer positions

--- app/jobs/notify_of_migration_job.rb
+++ app/jobs/notify_of_migration_job.rb
@@ -2,7 +2,7 @@ class NotifyOfMigrationJob < ApplicationJob
   queue_as :default
 
   def perform
-    users = User.includes(:emburse_cards).where.not(emburse_cards: { id: nil })
+    users = User.includes(:emburse_cards, :organizer_positions).where.not(emburse_cards: { id: nil }, organizer_positions: { id: nil }).uniq
     users.each do |user|
       EmburseCardMailer.with(user: user).warn_of_migration.deliver_later
     end
