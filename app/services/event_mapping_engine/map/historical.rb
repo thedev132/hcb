473cba3677a7409e9ffe3b6349c3cef35b26fae8
Max Wofford <max@maxwofford.com>
2020-10-11 16:11:49 +0000

    Initial historical assignment (#952)

--- app/services/event_mapping_engine/map/historical.rb
+++ app/services/event_mapping_engine/map/historical.rb
@@ -1,11 +1,36 @@
 module EventMappingEngine
   module Map
     class Historical
-      def initialize
+      def run
+        likely_historicals.find_each do |ct|
+          historical_transactions = Transaction.where(
+            amount: ct.amount_cents,
+            name: ct.memo,
+            date: ct.date
+          )
+
+          return unless historical_transactions.size == 1
+
+          guessed_event_id = historical_transactions.first&.event&.id
+
+          return unless guessed_event_id
+
+          attrs = {
+            canonical_transaction_id: ct.id
+            event_id: guessed_event_id
+          }
+          ::CanonicalEventMapping.create!(attrs)
+        end
       end
 
-      def run
-        # IMPLEMENT
+      private
+
+      def likely_historicals
+        ::CanonicalTransaction.where('created_at <= ?', last_historical_transaction_date)
+      end
+
+      def last_historical_transaction_date
+        Transaction.order('date DESC').first.date
       end
     end
   end
