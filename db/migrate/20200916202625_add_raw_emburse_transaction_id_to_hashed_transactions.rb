649909350cb46685eeb3376a9db654db4e42517c
motdotla <mot@mot.la>
2020-10-23 11:03:21 -0700

    [1010] Add process to hash raw csv transactions

--- db/migrate/20200916202625_add_raw_emburse_transaction_id_to_hashed_transactions.rb
+++ db/migrate/20200916202625_add_raw_emburse_transaction_id_to_hashed_transactions.rb
@@ -1,4 +1,6 @@
 class AddRawEmburseTransactionIdToHashedTransactions < ActiveRecord::Migration[6.0]
+  disable_ddl_transaction!
+
   def change
     add_column :hashed_transactions, :raw_emburse_transaction_id, :integer
   end
---
+++ spec/services/transaction_engine/hashed_transaction_service/raw_csv_transaction/import_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TransactionEngine::HashedTransactionService::RawCsvTransaction::Import do
+  fixtures :raw_csv_transactions
+
+  let(:raw_csv_transaction) { raw_csv_transactions(:raw_csv_transaction1) }
+
+  let(:service) { TransactionEngine::HashedTransactionService::RawCsvTransaction::Import.new }
+
+  it 'creates hashed transactions' do
+    expect do
+      service.run
+    end.to change(HashedTransaction, :count).by(1)
+  end
+
+  context 'when run twice' do
+    before do
+      service.run
+    end
+
+    it 'is idempotent and does not change the count' do
+      expect do
+        service.run
+      end.to_not change(HashedTransaction, :count)
+    end
+
+    context 'when new raw csv transaction is added' do
+      before do
+        attrs = raw_csv_transaction.attributes
+        attrs.delete('id')
+
+        RawCsvTransaction.create!(attrs)
+      end
+
+      it 'changes count' do
+        expect do
+          service.run
+        end.to change(HashedTransaction, :count).by(1)
+      end
+    end
+  end
+end
