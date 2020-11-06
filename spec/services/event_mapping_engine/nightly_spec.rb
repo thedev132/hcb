3fec3ef13ba0cdee4c9c5bf7380c2236eee625b3
motdotla <mot@mot.la>
2020-11-06 11:25:31 -0800

    Fix specs

--- spec/services/event_mapping_engine/nightly_spec.rb
+++ spec/services/event_mapping_engine/nightly_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe EventMappingEngine::Nightly do
   let(:service) { EventMappingEngine::Nightly.new }
 
   it 'succeeds' do
-    expect(service).to receive(:map_historical!).and_return(true)
+    expect(service).to receive(:map_historical_plaid!).and_return(true)
+    expect(service).to receive(:map_historical_emburse!).and_return(true)
     expect(service).to receive(:map_github!).and_return(true)
 
     service.run
