3ba2e15d7ec21a4a048885a1394f883870c35900
Lachlan Campbell <lachlan@hackclub.com>
2020-11-13 17:31:20 -0500

    HC-6 Sidebar navigation (#687)
    * First draft of sidebar navigation
    
    * Iterate on navigation
    
    * Update nav with new icons
    
    * Fix merge conflicts
    
    * Improve sidebar significantly
    
    * Improve mid-screen experience
    
    * Simplify donations page in Transparency Mode
    
    * Improve dev icon
    
    * Fix modal close border radius
    
    * Fix transaction table in Transparency
    
    * Continue iterating
    
    * Fix send icon sizing
    
    * Finish sidebar layout
    
    * Last pass
    
    * Add bottom padding to app

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,9 @@
 module EventsHelper
-  def dock_card(name, tooltip, icon, color, url)
-    link_to url, class: 'dock__card tooltipped tooltipped--n', 'aria-label': tooltip do
-      inline_icon(icon, size: 32, 'aria-hidden': true, class: color) + content_tag(:span, name)
+  def dock_item(name, tooltip, icon,  url, lg = false)
+    link_to url,
+      class: "dock__item #{lg && 'dock__item--lg'} tooltipped tooltipped--e",
+      'aria-label': tooltip do
+      inline_icon(icon, size: 32, class: 'primary') + content_tag(:span, name.html_safe, class: 'line-height-3')
     end
   end
 end
