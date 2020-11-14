61cab4e27c9500934dea6771bfe7506131c4afe2
motdotla <mot@mot.la>
2020-11-14 08:27:36 -0800

    Add nightly mapping job and nuke data before running

--- spec/models/canonical_hashed_mapping_spec.rb
+++ spec/models/canonical_hashed_mapping_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe CanonicalHashedMapping, type: :model do
-  fixtures  "canonical_hashed_mappings", "canonical_transactions"
+  fixtures "canonical_hashed_mappings", "canonical_transactions", "hashed_transactions"
 
   let(:canonical_hashed_mapping) { canonical_hashed_mappings(:canonical_hashed_mapping1) }
 
