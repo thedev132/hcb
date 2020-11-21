cee3b61e036d32d18dd08b7e742a3f74d3184c68
motdotla <mot@mot.la>
2020-11-21 09:40:18 -0800

    [raw-stripe] Update list to iterate over remote stripe transactions

--- app/services/partners/stripe/issuing/transactions/list.rb
+++ app/services/partners/stripe/issuing/transactions/list.rb
@@ -5,18 +5,36 @@ module Partners
         class List
           include StripeService
 
-          def initialize
-          end
-
           def run
-            ::StripeService::Issuing::Transaction.list(list_attrs)
+            stripe_transactions
           end
 
           private
 
-          def list_attrs
+          def stripe_transactions
+            resp = fetch_transactions
+
+            ts = resp.data
+
+            while resp.has_more
+              starting_after = ts.last.id
+
+              resp = fetch_transactions(starting_after: starting_after)
+
+              ts += resp.data
+            end
+
+            ts
+          end
+
+          def fetch_transactions(starting_after: nil)
+            ::StripeService::Issuing::Transaction.list(list_attrs(starting_after: starting_after))
+          end
+
+          def list_attrs(starting_after:)
             {
-              limit: 3
+              starting_after: starting_after,
+              limit: 100
             }
           end
         end
