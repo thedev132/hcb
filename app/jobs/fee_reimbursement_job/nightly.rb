d87b50ab79fcc52c9e61828242bcd48ffef16338
motdotla <mot@mot.la>
2020-12-30 12:00:03 -0800

    Kick off another job

--- app/jobs/fee_reimbursement_job/nightly.rb
+++ app/jobs/fee_reimbursement_job/nightly.rb
@@ -3,8 +3,8 @@
 module FeeReimbursementJob
   class Nightly < ApplicationJob
     def perform
-      FeeReimbursement.unprocessed.each do |fee_reimbursement_id|
-        FeeReimbursementService::ProcessOnSvb.new(fee_reimbursement_id: fee_reimbursement_id).run
+      FeeReimbursement.unprocessed.each do |fr|
+        FeeReimbursementJob::ProcessOnSvb.perform(fr.id)
       end
     end
   end
