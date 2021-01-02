a6523d015be7b21e598a3db163ac437633eb3350
motdotla <mot@mot.la>
2021-01-02 15:59:24 -0800

    Remove nuke

--- app/jobs/pending_transaction_engine_job/nightly.rb
+++ app/jobs/pending_transaction_engine_job/nightly.rb
@@ -4,7 +4,7 @@ module PendingTransactionEngineJob
   class Nightly < ApplicationJob
     def perform
       ::PendingEventMappingEngine::Nuke.new.run
-      ::PendingTransactionEngine::Nuke.new.run
+      #::PendingTransactionEngine::Nuke.new.run
 
       ::PendingTransactionEngine::Nightly.new.run
     end
