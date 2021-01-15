59895e020e7c9ea6bccbffc23a91f570ae2e8552
motdotla <mot@mot.la>
2021-01-14 16:25:34 -0800

    Move fee payments calculation to run against fees table - in order to support non-standard named canonical transactions

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -4,4 +4,6 @@ class Fee < ApplicationRecord
   validates :reason, presence: true
   validates :amount_cents_as_decimal, numericality: { greater_than_or_equal_to: 0 }
   validates :event_sponsorship_fee, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :hack_club_fee, -> { where(reason: "HACK CLUB FEE") }
 end
