85693cecda966beadc42668458c2c6e39b6f0193
motdotla <mot@mot.la>
2021-01-23 15:07:56 -0800

    Provide a waive fee link

--- app/controllers/canonical_transactions_controller.rb
+++ app/controllers/canonical_transactions_controller.rb
@@ -6,4 +6,17 @@ class CanonicalTransactionsController < ApplicationController
 
     redirect_to transaction_url(params[:id])
   end
+
+  def waive_fee
+    authorize CanonicalTransaction
+
+    @canonical_transaction = CanonicalTransaction.find(params[:id])
+
+    fee = @canoical_transaction.canonical_event_mapping.fees.first
+    fee.amount_cents_as_decimal = 0
+    fee.reason = "REVENUE WAIVED"
+    fee.save!
+
+    redirect_to transaction_url(params[:id])
+  end
 end
