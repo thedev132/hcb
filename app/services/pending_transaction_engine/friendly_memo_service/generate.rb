b3868ea3fa84231e8b5862a18aedbdbe8e8d72cd
motdotla <mot@mot.la>
2021-02-08 12:55:29 -0800

    Add friendly memo for outgoing checks

--- app/services/pending_transaction_engine/friendly_memo_service/generate.rb
+++ app/services/pending_transaction_engine/friendly_memo_service/generate.rb
@@ -6,6 +6,8 @@ module PendingTransactionEngine
       end
 
       def run
+        return "CHECK ##{check_number}" if outgoing_check?
+
         memo
       end
 
@@ -13,6 +15,22 @@ module PendingTransactionEngine
 
       def memo
         @memo ||= @pending_canonical_transaction.memo
+      end
+
+      def memo_upcase
+        memo.upcase
+      end
+
+      def outgoing_check?
+        @outgoing_check ||= raw_pending_outgoing_check_transaction.present?
+      end
+
+      def raw_pending_outgoing_check_transaction
+        @raw_pending_outgoing_check_transaction ||= @pending_canonical_transaction.raw_pending_outgoing_check_transaction
+      end
+
+      def check_number
+        raw_pending_outgoing_check_transaction.check_number
       end
     end
   end
