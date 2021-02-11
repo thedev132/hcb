e867b7a73b2ee42e820564422d5151b5551f33ca
motdotla <mot@mot.la>
2021-02-10 17:02:03 -0800

    Cancel a check

--- app/services/check_service/cancel.rb
+++ app/services/check_service/cancel.rb
@@ -1,5 +1,5 @@
 module CheckService
-  class Create
+  class Cancel
     def initialize(check_id:)
       @check_id = check_id
     end
@@ -8,7 +8,7 @@ module CheckService
       ActiveRecord::Base.transaction do
         check.mark_canceled!
 
-        Partners::Lob::Checks::Cancel.new(id: check.lob_id).run
+        # ::Partners::Lob::Checks::Cancel.new(id: check.lob_id).run # not supported/necessary on our plan
       end
     end
 
