aa29c014bc3ac13d20b06d5895dd0499a4da3309
motdotla <mot@mot.la>
2021-02-10 16:45:12 -0800

    Faster pending checks assignment

--- app/services/pending_transaction_engine/raw_pending_outgoing_check_transaction_service/lob/import.rb
+++ app/services/pending_transaction_engine/raw_pending_outgoing_check_transaction_service/lob/import.rb
@@ -7,10 +7,9 @@ module PendingTransactionEngine
 
         def run
           pending_outgoing_check_transactions.each do |poct|
-            ::RawPendingOutgoingCheckTransaction.find_or_initialize_by(lob_transaction_id: poct["id"]).tap do |t|
-              t.lob_transaction = poct
-              t.amount = -poct["amount"] # Lob returns amounts like 10.02 or 10 (dollar.cents format with cents chopped off for round numbers). use negative since these are outgoing.
-              t.date_posted = poct["date_created"]
+            ::RawPendingOutgoingCheckTransaction.find_or_initialize_by(check_transaction_id: poct.id.to_s).tap do |t|
+              t.amount_cents = -check.amount
+              t.date_posted = check.created_at
             end.save!
           end
 
@@ -20,7 +19,7 @@ module PendingTransactionEngine
         private
 
         def pending_outgoing_check_transactions
-          @pending_outgoing_check_transactions ||= ::Partners::Lob::Checks::List.new(start_date: Time.now.utc - 5.years).run
+          @pending_outgoing_check_transactions ||= Check.all
         end
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_220837) do
+ActiveRecord::Schema.define(version: 2021_02_11_002130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -749,6 +749,7 @@ ActiveRecord::Schema.define(version: 2021_02_08_220837) do
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "check_transaction_id"
   end
 
   create_table "raw_pending_stripe_transactions", force: :cascade do |t|
