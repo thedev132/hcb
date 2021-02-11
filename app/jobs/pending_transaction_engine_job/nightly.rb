b22aa6495166e0c4ca88c49cefda41aae71e8bda
motdotla <mot@mot.la>
2021-02-11 11:36:06 -0800

    Turn off pending mapping nuke

--- app/jobs/pending_transaction_engine_job/nightly.rb
+++ app/jobs/pending_transaction_engine_job/nightly.rb
@@ -3,7 +3,7 @@
 module PendingTransactionEngineJob
   class Nightly < ApplicationJob
     def perform
-      ::PendingEventMappingEngine::Nuke.new.run
+      #::PendingEventMappingEngine::Nuke.new.run
       #::PendingTransactionEngine::Nuke.new.run
 
       ::PendingTransactionEngine::Nightly.new.run
