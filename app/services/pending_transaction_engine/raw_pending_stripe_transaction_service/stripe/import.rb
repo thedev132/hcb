f505cd37ec1f0c9963a8a236aafce7025036e094
motdotla <mot@mot.la>
2021-02-12 11:25:05 -0800

    StripeAuthorizations should enter pending engine as negative amount_cents

--- app/services/pending_transaction_engine/raw_pending_stripe_transaction_service/stripe/import.rb
+++ app/services/pending_transaction_engine/raw_pending_stripe_transaction_service/stripe/import.rb
@@ -9,7 +9,7 @@ module PendingTransactionEngine
           pending_stripe_transactions.each do |t|
             ::RawPendingStripeTransaction.find_or_initialize_by(stripe_transaction_id: t[:id]).tap do |st|
               st.stripe_transaction = t
-              st.amount_cents = t[:amount]
+              st.amount_cents = -t[:amount] # it's a transaction card swipe so it is always negative (but Stripe returns it as a positive value)
               st.date_posted = Time.at(t[:created])
             end.save!
           end
