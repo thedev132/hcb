e5621ca8c71c8ff3ce3d872aa529f814aaed4443
motdotla <mot@mot.la>
2021-02-16 11:14:18 -0800

    Add special case for SVBFSOPERATING account

--- app/services/transaction_engine/shared.rb
+++ app/services/transaction_engine/shared.rb
@@ -11,6 +11,8 @@ module TransactionEngine
         "EVENTFISCALSPONSORSHIP"
       when 4
         "FSMAIN"
+      when "SVBFSOPERATING" # SVB FS OPERATING
+        "SVBFSOPERATING" # used to handle special case of some ACH transfers going out under the wrong bank account
       when 9
         "HACKFOUNDATION5667" # FS CHECK ESCROW
       when 11
