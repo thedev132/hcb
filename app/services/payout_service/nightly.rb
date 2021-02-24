cdd2a640d03e7b066e38af948d0abe498328b34b
motdotla <mot@mot.la>
2021-02-23 22:35:07 -0800

    Adjust naming

--- app/services/payout_service/nightly.rb
+++ app/services/payout_service/nightly.rb
@@ -1,12 +1,12 @@
 module PayoutService
   class Nightly
     def run
-      Donation.succeeded.where("payout_id is null").each do |d|
+      Donation.succeeded.where("payout_id is null").each do |donation|
         # 1. fetch payment intent
-        pi = ::Partners::Stripe::PaymentIntents::Show.new(id: d.stripe_payment_intent_id).run
+        payment_intent = ::Partners::Stripe::PaymentIntents::Show.new(id: donation.stripe_payment_intent_id).run
 
         # 2. get remote available_on timestamp
-        available_on = pi.charges.data.first.balance_transaction.available_on
+        available_on = payment_intent.charges.data.first.balance_transaction.available_on
 
         # 3. create payout if time is ready. TODO: move this into the scope (by later moving the available_on into its own field on the donation table)
         donation.create_payout! if ready_for_payout?(available_on: available_on)
