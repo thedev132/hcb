bc8e815d68c4fd518f00a70ae8598d9441155935
motdotla <mot@mot.la>
2021-02-24 11:22:21 -0800

    Add allow2ban

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -91,5 +91,15 @@ class Rack::Attack
     end
   end
 
-
+  # Lockout IP addresses that are hammering your donation page.
+  # After 20 requests in 1 minute, block all requests from that IP for 1 hour.
+  blocklist('allow2ban donation scrapers') do |req|
+    # `filter` returns false value if request is to your donation page (but still
+    # increments the count) so request below the limit are not blocked until
+    # they hit the limit.  At that point, filter will return true and block.
+    Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 20, findtime: 1.minute, bantime: 1.hour) do
+      # The count for the IP is incremented if the return value is truthy.
+      req.path.start_with?('/donations/start') || req.path.start_with?('/donations/hq')
+    end
+  end
 end
