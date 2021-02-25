066e18a36a40c14c71dea185f8d3fe709d776338
motdotla <mot@mot.la>
2021-02-25 15:03:04 -0800

    Add to stripe top up logic

--- app/models/canonical_transaction.rb
+++ app/models/canonical_transaction.rb
@@ -9,8 +9,8 @@ class CanonicalTransaction < ApplicationRecord
 
   scope :likely_github, -> { where("memo ilike '%github grant%'") }
   scope :likely_hack_club_fee, -> { where("memo ilike '%Hack Club Bank Fee TO ACCOUNT%'") }
-  scope :stripe_top_up, -> { where("memo ilike '%Hack Club Bank Stripe Top%'") }
-  scope :not_stripe_top_up, -> { where("memo not ilike '%Hack Club Bank Stripe Top%' or memo is null") }
+  scope :stripe_top_up, -> { where("memo ilike '%Hack Club Bank Stripe Top%' or memo ilike '%HACKC Stripe Top%'") }
+  scope :not_stripe_top_up, -> { where("(memo not ilike '%Hack Club Bank Stripe Top%' and memo not ilike '%HACKC Stripe Top%') or memo is null") }
 
   monetize :amount_cents
 
