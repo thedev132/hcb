5621c8739e478d7f316196007bce064aa57df237
motdotla <mot@mot.la>
2021-02-24 16:30:15 -0800

    Only run if present

--- app/services/temp/sync_custom_memos/nightly.rb
+++ app/services/temp/sync_custom_memos/nightly.rb
@@ -6,6 +6,8 @@ module Temp
           next unless t.display_name
 
           rpt = RawPlaidTransaction.where(plaid_transaction_id: t.plaid_id).first
+          
+          next unless rpt # it's possible that the raw plaid transaction is not yet imported because it's a separate process
 
           rpt.hashed_transactions.each do |ht|
             ct = ht.canonical_transaction
@@ -20,6 +22,8 @@ module Temp
           next unless sa.display_name
 
           rst = RawStripeTransaction.where(stripe_transaction_id: sa.stripe_id).first
+
+          next unless rst
 
           rst.hashed_transactions.each do |ht|
             ct = ht.canonical_transaction
