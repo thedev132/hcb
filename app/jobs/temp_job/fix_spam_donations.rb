078292ba85ab901da84f971256734e45382b82ab
motdotla <mot@mot.la>
2021-02-24 18:14:28 -0800

    Inherit from applicationjob

--- app/jobs/temp_job/fix_spam_donations.rb
+++ app/jobs/temp_job/fix_spam_donations.rb
@@ -1,5 +1,5 @@
 module TempJob
-  class FixSpamDonations
+  class FixSpamDonations < ApplicationJob
     def perform
       ::Donation.not_succeeded.where(event_id: 183, amount: 100).find_each(batch_size: 100) do |d|
         ::StripeService::PaymentIntent.cancel(d.stripe_payment_intent_id) rescue nil
