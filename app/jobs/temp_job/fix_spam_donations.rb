062d8b39bba59a07b039ca23e3ecc31012468f4e
motdotla <mot@mot.la>
2021-02-24 16:39:42 -0800

    Fix temp job

--- app/jobs/temp_job/fix_spam_donations.rb
+++ app/jobs/temp_job/fix_spam_donations.rb
@@ -1,8 +1,8 @@
 module TempJob
   class FixSpamDonations
     def perform
-      Donation.not_succeeded.where(event_id: 183, amount: 100).find_each(batch_size: 100) do |d|
-        StripeService::PaymentIntent.cancel(d.stripe_payment_intent_id) rescue nil
+      ::Donation.not_succeeded.where(event_id: 183, amount: 100).find_each(batch_size: 100) do |d|
+        ::StripeService::PaymentIntent.cancel(d.stripe_payment_intent_id) rescue nil
         d.destroy!
       end
     end
