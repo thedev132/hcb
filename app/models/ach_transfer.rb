c395285dec2bc955b02c697d515f380601ea5c0c
motdotla <mot@mot.la>
2021-03-04 16:17:45 -0800

    Step 1 toward marking ACH transfers deposited on new engine - move to state machine

--- app/models/ach_transfer.rb
+++ app/models/ach_transfer.rb
@@ -1,4 +1,5 @@
 class AchTransfer < ApplicationRecord
+  include AASM
   include Commentable
 
   belongs_to :creator, class_name: 'User'
@@ -8,21 +9,34 @@ class AchTransfer < ApplicationRecord
 
   has_one :t_transaction, class_name: 'Transaction', inverse_of: :ach_transfer
 
-  scope :pending, -> { where(approved_at: nil, rejected_at: nil) }
   scope :approved, -> { where.not(approved_at: nil) }
-  scope :rejected, -> { where.not(rejected_at: nil) }
 
-  def self.in_transit
+  aasm do
+    state :pending, initial: true
+    state :in_transit
+    state :rejected
+    state :deposited
+
+    event :mark_in_transit do
+      transitions from: :pending, to: :in_transit
+    end
+
+    event :mark_rejected do
+      transitions from: :pending, to: :rejected
+    end
+
+    event :mark_deposited do
+      transitions from: :in_transit, to: :deposited
+    end
+  end
+
+  scope :pending_deprecated, -> { where(approved_at: nil, rejected_at: nil) }
+  def self.in_transit_deprecated
     select { |a| a.approved_at.present? && a.t_transaction.nil? }
   end
-
+  scope :rejected_deprecated, -> { where.not(rejected_at: nil) }
   def self.delivered
     select { |a| a.t_transaction.present? }
-  end
-
-  # TODO: move to aasm
-  def aasm_state
-    status
   end
 
   def status
@@ -73,7 +87,7 @@ class AchTransfer < ApplicationRecord
     'checkmark' if deposited?
   end
 
-  def pending?
+  def pending_deprecated?
     approved_at.nil? && rejected_at.nil?
   end
 
@@ -81,15 +95,15 @@ class AchTransfer < ApplicationRecord
     !pending?
   end
 
-  def rejected?
+  def rejected_deprecated?
     rejected_at.present?
   end
 
-  def in_transit?
+  def in_transit_deprecated?
     approved_at.present?
   end
 
-  def deposited?
+  def deposited_deprecated?
     t_transaction.present?
   end
 
@@ -98,18 +112,12 @@ class AchTransfer < ApplicationRecord
   end
 
   def approve!
-    if !pending?
-      errors.add(:ach_transfer, 'has already been approved or rejected!')
-      return false
-    end
+    mark_in_transit!
     update(approved_at: DateTime.now)
   end
 
   def reject!
-    if !pending?
-      errors.add(:ach_transfer, 'has already been approved or rejected!')
-      return false
-    end
+    mark_rejected!
     update(rejected_at: DateTime.now)
   end
 end
---
+++ db/migrate/20210305000917_add_aasm_state_to_ach_transfers.rb
@@ -0,0 +1,5 @@
+class AddAasmStateToAchTransfers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :ach_transfers, :aasm_state, :string
+  end
+end
