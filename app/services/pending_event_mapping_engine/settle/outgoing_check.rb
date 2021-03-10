d39cb77564e291441ef109d75110a51cb1f0fff9
motdotla <mot@mot.la>
2021-03-10 13:55:58 -0800

    Smarter settling for invoice and outgoing check

--- app/services/pending_event_mapping_engine/settle/outgoing_check.rb
+++ app/services/pending_event_mapping_engine/settle/outgoing_check.rb
@@ -4,13 +4,13 @@ module PendingEventMappingEngine
       def run
         unsettled.find_each(batch_size: 100) do |cpt|
           # 1. identify check
-          check = cpt.raw_pending_outgoing_check_transaction.check
+          check = cpt.check
           Airbrake.notify("Check not found for canonical pending transaction #{cpt.id}") unless check
           next unless check
           event = check.event
 
           # 2. look up canonical - scoped to event for added accuracy
-          cts = event.canonical_transactions.where("memo ilike '#{check.check_number} CHECK%'")
+          cts = event.canonical_transactions.where("memo ilike '#{check.check_number} CHECK%' and date >= ?", cpt.date).order("date asc")
 
           next if cts.count < 1 # no match found yet. not processed.
           Airbrake.notify("matched more than 1 canonical transaction for check_number #{check.check_number}") if cts.count > 1
