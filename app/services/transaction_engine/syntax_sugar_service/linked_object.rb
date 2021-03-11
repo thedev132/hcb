57907d3a3491a1d3ac591fafe0770841b46d31fb
motdotla <mot@mot.la>
2021-03-11 09:44:46 -0800

    Return nil unless event attached to canonical transaction

--- app/services/transaction_engine/syntax_sugar_service/linked_object.rb
+++ app/services/transaction_engine/syntax_sugar_service/linked_object.rb
@@ -29,14 +29,20 @@ module TransactionEngine
       private
 
       def likely_check
+        return nil unless event
+
         event.checks.where(check_number: likely_outgoing_check_number).first
       end
 
       def likely_ach
+        return nil unless event
+
         event.ach_transfers.where("recipient_name ilike '%#{likely_outgoing_ach_name}%' and amount = #{-amount_cents}").first # TODO: add smarts where multiple ach transfers to same person with same value
       end
 
       def likely_invoice
+        return nil unless event
+
         potential_payouts = event.payouts.where("invoice_payouts.statement_descriptor ilike 'PAYOUT #{likely_incoming_invoice_short_name}%' and invoice_payouts.amount = #{amount_cents}")
 
         return nil unless potential_payouts.present?
@@ -45,6 +51,8 @@ module TransactionEngine
       end
 
       def likely_invoice_or_donation_for_fee_refund
+        return nil unless event
+
         fee_reimbursement = FeeReimbursement.where("transaction_memo ilike '%#{likely_donation_for_fee_refund_hex_random_id}%'").first
 
         return nil unless fee_reimbursement
@@ -53,6 +61,8 @@ module TransactionEngine
       end
 
       def likely_donation
+        return nil unless event
+
         potential_donation_payouts = event.donation_payouts.where("donation_payouts.statement_descriptor ilike 'DONATE #{likely_donation_short_name}%' and donation_payouts.amount = #{amount_cents}")
 
         return nil unless potential_donation_payouts.present?
@@ -61,6 +71,8 @@ module TransactionEngine
       end
 
       def likely_disbursement
+        return nil unless event
+
         Disbursement.where(id: likely_disbursement_id).first
       end
 
