6669f245793b1e8d45d4bda51b061ca5ab675b95
motdotla <mot@mot.la>
2021-03-10 16:23:56 -0800

    Add to ignorable errors

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -72,11 +72,19 @@ end
 # Rails.logger = Airbrake::AirbrakeLogger.new(Rails.logger)
 #
 
-noisy_errors = [ActionController::RoutingError]
+noisy_errors = [ActionController::RoutingError, Plaid::PlaidAPIError, Google::Apis::ClientError]
 
 Airbrake.add_filter do |notice|
   next unless noisy_errors.include?(notice.stash[:exception].class)
 
   # Ignore this error 90% of the time.
   notice.ignore! if rand(1..10) <= 9
+end
+
+ignorable_errors = [SignalException, Sidekiq::Shutdown]
+
+Airbrake.add_filter do |notice|
+  next unless ignorable_errors.include?(notice.stash[:exception].class)
+
+  notice.ignore!
 end
