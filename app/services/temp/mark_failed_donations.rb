39418f65633ad83270afc8cf45d8c1782f5c1afe
motdotla <mot@mot.la>
2021-03-17 10:14:26 -0700

    Do not mark failed if already marked failed

--- app/services/temp/mark_failed_donations.rb
+++ app/services/temp/mark_failed_donations.rb
@@ -4,7 +4,8 @@ module Temp
       ::Partners::Stripe::Charges::List.new.run do |sc|
         if sc.status == "failed" && sc.payment_intent.present?
           d = Donation.where(stripe_payment_intent_id: sc.payment_intent).first
-          d.mark_failed! if d
+
+          d.mark_failed! if d && !d.failed?
         end
       end
     end
