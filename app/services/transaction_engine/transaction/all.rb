14056ea2158d1a6b11ee065fbf89aca4ba17181b
motdotla <mot@mot.la>
2021-03-20 09:58:32 -0700

    Add alternate roll up query

--- app/services/transaction_engine/transaction/all.rb
+++ app/services/transaction_engine/transaction/all.rb
@@ -25,17 +25,30 @@ module TransactionEngine
         @canonical_transactions ||= CanonicalTransaction.includes(:receipts).where(id: canonical_event_mappings.pluck(:canonical_transaction_id)).order("date desc, id desc")
       end
 
+      def canonical_transaction_ids
+        @canonical_transaction_ids ||= canonical_event_mappings.pluck(:canonical_transaction_id)
+      end
+
       def canonical_transactions_grouped
         q = <<-SQL
           select 
-            hcb_code
-            ,array_to_string(array_agg(date), ', ')
-            ,array_to_string(array_agg(memo), ', ')
-            ,sum(amount_cents) as amount_cents
+            COALESCE(ct.hcb_code, CAST(ct.id AS text)) as hcb_code
+            ,array_to_string(array_agg(ct.date), ', ') as date
+            ,array_to_string(array_agg(ct.memo), ', ') as memo
+            ,sum(ct.amount_cents) as amount_cents
+            ,(sum(ct.amount_cents) / 100.0) as amount
           from 
-            canonical_transactions
+            canonical_transactions ct
+          where ct.id in (
+            select
+              cem.canonical_transaction_id
+            from
+              canonical_event_mappings cem
+            where
+              cem.event_id = #{event.id}
+          )
           group by
-            hcb_code
+            COALESCE(ct.hcb_code, CAST(ct.id AS text))
         SQL
         ActiveRecord::Base.connection.execute(q)
       end
