3ae96a2b65015aa9bdb998f27125b6d8ec5db781
motdotla <mot@mot.la>
2021-03-23 10:44:25 -0700

    Idempotent creation of pending transaction and event mapping on webhook

--- app/jobs/stripe_authorization_job/create_from_webhook.rb
+++ app/jobs/stripe_authorization_job/create_from_webhook.rb
@@ -3,23 +3,7 @@
 module StripeAuthorizationJob
   class CreateFromWebhook < ApplicationJob
     def perform(stripe_transaction_id)
-      # 1. fetch remote stripe transaction (authorization)
-      remote_stripe_transaction = ::Partners::Stripe::Issuing::Authorizations::Show.new(id: stripe_transaction_id).run
-
-      # 2. import into the db
-      rpst = ::PendingTransactionEngine::RawPendingStripeTransactionService::Stripe::ImportSingle.new(remote_stripe_transaction: remote_stripe_transaction).run
-
-      # 3. canonize the newly added raw pending stripe transaction
-      attrs = {
-        date: rpst.date,
-        memo: rpst.memo,
-        amount_cents: rpst.amount_cents,
-        raw_pending_stripe_transaction_id: rpst.id
-      }
-      ct = ::CanonicalPendingTransaction.create!(attrs)
-
-      # 4. map to the event
-      # TODO: 
+      ::StripeAuthorizationService::CreateFromWebhook.new(stripe_transaction_id: stripe_transaction_id).run
     end
   end
 end
---
+++ app/services/stripe_authorization_service/create_from_webhook.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module StripeAuthorizationService
+  class CreateFromWebhook
+    def initialize(stripe_transaction_id:)
+      @stripe_transaction_id = stripe_transaction_id
+    end
+
+    def run
+      ActiveRecord::Base.transaction do
+        # 1. fetch remote stripe transaction (authorization)
+        remote_stripe_transaction = ::Partners::Stripe::Issuing::Authorizations::Show.new(id: @stripe_transaction_id).run
+        return unless remote_stripe_transaction
+
+        # 2. idempotent import into the db
+        rpst = ::PendingTransactionEngine::RawPendingStripeTransactionService::Stripe::ImportSingle.new(remote_stripe_transaction: remote_stripe_transaction).run
+
+        # 3. idempotent canonize the newly added raw pending stripe transaction
+        cpt = ::PendingTransactionEngine::CanonicalPendingTransactionService::ImportSingle::Stripe.new(raw_pending_stripe_transaction: rpst).run
+
+        # 4. idempotent map to event
+        ::PendingEventMappingEngine::Map::Single::Stripe.new(canonical_pending_transaction: cpt).run
+      end
+    end
+  end
+end
