57e26f6c565257786c96a3493bab9420b7fa0b09
motdotla <mot@mot.la>
2021-03-24 14:50:52 -0700

    Fix migration error

--- db/migrate/20210324213548_add_unique_index_to_hcb_codes_hcb_code.rb
+++ db/migrate/20210324213548_add_unique_index_to_hcb_codes_hcb_code.rb
@@ -2,7 +2,6 @@ class AddUniqueIndexToHcbCodesHcbCode < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def change
-    remove_index :hcb_codes, :hcb_code
     add_index :hcb_codes, :hcb_code, unique: true, algorithm: :concurrently
   end
 end
