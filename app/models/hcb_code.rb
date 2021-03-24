e5c5fb140eaf54ad1c2ced9091d2a47e0b3db369
motdotla <mot@mot.la>
2021-03-24 15:33:35 -0700

    Adds hcb codes admin interface

--- app/models/hcb_code.rb
+++ app/models/hcb_code.rb
@@ -3,4 +3,12 @@
 class HcbCode < ApplicationRecord
   include Commentable
   include Receiptable
+
+  def canonical_pending_transactions
+    CanonicalPendingTransaction.where(hcb_code: hcb_code)
+  end
+
+  def canonical_transactions
+    CanonicalTransaction.where(hcb_code: hcb_code)
+  end
 end
