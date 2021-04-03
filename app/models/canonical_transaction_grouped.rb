c94e0f3e9d0336f536b8d686bfdbb729b77ed2f9
motdotla <mot@mot.la>
2021-04-03 09:30:30 -0700

    Direct to disbursement url when a disbursement

--- app/models/canonical_transaction_grouped.rb
+++ app/models/canonical_transaction_grouped.rb
@@ -20,6 +20,8 @@ class CanonicalTransactionGrouped
   end
 
   def url
+    return "/transactions/#{ct.id}" if disbursement?
+
     return "/hcb/#{local_hcb_code.hashid}" if local_hcb_code
 
     "/transactions/#{ct.id}"
