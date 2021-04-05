6843eb5d4c9ce04aa18ba786e9ee43ff3bbfa4ac
motdotla <mot@mot.la>
2021-04-05 10:40:56 -0700

    Fix spelling

--- app/services/donation_service/refunds.rb
+++ app/services/donation_service/refunds.rb
@@ -2,7 +2,7 @@ module DonationService
   class Refunds
     def run
       ::Donation.deposited.each do |donation|
-        if donation.remote_funded?
+        if donation.remote_refunded?
 
           ::DonationService::Refund.new(donation: donation).run
 
