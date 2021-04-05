45c1ef16420474db0295e4f4504a6268673b77ee
motdotla <mot@mot.la>
2021-04-05 10:48:26 -0700

    Move donation refund to a sidekiq job

--- app/services/donation_service/refund.rb
+++ app/services/donation_service/refund.rb
@@ -4,17 +4,19 @@ module DonationService
     include ::Shared::Selenium::TransferFromFsOperatingToFsMain
     include ::Shared::Selenium::TransferFromFsMainToFsOperating
 
-    def initialize(donation:)
-      @donation = donation
+    def initialize(donation_id:)
+      @donation_id = donation_id
     end
 
     def run
+      return nil unless donation.event.id == 183 # temporary - only apply to HQ
+
       login_to_svb!
 
       ActiveRecord::Base.transaction do
-        raise ArgumentError unless @donation.remote_refunded?
+        raise ArgumentError unless donation.remote_refunded?
 
-        @donation.mark_refunded!
+        donation.mark_refunded!
 
         transfer_from_fs_main_to_fs_operating!(amount_cents: amount_cents, memo: memo) # make the transfer on remote bank similar to monthly fee service
       end
@@ -27,11 +29,15 @@ module DonationService
     private
 
     def amount_cents
-      @donation.amount
+      donation.amount
     end
 
     def memo
-      "#{@donation.hcb_code} REFUND"
+      "#{donation.hcb_code} REFUND"
+    end
+
+    def donation
+      @donation ||= ::Donation.find(@donation_id)
     end
   end
 end
