e49c50f92a55ac2f7b6f27ff771dd167924ca989
motdotla <mot@mot.la>
2021-04-08 11:38:28 -0700

    Identify the transaction

--- app/services/pending_event_mapping_engine/settle/donation_hcb_code.rb
+++ app/services/pending_event_mapping_engine/settle/donation_hcb_code.rb
@@ -5,7 +5,10 @@ module PendingEventMappingEngine
         unsettled.find_each(batch_size: 100) do |cpt|
           # 1. Wait for 2 canonical transactions (payout and fee reimbursement)
           if cpt.local_hcb_code.canonical_transactions.length == 2
-            # 2. mark no longer pending
+            # 2. identify ct
+            ct = cpt.local_hcb_code.canonical_transactions.first
+
+            # 3. mark no longer pending
             attrs = {
               canonical_transaction_id: ct.id,
               canonical_pending_transaction_id: cpt.id
