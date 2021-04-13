d5c8c89f41d34b27c77ffae16eac7837f0270c90
motdotla <mot@mot.la>
2021-04-13 16:52:12 -0700

    Patch checkbox logic for new event

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -143,8 +143,8 @@ class AdminController < ApplicationController
     attrs = {
       name: params[:name],
       emails: emails,
-      has_fiscal_sponsorship_document: params[:has_fiscal_sponsorship_document] === 1 ? true : false,
-      spend_only: params[:spend_only] === 1 ? true : false,
+      has_fiscal_sponsorship_document: params[:has_fiscal_sponsorship_document].to_i === 1 ? true : false,
+      spend_only: params[:spend_only].to_i === 1 ? true : false,
       sponsorship_fee: params[:sponsorship_fee]
     }
     ::EventService::Create.new(attrs).run
