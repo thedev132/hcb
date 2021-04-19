dd5d57bc06795254214b63e88e0e361a207c0730
Gary Tou <gary@garytou.com>
2021-04-19 13:17:45 -0700

    use update_column

--- app/jobs/temp_job/event_point_of_contact_mel.rb
+++ app/jobs/temp_job/event_point_of_contact_mel.rb
@@ -3,8 +3,7 @@ module TempJob
     def perform
 			mel_user_id = 2046
 			Event.where.not(point_of_contact_id: mel_user_id).find_each(batch_size: 100) do |e|
-				e.point_of_contact_id = mel_user_id
-				e.save!
+				e.update_column(:point_of_contact_id, mel_user_id)
 			end
     end
   end
