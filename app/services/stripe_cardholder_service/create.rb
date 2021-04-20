b6f3d4008c2204234d4dd55776815794c63b9929
motdotla <mot@mot.la>
2021-04-19 23:07:19 -0700

    Rename to user

--- app/services/stripe_cardholder_service/create.rb
+++ app/services/stripe_cardholder_service/create.rb
@@ -20,7 +20,7 @@ module StripeCardholderService
 
     def attrs
       {
-        current_user: @current_user,
+        user: @current_user,
         stripe_billing_address_line1: line1,
         #stripe_billing_address_line2: line2,
         stripe_billing_address_city: city,
