bfc533dee7a08b616c2aa3ba1ef03140421f0c6e
motdotla <mot@mot.la>
2021-04-26 14:30:47 -0700

    Add additional docs to Bank connect

--- app/documentations/connect_v1_documentation.rb
+++ app/documentations/connect_v1_documentation.rb
@@ -3,15 +3,15 @@
 class ConnectV1Documentation < ApplicationDocumentation
   swagger_path "/api/connect/v1/start" do
     operation :get do
-      key :summary, "GET v1"
-      key :description, "Send user to Bank Connect flow"
+      key :summary, "Send user through Bank Connect flow"
+      key :description, "Send user through Bank Connect flow"
       key :tags, ["connect"]
       key :operationId, "connectV1Start"
 
       parameter do
-        key :name, :uniqueIdentifier
+        key :name, :organizationIdentifier
         key :in, :query
-        key :description, "Uniquely identify the person/organization applying through Bank Connect"
+        key :description, "Uniquely identify the organization applying through Bank Connect"
         key :required, true
         schema do
           key :type, :string
@@ -21,7 +21,17 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :redirectUrl
         key :in, :query
-        key :description, "Bank Connect redirects to this page after the user finishes completing Bank Connect."
+        key :description, "Bank Connect redirects to this page after the user finishes the Bank Connect flow"
+        key :required, true
+        schema do
+          key :type, :string
+        end
+      end
+
+      parameter do
+        key :name, :webhookUrl
+        key :in, :query
+        key :description, "Bank Connect sends a webhook payload to this endpoint when organization is approved for Bank"
         key :required, true
         schema do
           key :type, :string
@@ -31,7 +41,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :name
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The user's full name"
         key :required, false
         schema do
           key :type, :string
@@ -41,7 +51,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :email
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The user's email address"
         key :required, false
         schema do
           key :type, :string
@@ -51,7 +61,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :phone
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The user's phone number"
         key :required, false
         schema do
           key :type, :string
@@ -61,7 +71,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :address
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The user's address"
         key :required, false
         schema do
           key :type, :string
@@ -71,7 +81,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :birthdate
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The user's birthdate"
         key :required, false
         schema do
           key :type, :string
@@ -81,7 +91,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :organizationName
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The organization's name"
         key :required, false
         schema do
           key :type, :string
@@ -91,7 +101,7 @@ class ConnectV1Documentation < ApplicationDocumentation
       parameter do
         key :name, :organizationUrl
         key :in, :query
-        key :description, ""
+        key :description, "(optional) The organization's website url"
         key :required, false
         schema do
           key :type, :string
@@ -107,10 +117,61 @@ class ConnectV1Documentation < ApplicationDocumentation
 
       response 302 do
         key :description, "Following completion of html page, Bank Connect redirects to the redirectUrl you specified"
-        content :"text/html" do
-          key :example, "${redirectUrl}?uniqueIdentifier=${uniqueBankIdentifier}&status=started|pendingApproval|approved"
+        content :"url" do
+          key :example, "${redirectUrl}?organizationIdentifier=${organizationIdentifier}&status=pending|approved"
         end
       end
     end
   end
+
+  swagger_path "/api/connect/v1/webhook" do
+    operation :post do
+      key :summary, "Receive webhook payload from Bank Connect"
+      key :description, "Receive webhook payload from Bank Connect"
+      key :tags, ["connect"]
+      key :operationId, "connectV1Webhook"
+
+      response 200 do
+        key :description, "Parse this data in order to update the user's Bank Connect status"
+        content :"application/json" do
+          key :example, {
+            data: [
+              {
+                organizationIdentifier: "org_1234",
+                status: "approved"
+              }
+            ]
+          }
+        end
+      end
+    end
+  end
+
+  swagger_path "/api/connect/v1/organizations" do
+    operation :get do
+      key :summary, "Return status information on all your organizations"
+      key :description, "Return status information on all your organizations"
+      key :tags, ["connect"]
+      key :operationId, "connectV1Organizations"
+
+      response 200 do
+        key :description, "Parse this data in order to update the user's Bank Connect status"
+        content :"application/json" do
+          key :example, {
+            data: [
+              {
+                organizationIdentifier: "org_1234",
+                status: "approved"
+              },
+              {
+                organizationIdentifier: "org_4567",
+                status: "pending"
+              }
+            ]
+          }
+        end
+      end
+    end
+  end
+
 end
