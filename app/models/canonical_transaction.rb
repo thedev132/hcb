5c459e98bd9d433858d7adf537e9b0648d5950bb
motdotla <mot@mot.la>
2021-05-11 09:30:19 -0700

    Revert "First pass using new TX engine on stats"
    This reverts commit 9357c4d70a5e6b46ea1c12bf6c91172adf5129fc.

--- app/models/canonical_transaction.rb
+++ app/models/canonical_transaction.rb
@@ -33,7 +33,6 @@ class CanonicalTransaction < ApplicationRecord
   scope :stripe_top_up, -> { where("memo ilike '%Hack Club Bank Stripe Top%' or memo ilike '%HACKC Stripe Top%' or memo ilike '%HCKCLB Stripe Top%'") }
   scope :not_stripe_top_up, -> { where("(memo not ilike '%Hack Club Bank Stripe Top%' and memo not ilike '%HACKC Stripe Top%' and memo not ilike '%HCKCLB Stripe Top%') or memo is null") }
   scope :mapped_by_human, -> { includes(:canonical_event_mapping).where("canonical_event_mappings.user_id is not null").references(:canonical_event_mapping) }
-  scope :included_in_stats, -> { includes(canonical_event_mapping: :event).where(events: {omit_stats: false}) }
 
   monetize :amount_cents
 
