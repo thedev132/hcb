302db268bc4b30129a9d59aae499a60c46919e30
motdotla <mot@mot.la>
2021-05-24 14:22:20 -0700

    Create hcb code when partner donation is created

--- app/models/partner_donation.rb
+++ app/models/partner_donation.rb
@@ -1,3 +1,11 @@
 class PartnerDonation < ApplicationRecord
   belongs_to :partner
+
+  before_create :set_hcb_code
+
+  private
+
+  def set_hcb_code
+    self.hcb_code = "HCB-#{::TransactionGroupingEngine::Calculate::HcbCode::PARTNER_DONATION_CODE}-#{id}"
+  end
 end
--- spec/models/partner_donation_spec.rb
+++ spec/models/partner_donation_spec.rb
@@ -3,11 +3,21 @@
 require "rails_helper"
 
 RSpec.describe PartnerDonation, type: :model do
-  fixtures "partner_donations"
+  fixtures "partner_donations", "partners"
 
   let(:partner_donation) { partner_donations(:partner_donation1) }
 
   it "is valid" do
     expect(partner_donation).to be_valid
+  end
+
+  context "hcb code" do
+    let(:partner) { partners(:partner1) }
+
+    it "generates an hcb code" do
+      pd = partner.partner_donations.create!
+
+      expect(pd.hcb_code).to_not be_nil
+    end
   end
 end
