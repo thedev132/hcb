aa3e3b85d128a2072d86355cb91ef9bfad01b576
motdotla <mot@mot.la>
2021-05-26 09:22:05 -0700

    Generate pending transaction from bank fees

---
+++ app/services/pending_transaction_engine/raw_pending_bank_fee_transaction_service/bank_fee/import.rb
@@ -0,0 +1,27 @@
+module PendingTransactionEngine
+  module RawPendingBankFeeTransactionService
+    module BankFee
+      class Import
+        def initialize
+        end
+
+        def run
+          pending_bank_fee_transactions.each do |pbft|
+            ::RawPendingBankFeeTransaction.find_or_initialize_by(bank_fee_transaction_id: pbft.id.to_s).tap do |t|
+              t.amount_cents = pbft.amount_cents
+              t.date_posted = pbft.created_at
+            end.save!
+          end
+
+          nil
+        end
+
+        private
+
+        def pending_bank_fee_transactions
+          @pending_bank_fee_transactions ||= ::BankFee.where("amount_cents < 0")
+        end
+      end
+    end
+  end
+end
