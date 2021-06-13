a083ebcf9e2ed055e3df6f7d9ea90153ae1dae14
Gary Tou <gary@garytou.com>
2021-06-13 01:56:31 -0700

    Automate setting google workspace verification key

---
+++ app/jobs/g_suite_job/set_verification_key.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module GSuiteJob
+  class SetVerificationKey < ApplicationJob
+    def perform(g_suite_id)
+      @g_suite_id = g_suite_id
+      key = GSuiteService::GetVerificationKey.new(g_suite_id: g_suite_id).run
+
+      GSuiteService::Update.new(g_suite_id:g_suite.id, domain: g_suite.domain, verification_key: key).run
+    end
+
+    def g_suite
+      GSuite.find(@g_suite_id)
+    end
+  end
+end
