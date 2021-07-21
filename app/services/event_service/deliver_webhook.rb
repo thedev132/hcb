a4e8157aa1355b6092d807a11400f5a1ac81cf3e
Scott Motte <mot@mot.la>
2021-07-21 14:26:51 -0700

    Make webhook follow up to 10 redirects

--- app/services/event_service/deliver_webhook.rb
+++ app/services/event_service/deliver_webhook.rb
@@ -7,19 +7,33 @@ module EventService
     def run
       raise ArgumentError, 'webhook_url missing' unless event.webhook_url.present?
 
-      res = Faraday.post(event.webhook_url, Api::V1::OrganizationSerializer.new(event: event).run.to_json, 'Content-Type' => 'application/json')
-
-      if !res.success?
-        raise ArgumentError, "Error delivering webhook. HTTP status: #{res.status}"
+      res = conn.post(event.webhook_url) do |req|
+        req.body = Api::V1::OrganizationSerializer.new(event: event).run.to_json
       end
+
+      raise ArgumentError, "Error delivering webhook. HTTP status: #{res.status}" unless res.success?
 
       res
     end
 
     private
 
+    def conn
+      @conn ||= begin
+        Faraday.new(new_attrs) do |faraday|
+          faraday.use FaradayMiddleware::FollowRedirects, limit: 10
+        end
+      end
+    end
+
+    def new_attrs
+      {
+        headers: {'Content-Type' => 'application/json'}
+      }
+    end
+
     def event
-      Event.find(@event_id)
+      @event ||= Event.find(@event_id)
     end
 
   end
