56648c0c49fe3f7bc47d7857d0c02a51e17493d6
Scott Motte <mot@mot.la>
2021-07-20 23:13:21 -0700

    Remove migration - field already exists

--- db/migrate/20210721054736_add_raw_pending_partner_donation_transaction_id_to_canonical_pending_transactions.rb
+++
@@ -1,7 +0,0 @@
-class AddRawPendingPartnerDonationTransactionIdToCanonicalPendingTransactions < ActiveRecord::Migration[6.0]
-  disable_ddl_transaction!
-
-  def change
-    add_reference :canonical_pending_transactions, :raw_pending_partner_donation_transaction, null: true, index: {name: :index_canonical_pending_txs_on_raw_pending_partner_dntn_tx_id, algorithm: :concurrently}
-  end
-end
