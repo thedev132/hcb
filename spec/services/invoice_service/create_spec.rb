05cb8faa8c849c9da28acddfc3a0ad3f6c897686
Scott Motte <mot@mot.la>
2021-07-29 15:11:40 -0700

    Rubocop repair

--- spec/services/invoice_service/create_spec.rb
+++ spec/services/invoice_service/create_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe InvoiceService::Create, type: :model do
   end
 
   let(:service) { InvoiceService::Create.new(attrs) }
-  
+
   let(:stripe_invoice_item) { double("StripeInvoice", id: 1) }
   let(:stripe_invoice_values) do
     {
