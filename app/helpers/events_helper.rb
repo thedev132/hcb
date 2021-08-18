01487b900782b3965b630b38739bdb10c62db0ed
Gary Tou <gary@garytou.com>
2021-08-18 15:03:13 -0700

    Add PayPal transfer form

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,16 @@ module EventsHelper
       inline_icon(icon, size: 32, class: "primary") + content_tag(:span, name.html_safe, class: "line-height-3")
     end
   end
+
+  def transfers_airtable_form(embed: false, event: nil, user: nil)
+    embed_url = "https://airtable.com/embed/shrH9fMs5hof2HRup"
+    url = "https://airtable.com/shrH9fMs5hof2HRup"
+
+    prefill = []
+    prefill << "prefill_Event/Project+Name=#{event.name}" if event
+    prefill << "prefill_Submitter+Name=#{user.full_name}" if user
+    prefill << "prefill_Submitter+Email=#{user.email}" if user
+
+    (embed ? embed_url : url) + "?" + prefill.join("&")
+  end
 end
