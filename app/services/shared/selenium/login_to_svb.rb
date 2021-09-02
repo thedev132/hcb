c4da4a616ffc8c38d5e3fa0967bc7dc685af9e0d
Scott Motte <mot@mot.la>
2021-09-01 22:53:30 -0700

    Check if login is successful

--- app/services/shared/selenium/login_to_svb.rb
+++ app/services/shared/selenium/login_to_svb.rb
@@ -19,20 +19,20 @@ module Shared
         rescue
         end
 
-        # # Username
-        # sleep 1
-        # el = driver.find_element(id: "userId")
-        # el.send_keys(username)
+        # Username
+        sleep 1
+        el = driver.find_element(id: "userId")
+        el.send_keys(username)
 
-        # # Password
-        # sleep 1
-        # el = driver.find_element(id: "userPassword")
-        # el.send_keys(password)
+        # Password
+        sleep 1
+        el = driver.find_element(id: "userPassword")
+        el.send_keys(password)
 
-        # # Login
-        # sleep 1
-        # el = driver.find_element(id: "loginButton")
-        # el.click
+        # Login
+        sleep 1
+        el = driver.find_element(id: "loginButton")
+        el.click
 
         # # Potentially handle challenge question - otherwise continue
         # begin
@@ -51,12 +51,39 @@ module Shared
         #driver.cookies.add("SVB_OLB_TL_COOKIE","3b45dfb9-91b0-4f00-b967-160600e2fd4d", { domain: "*.svbconnect.com" })
         cookies = selenium_session.cookies
         cookies.each do |key, value|
-          driver.manage.add_cookie(name: key, value: value, path: "/", domain: "svbconnect.com")
+          if cookie_names.include?(key) # only add for a specific list of cookies
+            driver.manage.add_cookie(name: key, value: value, path: "/", domain: "svbconnect.com")
+          end
         end
 
         sleep 1
 
         driver.navigate.to(banking_url)
+
+        begin
+          # Wait until you see the transfer page
+          wait = ::Selenium::WebDriver::Wait.new(timeout: 10) # wait 10 seconds
+          wait.until { driver.find_element(:xpath, "//div[contains(concat(' ', normalize-space(@class),' '),' account-number ')]") }
+        rescue => e
+          Airbrake.notify(driver.inspect)
+          Airbrake.notify(driver.page_source)
+
+          selenium_session.mark_expired!
+
+          raise ArgumentError, "Login failed"
+        end
+      end
+
+      def cookie_names
+        [
+          "TL1M4Djm3mCaVQc_",
+          "_WL_AUTHCOOKIE_OnlineSiliconDummy",
+          "wfx_unq",
+          "SVB_OLB_TL_COOKIE",
+          "OnlineSiliconDummy",
+          "QuantumMetricSessionID",
+          "QuantumMetricUserID"
+        ]
       end
 
       def auth_url
