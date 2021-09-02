441e8493aec34b8c5a848707107c479625f39f0e
Scott Motte <mot@mot.la>
2021-09-02 11:14:10 -0700

    Move action under admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,9 +2,20 @@
 
 class AdminController < ApplicationController
   skip_after_action :verify_authorized # do not force pundit
-  before_action :signed_in_admin
+  before_action :signed_in_admin, except: [:twilio_inbound]
+  skip_before_action :signed_in_user, only: [:twilio_inbound]
 
   layout "application"
+
+  def twilio_messaging
+    ::MfaCodeService::Create.new(message: params[:Body]).run
+
+    # Don't reply to incoming sms message
+    # https://support.twilio.com/hc/en-us/articles/223134127-Receive-SMS-and-MMS-Messages-without-Responding
+    respond_to do |format|
+      format.xml { render xml: "<Response></Response>" }
+    end
+  end
 
   def tasks
     @active = pending_tasks
