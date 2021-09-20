b3e56bc21975ecd4dddaf75860ed0f5351e85d39
Max Wofford <max@maxwofford.com>
2021-09-20 14:10:16 -0400

    Prefill sticker request form

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -310,7 +310,7 @@ module StaticPagesHelper
         "Where's the money lebowski?!",
         "We put the 'poggers' in 'taxes' (there isn't any)",
         "We put the 'fun' in 'accrual-based accounting' (there isn't any)",
-        "<a href='https://hack.af/hcb-stickers' target='_blank'>Want stickers?</a>",
+        "<a href='https://hack.af/hcb-stickers?prefill_Recipient%20Name=#{current_user.full_name}&prefill_Login%20Email=#{current_user.email}' target='_blank'>Want stickers?</a>",
       ].sample
     end
   end
