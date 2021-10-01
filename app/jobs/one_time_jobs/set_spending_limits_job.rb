8fa63ce7632cbab33f14efcae548429a80147a31
Max Wofford <max@maxwofford.com>
2021-10-01 18:50:40 +0000

    Fix card iteration in one-time-job

--- app/jobs/one_time_jobs/set_spending_limits_job.rb
+++ app/jobs/one_time_jobs/set_spending_limits_job.rb
@@ -2,17 +2,18 @@
 
 module OneTimeJobs
   class SetSpendingLimitsJob < ApplicationJob
-    def perform(card)
+    def perform(card= nil)
       if card
         set_spending_limit card
       else
-        StripeCard.all.map(&:set_spending_limit)
+        StripeCard.all.map { |card| set_spending_limit card }
       end
     end
 
     private
 
     def set_spending_limit(card)
+      return if card.stripe_status == "deleted"
       ::StripeCardService::SetSpending.new(card_id: card.stripe_id,
                                            interval: "daily",
                                            amount: 500 * 100
