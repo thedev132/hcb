fd3f74c4d0a180550c14cc573cebdd5d864ae5f9
Max Wofford <max@maxwofford.com>
2021-10-01 18:33:30 +0000

    Fix invalid stripe service invocation

--- app/services/stripe_card_service/set_spending.rb
+++ app/services/stripe_card_service/set_spending.rb
@@ -4,7 +4,7 @@ module StripeCardService
   class SetSpending
     def initialize(card_id:, amount:, interval:)
       @card_id = card_id
-      @card = Card.find_by(stripe_id: card_id)
+      @card = StripeCard.find_by(stripe_id: card_id)
       @spending_limit = {
         amount: amount,
         interval: interval,
@@ -12,11 +12,12 @@ module StripeCardService
     end
 
     def run
-      StripeService::Issuing::Card.update(
+      ::StripeService::Issuing::Card.update(
         @card_id,
         spending_controls: { spending_limits: [@spending_limit] }
       )
       @card.sync_from_stripe!
+      @card.save!
     end
   end
 end
