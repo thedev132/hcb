a9155c28d2425d5c84426ffb212b87b1c3a496ac
Max Wofford <max@maxwofford.com>
2021-10-26 13:16:52 -0400

    Add note about rewrite task

--- db/migrate/20210213222744_change_default_for_pending_transaction_engine_at.rb
+++ db/migrate/20210213222744_change_default_for_pending_transaction_engine_at.rb
@@ -5,5 +5,7 @@ class ChangeDefaultForPendingTransactionEngineAt < ActiveRecord::Migration[6.0]
 
   def change
     change_column_default :events, :pending_transaction_engine_at, DateTime.now
+    # @msw TODO: This should be set to a specific time, because DateTime.now will
+    # constantly update the schema & cause confusion when staging git changes.
   end
 end
