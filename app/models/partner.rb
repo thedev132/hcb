02fd663057cbb540779410d23a1a04218d71f8d7
Max Wofford <max@maxwofford.com>
2021-10-27 12:20:14 -0400

    Ensure API keys on partners

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -7,6 +7,20 @@ class Partner < ApplicationRecord
   has_many :partner_donations, through: :events
 
   validates :slug, exclusion: { in: EXCLUDED_SLUGS }
+  validates :api_key, presence: true, uniqueness: true
+
+  before_create :set_api_key
 
   encrypts :stripe_api_key
+
+  def regenerate_api_key!
+    set_api_key(force: true)
+    save!
+  end
+
+  private
+
+  def set_api_key(force: false)
+    self.api_key = "hcbk_" + SecureRandom.hex(32) if self.private_api_key.nil? || force
+  end
 end
