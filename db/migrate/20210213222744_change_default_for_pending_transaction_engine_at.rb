ce3dc81f69e7b425bba7fbed01bfd9e702066558
Max Wofford <max@maxwofford.com>
2021-10-27 00:18:00 -0400

    Fix typo

--- db/migrate/20210213222744_change_default_for_pending_transaction_engine_at.rb
+++ db/migrate/20210213222744_change_default_for_pending_transaction_engine_at.rb
@@ -4,6 +4,6 @@ class ChangeDefaultForPendingTransactionEngineAt < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def change
-    change_column_default :events, :pending_transaction_engine_at, DateTime.new(2020, 2, 13, 22, 27, 44)
+    change_column_default :events, :pending_transaction_engine_at, DateTime.new(2021, 2, 13, 22, 27, 44)
   end
 end
