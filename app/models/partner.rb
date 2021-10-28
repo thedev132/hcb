e3953891c5c3f462056f8177848adcd2c941afa6
Max Wofford <max@maxwofford.com>
2021-10-28 10:29:49 -0400

    Redo partner api keys with attribute default

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,24 +3,28 @@
 class Partner < ApplicationRecord
   EXCLUDED_SLUGS = %w(connect api donations donation connects organization organizations)
 
+  attribute :api_key, :string, default: -> { new_api_key }
+
   has_many :events
   has_many :partner_donations, through: :events
 
-  validates :slug, exclusion: { in: EXCLUDED_SLUGS }
+  validates :slug, exclusion: { in: EXCLUDED_SLUGS }, uniqueness: true
   validates :api_key, presence: true, uniqueness: true
-
-  before_create :set_api_key
 
   encrypts :stripe_api_key
 
   def regenerate_api_key!
-    set_api_key(force: true)
-    save!
+    self.api_key = new_api_key
+    self.save!
   end
 
   private
 
-  def set_api_key(force: false)
-    self.api_key = "hcbk_" + SecureRandom.hex(32) if (self.private_api_key.nil? || force)
+  def new_api_key
+    "hcbk_#{SecureRandom.hex(32)}"
+  end
+
+  def self.new_api_key
+    self.new.send(:new_api_key)
   end
 end
