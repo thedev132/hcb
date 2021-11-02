ae6ee8ed176582c2011c3ce1320444c2bf0cf5b5
Max Wofford <max@maxwofford.com>
2021-11-02 18:47:14 -0400

    Fix incorrect use of public_id param in login generator

--- app/controllers/api/v2_controller.rb
+++ app/controllers/api/v2_controller.rb
@@ -33,12 +33,12 @@ module Api
       contract = Api::V2::GenerateLoginUrlContract.new.call(params.permit!.to_h)
       render json: json_error(contract), status: 400 and return unless contract.success?
 
-      event = current_partner.events.find_by_public_id(contract[:organization_id])
+      event = current_partner.events.find_by_public_id(contract[:public_id])
       current_partner.add_user_to_partnered_event!(user_email: contract[:email], event: event)
 
       attrs = {
         email: contract[:email],
-        public_organization_id: contract[:organization_id]
+        public_organization_id: contract[:public_id]
       }
 
       render json: Api::V2::GenerateLoginUrlSerializer.new(attrs).run
