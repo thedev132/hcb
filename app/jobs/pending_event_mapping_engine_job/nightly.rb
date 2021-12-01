54d83034120210ca276d5c73d0f8065d45d69b57
Max Wofford <max@maxwofford.com>
2021-12-01 12:20:54 -0500

    Tweak pending TX mapping jobs to reduce retry buildup

--- app/jobs/pending_event_mapping_engine_job/nightly.rb
+++ app/jobs/pending_event_mapping_engine_job/nightly.rb
@@ -2,6 +2,9 @@
 
 module PendingEventMappingEngineJob
   class Nightly < ApplicationJob
+    include Sidekiq::Worker
+    sidekiq_options retry: 1 # This is a job queued by sidekiq cron, so it's ok if we just wait for the next run
+
     def perform
       ::PendingEventMappingEngine::Nightly.new.run
     end
--- app/jobs/pending_transaction_engine_job/nightly.rb
+++ app/jobs/pending_transaction_engine_job/nightly.rb
@@ -2,6 +2,9 @@
 
 module PendingTransactionEngineJob
   class Nightly < ApplicationJob
+    include Sidekiq::Worker
+    sidekiq_options retry: 1 # This is a job queued by sidekiq cron, so it's ok if we just wait for the next run
+
     def perform
       ::PendingTransactionEngine::Nightly.new.run
     end
