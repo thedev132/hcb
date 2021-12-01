593be61b4342586ca0353be92e24e454c4f802f9
Gary Tou <gary@garytou.com>
2021-11-30 20:49:18 -0800

    Patch loop variable name for raw PDN imports (#2074)

--- app/services/pending_transaction_engine/raw_pending_partner_donation_transaction_service/partner_donation/import.rb
+++ app/services/pending_transaction_engine/raw_pending_partner_donation_transaction_service/partner_donation/import.rb
@@ -9,9 +9,9 @@ module PendingTransactionEngine
 
         def run
           pending_partner_donation.find_each(batch_size: 100) do |pdn|
-            ::RawPendingPartnerDonationTransaction.find_or_initialize_by(partner_donation_transaction_id: pdn.id.to_s).tap do |t|
-              t.amount_cents = pdt.payout_amount_cents
-              t.date_posted = pdt.created_at
+            ::RawPendingPartnerDonationTransaction.find_or_initialize_by(partner_donation_transaction_id: pdn.id.to_s).tap do |rppdt|
+              rppdt.amount_cents = pdn.payout_amount_cents
+              rppdt.date_posted = pdn.created_at
             end.save!
           end
 
