6a241746fc1174994a754a9dac03ded4e22f119f
Gary Tou <gary@garytou.com>
2021-12-02 18:54:00 -0800

    Revert "Tweak pending TX mapping jobs to reduce retry buildup" (#2083)
    * Revert "Don't retry pending tx engine jobs"
    
    This reverts commit ee86a4957529d95dde8ac5a1fa07ef5aedd38a64.
    
    * Revert "Tweak pending TX mapping jobs to reduce retry buildup"
    
    This reverts commit 54d83034120210ca276d5c73d0f8065d45d69b57.

--- app/jobs/pending_event_mapping_engine_job/nightly.rb
+++ app/jobs/pending_event_mapping_engine_job/nightly.rb
@@ -2,8 +2,6 @@
 
 module PendingEventMappingEngineJob
   class Nightly < ApplicationJob
-    sidekiq_options retry: false # This is a job queued by sidekiq cron, so it's ok if we just wait for the next run
-
     def perform
       ::PendingEventMappingEngine::Nightly.new.run
     end
--- app/jobs/pending_transaction_engine_job/nightly.rb
+++ app/jobs/pending_transaction_engine_job/nightly.rb
@@ -2,8 +2,6 @@
 
 module PendingTransactionEngineJob
   class Nightly < ApplicationJob
-    sidekiq_options retry: false # This is a job queued by sidekiq cron, so it's ok if we just wait for the next run
-
     def perform
       ::PendingTransactionEngine::Nightly.new.run
     end
