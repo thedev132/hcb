078188e0c2194aea1b1a01826a85c77c4715dbb1
Gary Tou <gary@garytou.com>
2021-12-02 19:25:51 -0800

    Don't reattempt pending tx engine jobs (#2085)
    #2079 and #2083

--- app/jobs/pending_event_mapping_engine_job/nightly.rb
+++ app/jobs/pending_event_mapping_engine_job/nightly.rb
@@ -2,6 +2,11 @@
 
 module PendingEventMappingEngineJob
   class Nightly < ApplicationJob
+    # Don't retry job, reattempt at next cron scheduled run
+    discard_on(StandardError) do |job, error|
+      Airbrake.notify(error)
+    end
+
     def perform
       ::PendingEventMappingEngine::Nightly.new.run
     end
--- app/jobs/pending_transaction_engine_job/nightly.rb
+++ app/jobs/pending_transaction_engine_job/nightly.rb
@@ -2,6 +2,11 @@
 
 module PendingTransactionEngineJob
   class Nightly < ApplicationJob
+    # Don't retry job, reattempt at next cron scheduled run
+    discard_on Exception do |job, error|
+      Airbrake.notify(error)
+    end
+
     def perform
       ::PendingTransactionEngine::Nightly.new.run
     end
