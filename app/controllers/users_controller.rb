7de78d28225ae620aa91d2cc642c94fad8745811
Gary Tou <gary@garytou.com>
2021-12-20 18:42:42 -0800

    Fix open redirect vulnerability (#2117)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,8 @@ class UsersController < ApplicationController
     if user.full_name.blank? || user.phone_number.blank?
       redirect_to edit_user_path(user.slug)
     else
-      redirect_to(params[:return_to] || root_path)
+      return_to = params[:return_to] if params[:return_to].start_with?(root_url)
+      redirect_to(return_to || root_path)
     end
 
   rescue Errors::InvalidLoginCode => e
