8164a4776742e3c953f27772233375fcd77a966b
Ella <72365100+eilla1@users.noreply.github.com>
2022-01-02 00:22:38 -0800

    Migrations: add address lines to partnered signups table (#2150)
    * Make phone no. input E.164 standard
    
    * Add theme toggle
    
    * Open link in new tab, make address a text_area
    
    * Change labels wip
    
    * Add form helper text
    
    * SCSS autoformatter & add dark styles loading animation class
    
    * SCSS single quotes
    
    Oops wrong setting on autoformatter
    
    * Add loading animations
    
    * No thoughts, head empty
    
    I think it broke :((((((((((((((
    
    * Fix scss imports\!\!
    
    * Format: remove spaces
    
    * Fix transition
    
    * Decrease font size of form helper text
    
    * Separate address field (ui only)
    
    * Speed up partner logos animation
    
    * Migrations: add address lines to partnered signups table
    
    * Add magic comment\!
    
    * Run migration
    
    * Make country an integer
    
    * Actually fix merge confict this time

---
+++ db/migrate/20211231074013_add_address_lines_to_partnered_signups.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddAddressLinesToPartneredSignups < ActiveRecord::Migration[6.0]
+  def change
+    add_column :partnered_signups, :owner_address_line1, :string
+    add_column :partnered_signups, :owner_address_line2, :string
+    add_column :partnered_signups, :owner_address_city, :string
+    add_column :partnered_signups, :owner_address_state, :string
+    add_column :partnered_signups, :owner_address_postal_code, :text
+    add_column :partnered_signups, :owner_address_country, :integer
+  end
+end
