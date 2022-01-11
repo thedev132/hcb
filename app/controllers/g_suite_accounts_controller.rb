cbfc1c12df08b1892b87bba43d2273784d9fc3a6
Gary Tou <gary@garytou.com>
2022-01-11 11:58:53 -0800

    Remove mention of application from GSuiteAccount creation (#2209)

--- app/controllers/g_suite_accounts_controller.rb
+++ app/controllers/g_suite_accounts_controller.rb
@@ -27,7 +27,7 @@ class GSuiteAccountsController < ApplicationController
     begin
       GSuiteAccountService::Create.new(attrs).run
 
-      flash[:success] = "Google Workspace account application submitted."
+      flash[:success] = "Google Workspace account submitted."
     rescue => e
       Airbrake.notify(e)
 
