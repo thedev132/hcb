600f2b35083ccfa7be50f4d03b739b067eebb579
Gary Tou <gary@garytou.com>
2022-01-27 23:43:04 -0800

    Don't retry SUP docusign and PDN payout jobs (#2271)

--- app/jobs/partner_donation_job/create_remote_payout.rb
+++ app/jobs/partner_donation_job/create_remote_payout.rb
@@ -2,6 +2,11 @@
 
 module PartnerDonationJob
   class CreateRemotePayout < ApplicationJob
+    # Don't retry job, reattempt at next cron scheduled run
+    discard_on(StandardError) do |job, error|
+      Airbrake.notify(error)
+    end
+
     def perform(partner_id, stripe_charge_id)
       ::PartnerDonationService::CreateRemotePayout.new(partner_id: partner_id, stripe_charge_id: stripe_charge_id).run
     end
