dff82bcf2afe0886552f51e19c13035606274f35
Gary Tou <gary@garytou.com>
2022-04-18 17:19:15 -0700

    Handle case were `manually_marked_as_paid_at` is nil (#2487)

--- app/services/pending_event_mapping_engine/settle/invoice.rb
+++ app/services/pending_event_mapping_engine/settle/invoice.rb
@@ -56,7 +56,7 @@ module PendingEventMappingEngine
             cts = event.canonical_transactions.missing_pending.where("amount_cents = ? and date > ?", cpt.amount_cents, cpt.date) unless cts.present? # see example canonical transaction 198588
 
             if cts.empty? # no match found yet. not processed.
-              Airbrake.notify("Old manually marked as paid invoice #{invoice.id} still doesn't have a matching CT.") if invoice.manually_marked_as_paid_at > 2.weeks.ago
+              Airbrake.notify("Old manually marked as paid invoice #{invoice.id} still doesn't have a matching CT.") if invoice.manually_marked_as_paid_at&.> 2.weeks.ago
               next
             end
 
