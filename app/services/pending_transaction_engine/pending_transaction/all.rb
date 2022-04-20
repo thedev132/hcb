0899d728ebf14429a4538cb9f59360282bdc486f
Gary Tou <gary@garytou.com>
2022-04-19 17:15:49 -0700

    Revert to showing all unsettled Canonical Pending Transactions (#2495)

--- app/services/pending_transaction_engine/pending_transaction/all.rb
+++ app/services/pending_transaction_engine/pending_transaction/all.rb
@@ -23,13 +23,15 @@ module PendingTransactionEngine
       end
 
       def canonical_pending_transactions
-        @canonical_pending_transactions ||= begin
-          if @search.present?
-            CanonicalPendingTransaction.unsettled_with_declined.where(id: canonical_pending_event_mappings.pluck(:canonical_pending_transaction_id)).search_memo(@search).order("date desc, canonical_pending_transactions.id desc")
-          else
-            CanonicalPendingTransaction.unsettled_with_declined.where(id: canonical_pending_event_mappings.pluck(:canonical_pending_transaction_id)).order("date desc, canonical_pending_transactions.id desc")
+        @canonical_pending_transactions ||=
+          begin
+            cpts = CanonicalPendingTransaction.unsettled
+                                              .where(id: canonical_pending_event_mappings.pluck(:canonical_pending_transaction_id))
+                                              .order("date desc, canonical_pending_transactions.id desc")
+
+            cpts = cpts.search_memo(@search) if @search.present?
+            cpts
           end
-        end
       end
 
     end
