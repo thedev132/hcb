b513f91311fa8a080e3f4d41e63d96f3c8ad8333
Albert Chae <albertchae@users.noreply.github.com>
2022-06-11 03:47:31 -0700

    To illustrate a shared setup block, split the test into 2 it blocks
    The before block without an argument is equivalent to before(:each)
    https://stackoverflow.com/questions/12152342/before-hook-in-rspec-same-as-before-all

--- spec/disbursement_wip.rb
+++ spec/disbursement_wip.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe PendingTransactionEngine do
@@ -8,44 +10,62 @@ RSpec.describe PendingTransactionEngine do
   end
 
   describe "source event" do
-    it "is valid" do
+    before do
       # Arrange
-      partner = Partner.create!({
+      @partner = Partner.create!({
         slug: SecureRandom.hex(30)
       })
 
-      source_event = Event.create!({
+      @source_event = Event.create!({
         name: 'source hacks',
-        partner_id: partner.id,
+        partner_id: @partner.id,
         sponsorship_fee: 0,
         organization_identifier: SecureRandom.hex(30)
       })
 
-      destination_event = Event.create!({
+      @destination_event = Event.create!({
         name: 'destinoshacks',
-        partner_id: partner.id,
+        partner_id: @partner.id,
         sponsorship_fee: 0,
         organization_identifier: SecureRandom.hex(30)
       })
 
-      disbursement = Disbursement.create!({
-        event: destination_event,
-        source_event: source_event,
+      @disbursement = Disbursement.create!({
+        event: @destination_event,
+        source_event: @source_event,
         amount: 100,
         name: 'for fun'
       })
 
-      expect(source_event).to be_valid
-      expect(destination_event).to be_valid
-      expect(disbursement).to be_valid
+      expect(@source_event).to be_valid
+      expect(@destination_event).to be_valid
+      expect(@disbursement).to be_valid
+    end
 
-      expect(disbursement.raw_pending_incoming_disbursement_transaction).to eq(nil)
-      expect(disbursement.raw_pending_outgoing_disbursement_transaction).to eq(nil)
+    it "creates raw incoming and outgoing transactions" do
+      expect(@disbursement.raw_pending_incoming_disbursement_transaction).to eq(nil)
+      expect(@disbursement.raw_pending_outgoing_disbursement_transaction).to eq(nil)
 
       cpt_count = CanonicalPendingTransaction.all.size
-      incoming_amount = incoming_deposits destination_event
 
       # Act
+      ::PendingTransactionEngine::RawPendingIncomingDisbursementTransactionService::Disbursement::Import.new.run
+      ::PendingTransactionEngine::RawPendingOutgoingDisbursementTransactionService::Disbursement::Import.new.run
+
+      ::PendingTransactionEngine::CanonicalPendingTransactionService::Import::IncomingDisbursement.new.run
+      ::PendingTransactionEngine::CanonicalPendingTransactionService::Import::OutgoingDisbursement.new.run
+
+      # Assert
+      @disbursement.reload
+      expect(@disbursement.raw_pending_incoming_disbursement_transaction).to be_present
+      expect(@disbursement.raw_pending_outgoing_disbursement_transaction).to be_present
+
+      expect(CanonicalPendingTransaction.all.size).to eq(cpt_count + 2)
+    end
+
+    it "increments the incoming deposit total" do
+      incoming_amount = incoming_deposits @destination_event
+
       ::PendingTransactionEngine::RawPendingIncomingDisbursementTransactionService::Disbursement::Import.new.run
       ::PendingTransactionEngine::RawPendingOutgoingDisbursementTransactionService::Disbursement::Import.new.run
 
@@ -59,13 +79,7 @@ RSpec.describe PendingTransactionEngine do
       ::PendingEventMappingEngine::Decline::IncomingDisbursement.new.run
       ::PendingEventMappingEngine::Decline::OutgoingDisbursement.new.run
 
-      # Assert
-      disbursement.reload
-      expect(disbursement.raw_pending_incoming_disbursement_transaction).to be_present
-      expect(disbursement.raw_pending_outgoing_disbursement_transaction).to be_present
-
-      expect(CanonicalPendingTransaction.all.size).to eq(cpt_count + 2)
-      expect(incoming_deposits(destination_event)).to eq(incoming_amount + 100)
+      expect(incoming_deposits(@destination_event)).to eq(incoming_amount + 100)
     end
   end
 end
