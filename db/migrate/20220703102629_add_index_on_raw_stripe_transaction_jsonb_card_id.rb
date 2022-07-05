502582dd551dadd0dfabe3c5f73747bc02d6dbd8
albertchae <albertchae@users.noreply.github.com>
2022-07-05 11:03:15 -0700

    Add index on stripe_transaction->card->>id on raw_stripe_transactions (#2742)
    Same as https://github.com/hackclub/bank/pull/2741
    but for raw_stripe_transactions instead of raw_pending_stripe_transactions

---
+++ db/migrate/20220703102629_add_index_on_raw_stripe_transaction_jsonb_card_id.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AddIndexOnRawStripeTransactionJsonbCardId < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  def up
+    add_index :raw_stripe_transactions, "((stripe_transaction->'card'->>'id')::text)", name: :index_raw_stripe_transactions_on_card_id_text, using: 'hash', algorithm: :concurrently
+  end
+
+  def down
+    remove_index :raw_stripe_transactions, name: :index_raw_stripe_transactions_on_card_id_text
+  end
+
+end
