d497a29ad70ebe1e87ef2f9cdbf90dcf944b9d3f
Gary Tou <gary@garytou.com>
2022-08-03 15:51:59 -0700

    Card Charge approval mailer: Properly render amount (#2880)

---
+++ spec/mailers/previews/canonical_pending_transaction_mailer_preview.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CanonicalPendingTransactionMailerPreview < ActionMailer::Preview
+  def notify_approved
+    # @cpt = CanonicalPendingTransaction.stripe.last
+    @cpt = CanonicalPendingTransaction.stripe.where('amount_cents < ?', -1_000_00).last
+
+    CanonicalPendingTransactionMailer.with(
+      canonical_pending_transaction_id: @cpt.id,
+    ).notify_approved
+  end
+
+end
