46fa72476ac2b9f8e64f789819951e29aa123e83
albertchae <albertchae@users.noreply.github.com>
2022-09-06 10:42:56 -0700

    Update missing HcbCode tracking (#2987)
    * Remove unnecessary lazy creation of hcb_code for canonical_pending_transactions
    
    This count has been 0 for the past few weeks, which means the
    after_create callback is successfully creating a hcb code every time.
    
    ```
    > Ahoy::Event.where(name: "pendingTransactionMissingHcbCount").pluck(:properties).uniq
       (8.6ms)  SELECT "ahoy_events"."properties" FROM "ahoy_events" WHERE "ahoy_events"."name" = $1  [["name", "pendingTransactionMissingHcbCount"]]
    => [{"count"=>0}]
    ```
    
    It should also be safe to run this on production after
    `Ahoy::Event.where(name: "pendingTransactionMissingHcbCount").delete_all`
    
    * Only count nil hcb_code as missing for canonical_transactions
    
    The previous way would count "unknown" also, so it wasn't informative as
    to whether https://github.com/hackclub/bank/pull/2863 was working

--- app/services/transaction_grouping_engine/nightly.rb
+++ app/services/transaction_grouping_engine/nightly.rb
@@ -11,45 +11,26 @@ module TransactionGroupingEngine
     def run
       canonical_transaction_missing_hcb_count = 0
       canonical_transactions.find_each(batch_size: 100) do |ct|
+        if ct.hcb_code.nil?
+          canonical_transaction_missing_hcb_count += 1
+        end
+
         hcb_code = ::TransactionGroupingEngine::Calculate::HcbCode.new(canonical_transaction_or_canonical_pending_transaction: ct).run
 
         ct.update_column(:hcb_code, hcb_code)
 
         HcbCode.find_or_create_by!(hcb_code: hcb_code)
-
-        canonical_transaction_missing_hcb_count += 1
       end
       Ahoy::Event.create!({
                             name: "canonicalTransactionMissingHcbCount",
                             properties: { count: canonical_transaction_missing_hcb_count }
                           })
-
-
-      canonical_pending_transaction_missing_hcb_count = 0
-      canonical_pending_transactions.find_each(batch_size: 100) do |cpt|
-        hcb_code = ::TransactionGroupingEngine::Calculate::HcbCode.new(canonical_transaction_or_canonical_pending_transaction: cpt).run
-
-        cpt.update_column(:hcb_code, hcb_code)
-
-        HcbCode.find_or_create_by!(hcb_code: hcb_code)
-
-        canonical_pending_transaction_missing_hcb_count += 1
-      end
-      Ahoy::Event.create!({
-                            name: "pendingTransactionMissingHcbCount",
-                            properties: { count: canonical_pending_transaction_missing_hcb_count }
-                          })
-
     end
 
     private
 
     def canonical_transactions
       CanonicalTransaction.missing_or_unknown_hcb_code.where("date >= ?", @start_date)
-    end
-
-    def canonical_pending_transactions
-      CanonicalPendingTransaction.missing_or_unknown_hcb_code
     end
 
   end
