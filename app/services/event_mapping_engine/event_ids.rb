1a6241075a9f005d40e89c4a63d58efed160d282
Caleb Denio <cjdenio44@gmail.com>
2022-09-26 01:13:30 -0400

    More elegantly handle hard-coded event IDs

---
+++ app/services/event_mapping_engine/event_ids.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module EventMappingEngine
+  module EventIds
+    INCOMING_FEES = 1671
+    HACK_CLUB_BANK = 636
+    NOEVENT = 999
+    HACKATHON_GRANT_FUND = 1610
+  end
+end
