4a474f902243c13e64813168e91074327d35e1fb
albertchae <albertchae@users.noreply.github.com>
2022-10-02 17:49:29 -0700

    update schema.rb date (#3077)
    Forgot to include this as part of https://github.com/hackclub/bank/pull/3075

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_164239) do
+ActiveRecord::Schema.define(version: 2022_10_01_225207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
