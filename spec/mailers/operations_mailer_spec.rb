ff56bded2d840ae1a4f45b13a6757d8970e100cb
albertchae <albertchae@users.noreply.github.com>
2022-10-04 16:30:59 -0700

    Unskip operations_mailer_spec and fix (#3081)

--- spec/mailers/operations_mailer_spec.rb
+++ spec/mailers/operations_mailer_spec.rb
@@ -2,23 +2,20 @@
 
 require "rails_helper"
 
-RSpec.describe OperationsMailer, type: :mailer, skip: true do
-  fixtures "g_suites"
+RSpec.describe OperationsMailer, type: :mailer do
+  let(:g_suite) { create(:g_suite) }
 
-  let(:g_suite) { g_suites(:g_suite1) }
-  let(:g_suite_id) { g_suite.id }
-
-  let(:mailer) { OperationsMailer.with(g_suite_id: g_suite_id).g_suite_entering_verifying_state }
+  let(:mailer) { OperationsMailer.with(g_suite_id: g_suite.id).g_suite_entering_verifying_state }
 
   it "renders to" do
     expect(mailer.to).to eql(["bank@hackclub.com"])
   end
 
   it "renders subject" do
-    expect(mailer.subject).to eql("[OPS] [ACTION] [GSuite] Verify event1.example.com")
+    expect(mailer.subject).to eql("[OPS] [ACTION] [Google Workspace] Verify #{g_suite.domain}")
   end
 
   it "includes verification url in body" do
-    expect(mailer.body).to include(g_suite.verification_url)
+    expect(mailer.body).to include(google_workspace_process_admin_url(g_suite))
   end
 end
