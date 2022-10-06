9cabadaeecd32774322f01151c61b5f0585f3e80
albertchae <albertchae@users.noreply.github.com>
2022-10-05 23:26:16 -0700

    Unskip g_suite_mailer_spec and convert fixtures to factories (#3067)
    https://github.com/hackclub/bank/issues/3008
    
    The main issue here is that the URL being checked didn't conform to the actual structure for the event's gsuite overview. With factories we have to make it more dynamic, but because the implementation is using the event_g_suite_overview_url helper, it'd be kind of a tautological test, so I checked for an expected URL built with File.join instead.
    
    
    * Unskip g_suite_mailer_spec and fix
    
    It looks like we are looking for the gsuite event URL, which is not the
    URL that was in the test
    
    * Convert fixtures to factories
    
    This required updating the URL to be more dynamic. We could have used
    the event_g_suite_overview_url helper but that's the same method the
    implementation uses so it'd be kind of a tautological test, so I checked
    for a URL built with File.join instead.

--- spec/mailers/g_suite_mailer_spec.rb
+++ spec/mailers/g_suite_mailer_spec.rb
@@ -2,43 +2,42 @@
 
 require "rails_helper"
 
-RSpec.describe GSuiteMailer, type: :mailer, skip: true do
-  fixtures "events", "users", "g_suites"
-
-  let(:g_suite) { g_suites(:g_suite1) }
-  let(:g_suite_id) { g_suite.id }
+RSpec.describe GSuiteMailer, type: :mailer do
+  let(:g_suite) { create(:g_suite) }
 
   let(:recipient) { "email@mailinator.com" }
 
   describe "#notify_of_configuring" do
-    let(:mailer) { GSuiteMailer.with(g_suite_id: g_suite_id, recipient: recipient).notify_of_configuring }
+    let(:mailer) { GSuiteMailer.with(g_suite_id: g_suite.id, recipient: recipient).notify_of_configuring }
 
     it "renders to" do
       expect(mailer.to).to eql([recipient])
     end
 
     it "renders subject" do
-      expect(mailer.subject).to eql("[Action Requested] Your Google Workspace for event1.example.com needs configuration")
+      expect(mailer.subject).to eql("[Action Requested] Your Google Workspace for #{g_suite.domain} needs configuration")
     end
 
     it "includes g suite overview url in body" do
-      expect(mailer.body).to include("http://example.com/event1/google_workspace")
+      g_suite_overview_url = File.join(root_url, g_suite.event.slug, "google_workspace")
+      expect(mailer.body).to include(g_suite_overview_url)
     end
   end
 
   describe "#notify_of_verified" do
-    let(:mailer) { GSuiteMailer.with(g_suite_id: g_suite_id, recipient: recipient).notify_of_verified }
+    let(:mailer) { GSuiteMailer.with(g_suite_id: g_suite.id, recipient: recipient).notify_of_verified }
 
     it "renders to" do
       expect(mailer.to).to eql([recipient])
     end
 
     it "renders subject" do
-      expect(mailer.subject).to eql("[Google Workspace Verified] Your Google Workspace for event1.example.com has been verified")
+      expect(mailer.subject).to eql("[Google Workspace Verified] Your Google Workspace for #{g_suite.domain} has been verified")
     end
 
     it "includes g suite overview url in body" do
-      expect(mailer.body).to include("http://example.com/event1/google_workspace")
+      g_suite_overview_url = File.join(root_url, g_suite.event.slug, "google_workspace")
+      expect(mailer.body).to include(g_suite_overview_url)
     end
   end
 end
