ddea190e86ab0d295a76bbeb51053f688ad69887
Tinu Vanapamula <tinuvanapamula@gmail.com>
2022-11-11 19:06:18 -0800

    Fix leak of invited user's email in Transparency mode (#3218)
    * parse email in user helper
    
    * Update app/helpers/users_helper.rb
    
    Co-authored-by: Gary Tou <gary@garytou.com>
    
    * Update app/helpers/users_helper.rb
    
    Co-authored-by: Gary Tou <gary@garytou.com>
    
    * update parsing and fix modifying hex
    
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,10 @@ require "cgi"
 
 module UsersHelper
   def gravatar_url(email, name, id, size)
-    name ||= email
+    name ||= begin
+      temp = email.split("@").first.split(/[^a-z\d]/i).compact_blank
+      temp.length == 1 ? temp.first.first(2) : temp.first(2).map(&:first).join
+    end
     hex = Digest::MD5.hexdigest(email.downcase.strip)
     "https://gravatar.com/avatar/#{hex}?s=#{size}&d=https%3A%2F%2Fui-avatars.com%2Fapi%2F/#{CGI.escape(name)}/#{size}/#{get_user_color(id)}/fff"
   end
