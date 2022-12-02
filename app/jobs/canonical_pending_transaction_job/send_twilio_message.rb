ac9a89629dafe0b48153893df600c12b48f49422
Max Wofford <max@maxwofford.com>
2022-12-02 11:56:48 -0500

    Only send SMS notifs on IRL charges (#3279)

--- app/jobs/canonical_pending_transaction_job/send_twilio_message.rb
+++ app/jobs/canonical_pending_transaction_job/send_twilio_message.rb
@@ -4,6 +4,10 @@ module CanonicalPendingTransactionJob
   class SendTwilioMessage < ApplicationJob
     include HcbCodeHelper # for attach_receipt_url
 
+    # This is a heuristic to avoid sending SMS for online charges. This isn't
+    # always correct.
+    IN_PERSON_AUTH_METHODS = %w[keyed_in swipe chip contactless].freeze
+
     def perform(cpt_id:, user_id:)
       @cpt = CanonicalPendingTransaction.find(cpt_id)
       @user = User.find(user_id)
@@ -12,10 +16,18 @@ module CanonicalPendingTransactionJob
       return unless Flipper.enabled?(:sms_receipt_notifications_2022_11_23, @user)
       # rubocop:enable Naming/VariableNumber
 
+      return unless IN_PERSON_AUTH_METHODS.include? auth_method
+
       hcb_code = @cpt.local_hcb_code
       message = "Your card was charged $#{@cpt.amount.abs} for '#{@cpt.memo}'. Plz upload it here: #{attach_receipt_url hcb_code}"
 
       TwilioMessageService::Send.new(@user, message, hcb_code: hcb_code).run!
+    end
+
+    private
+
+    def auth_method
+      @cpt&.raw_pending_stripe_transaction&.authorization_method
     end
 
   end
