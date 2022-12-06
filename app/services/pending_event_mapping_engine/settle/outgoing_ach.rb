01e132e9cefac647da4a6ffe62fafd7a1e93b837
Caleb Denio <cjdenio44@gmail.com>
2022-12-06 09:15:31 -0500

    Settle pending ACHs with HCB code (#3266)

--- app/services/pending_event_mapping_engine/settle/outgoing_ach.rb
+++ app/services/pending_event_mapping_engine/settle/outgoing_ach.rb
@@ -5,22 +5,10 @@ module PendingEventMappingEngine
     class OutgoingAch
       def run
         unsettled.find_each(batch_size: 100) do |cpt|
-          # 1. identify ach number
-          ach_transfer = cpt.ach_transfer
-          Airbrake.notify("AchTransfer not found for canonical pending transaction #{cpt.id}") unless ach_transfer
-          next unless ach_transfer
+          ct = cpt.local_hcb_code.canonical_transactions.first
 
-          event = ach_transfer.event
+          next unless ct
 
-          # 2. look up canonical - scoped to event for added accuracy
-          cts = event.canonical_transactions.where("memo ilike '%#{::TransactionEngine::SyntaxSugarService::Shared::OUTGOING_ACH_MEMO_PART}%' and amount_cents = #{cpt.amount_cents} and date >= ?", cpt.date).order("date asc")
-
-          next if cts.count < 1 # no match found yet. not processed.
-
-          Airbrake.notify("matched more than 1 canonical transaction for ach transfer #{ach_transfer.id}") if cts.count > 1
-          ct = cts.first
-
-          # 3. mark no longer pending
           CanonicalPendingTransactionService::Settle.new(
             canonical_transaction: ct,
             canonical_pending_transaction: cpt
