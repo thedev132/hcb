58e3f94d6d9dc5e8e1cb6e62c942b1d47c808741
Max Wofford <max@maxwofford.com>
2022-12-07 12:56:48 -0500

    Don't gate sms notifs to admins (#3293)

--- app/jobs/canonical_pending_transaction_job/send_twilio_message.rb
+++ app/jobs/canonical_pending_transaction_job/send_twilio_message.rb
@@ -19,7 +19,7 @@ module CanonicalPendingTransactionJob
       return unless IN_PERSON_AUTH_METHODS.include? auth_method
 
       hcb_code = @cpt.local_hcb_code
-      message = "Your card was charged $#{@cpt.amount.abs} for '#{@cpt.memo}'. Plz upload it here: #{attach_receipt_url hcb_code}"
+      message = "Your card was charged $#{@cpt.amount.abs} at '#{@cpt.memo}'. Upload your receipt: #{attach_receipt_url hcb_code}"
 
       TwilioMessageService::Send.new(@user, message, hcb_code: hcb_code).run!
     end
