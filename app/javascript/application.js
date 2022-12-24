15c2e2cf52b5ed3672a0919944a59bcba6ee1a13
Caleb Denio <cjdenio44@gmail.com>
2022-12-23 23:38:02 -0500

    Fix modals + React components inside Turbo Frames (#3344)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -22,6 +22,15 @@ ReactRailsUJS.useContext(componentRequireContext)
 ReactRailsUJS.handleEvent('turbo:load', ReactRailsUJS.handleMount)
 ReactRailsUJS.handleEvent('turbo:before-render', ReactRailsUJS.handleUnmount)
 
+ReactRailsUJS.handleEvent('turbo:frame-load', ReactRailsUJS.handleMount)
+ReactRailsUJS.handleEvent('turbo:frame-render', ReactRailsUJS.handleUnmount)
+
+// Remove modals triggered by <turbo-frames> when the frame is unloaded.
+// Bad stuff happens if you don't do this. Trust me. ~ @cjdenio
+document.addEventListener('turbo:frame-render', () => {
+  window.$('.jquery-modal [data-behavior~=modal].turbo-frame-modal').remove()
+})
+
 import './controllers'
 
 import { Turbo } from '@hotwired/turbo-rails'
