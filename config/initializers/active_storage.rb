656d92a72007e8b35dc9e1eef868bb4027eb99fb
albertchae <albertchae@users.noreply.github.com>
2023-01-01 10:48:58 -0800

    Configure ActiveStorage to automatically preview HEIC files as pngs (#3366)
    * Configure ActiveStorage to automatically preview HEIC files as pngs
    
    From https://guides.rubyonrails.org/active_storage_overview.html#transforming-images
    
    ```
    If a variant is requested, Active Storage will automatically apply transformations depending on the image's format:
    
        Content types that are variable (as dictated by config.active_storage.variable_content_types) and not considered web images (as dictated by config.active_storage.web_image_content_types), will be converted to PNG.
    ```
    
    `config.active_storage.web_image_content_types` only contains `["image/png", "image/jpeg", "image/jpg", "image/gif"]`, so by adding `"image/heic"` to `config.active_storage.variable_content_types`, we should trigger ActiveStorage's automatic conversion (using the image_processing gem, which was added several months ago in https://github.com/hackclub/bank/commit/ec1065c2d1c5cd6643c6e5b69fa32dfa97e08e6b)
    
    * Add buildpack for imagemagick with HEIC
    
    The default imagemagick on the heroku-20 stack is not compiled with HEIC
    support. There are several imagemagick with HEIC buildpacks in the
    heroku marketplace, so I used the same one recommended by
    https://mariochavez.io/desarrollo/2021/06/22/heic-support-active-storage/
    
    Also it seems to be associated with a reputable company.
    
    * Fix upload dialog to allow selecting heic files
    
    We need this explicitly because although `image/*` should handle all
    image MIME types
    https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept
    but as of today, heic support in browsers is non-existent
    https://caniuse.com/heif

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  # Whatever is added to variable_content_types but is also not in config.active_storage.web_image_content_types
+  # will be auto-converted to png by ActiveStorage
+  config.active_storage.variable_content_types << "image/heic"
+end
