d67134bfb9ccf95096f8e0d2b00932411136b890
Caleb Denio <cjdenio44@gmail.com>
2023-02-06 22:56:52 -0500

    Fix deletion requests for deleted events

--- app/models/organizer_position_deletion_request.rb
+++ app/models/organizer_position_deletion_request.rb
@@ -38,6 +38,7 @@ class OrganizerPositionDeletionRequest < ApplicationRecord
   belongs_to :submitted_by, class_name: "User"
   belongs_to :closed_by, class_name: "User", required: false
   belongs_to :organizer_position, with_deleted: true
+  has_one :event, through: :organizer_position
 
   scope :under_review, -> { where(closed_at: nil) }
 
