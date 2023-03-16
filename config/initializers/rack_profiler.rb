fe23d8a0b6985bb10440be1e9780112fadfdb94f
Max Wofford <max@maxwofford.com>
2023-03-16 14:00:06 -0400

    Use redis in production for rack mini profiler (#3618)

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -2,4 +2,9 @@
 
 if Rails.env.production?
   Rack::MiniProfiler.config.start_hidden = true
+
+  # We're using redis because we're running on multiple heroku dynos
+  # With the default file storage, we ran into https://github.com/hackclub/bank/issues/3617#issuecomment-1463316993
+  Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_URL"] }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 end
