d768c4b2029548b8b6d84dcd5a4c48364bb24ac3
Caleb Denio <cjdenio44@gmail.com>
2023-04-17 09:51:59 -0400

    fix donation refund button

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -108,7 +108,7 @@ class DonationsController < ApplicationController
     @donation = Donation.find(params[:id])
     @hcb_code = @donation.local_hcb_code
 
-    ::DonationJob::Refund.perform_later(@donation.id)
+    ::DonationService::Refund.new(donation_id: @donation.id).run
 
     redirect_to hcb_code_path(@hcb_code.hashid), flash: { success: "The refund process has been queued for this donation." }
   end
