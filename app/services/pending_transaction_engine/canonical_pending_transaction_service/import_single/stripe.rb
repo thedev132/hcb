62540d022e93b68618c75a4d8d8a2f2e0f2262a4
Caleb Denio <cjdenio44@gmail.com>
2023-08-08 14:51:37 -0400

    Fix error with Stripe webhook

--- app/services/pending_transaction_engine/canonical_pending_transaction_service/import_single/stripe.rb
+++ app/services/pending_transaction_engine/canonical_pending_transaction_service/import_single/stripe.rb
@@ -9,11 +9,15 @@ module PendingTransactionEngine
         end
 
         def run
-          CanonicalPendingTransaction.find_or_initialize_by(raw_pending_stripe_transaction: @raw_pending_stripe_transaction).tap do |cpt|
-            cpt.date = @raw_pending_stripe_transaction.date
-            cpt.memo = @raw_pending_stripe_transaction.memo
-            cpt.amount_cents = @raw_pending_stripe_transaction.amount_cents
-          end.save!
+          cpt = CanonicalPendingTransaction.find_or_initialize_by(raw_pending_stripe_transaction: @raw_pending_stripe_transaction)
+
+          cpt.date = @raw_pending_stripe_transaction.date
+          cpt.memo = @raw_pending_stripe_transaction.memo
+          cpt.amount_cents = @raw_pending_stripe_transaction.amount_cents
+
+          cpt.save!
+
+          return cpt
         end
 
       end
