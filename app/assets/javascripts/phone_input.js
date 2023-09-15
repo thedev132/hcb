9c51f9976758f1a569183a6f1bf24a589979a614
Sam Poder <sam.r.poder@gmail.com>
2023-09-15 10:08:49 -0700

    Expand country list at top of phone number input (#4244)
    Co-authored-by: Caleb Denio <cjdenio44@gmail.com>

--- app/assets/javascripts/phone_input.js
+++ app/assets/javascripts/phone_input.js
@@ -2,6 +2,7 @@
   const phoneInputField = document.querySelector('#phone_raw')
   const phoneInput = window.intlTelInput(phoneInputField, {
     initialCountry: 'us',
+    preferredCountries: ['us', 'in', 'ca', 'sg', 'au', 'gb'],
     utilsScript:
       'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js'
   })
