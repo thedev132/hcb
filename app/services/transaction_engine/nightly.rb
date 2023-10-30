e201730ec08e374761a507a938c8bfa3a5a5c6af
Caleb Denio <cjdenio44@gmail.com>
2023-10-30 11:30:52 -0400

    Patch for card overspending glitch (#4369)

--- app/services/transaction_engine/nightly.rb
+++ app/services/transaction_engine/nightly.rb
@@ -32,6 +32,10 @@ module TransactionEngine
 
       # 5 fix memo mistakes
       fix_memo_mistakes!
+
+      # temporary patch for https://github.com/hackclub/hcb/issues/4360
+      EventMappingEngine::Nightly.new.run
+      PendingEventMappingEngine::Nightly.new.run
     end
 
     private
