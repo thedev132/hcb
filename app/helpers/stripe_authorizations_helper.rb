a21531ee7f42621e055029228aed169d04ab2acb
Caleb Denio <cjdenio44@gmail.com>
2023-11-14 18:10:04 -0500

    Handle nil merchant city (#4461)
    Fixes #4460

--- app/helpers/stripe_authorizations_helper.rb
+++ app/helpers/stripe_authorizations_helper.rb
@@ -2,7 +2,7 @@
 
 module StripeAuthorizationsHelper
   def is_city_or_phone?(str)
-    return nil if str.empty?
+    return nil if str.blank?
 
     has_digits = str.scan(/\d/).any?
     has_digits ? :phone : :city
