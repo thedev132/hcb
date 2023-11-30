be56fe76ddbbada1029a2a5c83c5160e4547bc73
Sam Poder <sam.r.poder@gmail.com>
2023-11-30 12:57:41 -0800

    Enable Reply-to On Donation Notifications (#4520)
    ngl i just want to send ishan a meme
    
    <img width="778" alt="Screenshot 2023-11-26 at 10 20 39 PM"
    src="https://github.com/hackclub/hcb/assets/39828164/1c2ddf14-f036-4910-9b62-bead749ef286">

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -12,14 +12,14 @@ class DonationMailer < ApplicationMailer
     @donation = params[:donation]
     @emails = @donation.event.users.map { |u| u.email }
 
-    mail to: @emails, subject: "Congrats on receiving your first donation for #{@donation.event.name}! 🎉"
+    mail to: @emails, subject: "Congrats on receiving your first donation for #{@donation.event.name}! 🎉", reply_to: @donation.email
   end
 
   def donation_with_message_notification
     @donation = params[:donation]
     @emails = @donation.event.users.pluck(:email)
 
-    mail to: @emails, subject: "You've received a donation for #{@donation.event.name}! 🎉"
+    mail to: @emails, subject: "You've received a donation for #{@donation.event.name}! 🎉", reply_to: @donation.email
   end
 
 end
