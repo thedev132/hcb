882217e7f74ec7ff99042f37725ccd57404c769d
Caleb Denio <cjdenio44@gmail.com>
2023-12-01 15:07:53 -0500

    Add timezone to user_find API endpoint (#4587)

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -132,7 +132,8 @@ class ApiController < ApplicationController
       id: user.id,
       orgs: user.events.not_hidden.map { |e| { name: e.name, slug: e.slug, demo: e.demo_mode?, balance: e.balance_available } },
       card_count: user.stripe_cards.count,
-      recent_transactions:
+      recent_transactions:,
+      timezone: user.user_sessions.where.not(timezone: nil).order(created_at: :desc).first&.timezone,
     }
   end
 
