3c18b7caf7100c82dedd5a0498c4ffd22b145feb
Sam Poder <sam.r.poder@gmail.com>
2023-12-01 10:22:53 -0800

    Add WebP as a `web_image_content_type` (#4442)
    Based on
    https://edgeguides.rubyonrails.org/configuring.html#config-active-storage-web-image-content-types,
    I think this should fix #4441.

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -4,4 +4,5 @@ Rails.application.configure do
   # Whatever is added to variable_content_types but is also not in config.active_storage.web_image_content_types
   # will be auto-converted to png by ActiveStorage
   config.active_storage.variable_content_types << "image/heic"
+  config.active_storage.web_image_content_types << "image/webp"
 end
