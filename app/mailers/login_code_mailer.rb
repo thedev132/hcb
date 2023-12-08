e56c154186f2e6492c400933b5a9c5e43b8564e7
Caleb Denio <cjdenio44@gmail.com>
2023-12-08 15:16:27 -0500

    Set login code Reply-To to hcb@hackclub.com (#4658)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    When users reply to HCB login emails, their email gets sent to the
    entirety of Hack Club staff.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    Now they go to `hcb@hackclub.com`! I've kept the `From` address as
    login@hackclub.com for backwards compatibility with any email
    rules/filters people might have set up.
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/mailers/login_code_mailer.rb
+++ app/mailers/login_code_mailer.rb
@@ -7,7 +7,7 @@ class LoginCodeMailer < ApplicationMailer
     mail(to: email_address,
          subject: "Hack Club Login Code: #{@pretty_login_code}",
          from: "Hack Club <login@hackclub.com>",
-         reply_to: "Hack Club Team <team@hackclub.com>")
+         reply_to: "HCB <hcb@hackclub.com>")
   end
 
 end
