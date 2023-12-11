ba8eaa5e089b2822e53acf3d5aa0c17bac6cd02a
Sam Poder <sampoder@berkeley.edu>
2023-12-11 15:38:05 -0800

    Improve The Seed (#4624)
    Co-authored-by: sarthak mohanty ☻ <me@sarthakmohanty.me>
    Co-authored-by: Gary Tou <gary@garytou.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,153 @@
 user = User.first
 
 if user.nil?
-  puts "Woah there, there aren't any users! Please sign in first."
-else
-  puts "Continuing with #{user.email}..."
-
-  user.make_admin! if !user.admin?
-
-  event = Event.create_with(
-    name: "Test Org",
-    slug: "test",
-    can_front_balance: true,
-    point_of_contact: user,
-    sponsorship_fee: 0.07,
-    organization_identifier: "bank_#{SecureRandom.hex}",
-  ).find_or_create_by!(slug: "test")
-
-  OrganizerPositionInvite.create!(
-    event:,
-    user:,
-    sender: user,
-  )
-
-  puts "Done!"
+  puts "Woah there, there aren't any users! Creating an user (admin@example.com)."
+  user = User.create!(email: "admin@example.com")
 end
+
+puts "Continuing with #{user.email}..."
+
+user.make_admin! unless user.admin?
+
+demo_event = Event.create_with(
+  name: "DevHacks (Demo Event)",
+  slug: "devhacks",
+  can_front_balance: true,
+  point_of_contact: user,
+  sponsorship_fee: 0.07,
+  category: 4,
+  demo_mode: true,
+  organization_identifier: "bank_#{SecureRandom.hex}",
+).find_or_create_by!(slug: "devhacks")
+
+OrganizerPositionInvite.create!(
+  event: demo_event,
+  user:,
+  sender: user,
+)
+
+non_transparent_event = Event.create_with(
+  name: "ExpensiCon 2023 (Non-Transparent Event)",
+  slug: "expensicon23",
+  can_front_balance: true,
+  point_of_contact: user,
+  sponsorship_fee: 0.07,
+  category: 2,
+  organization_identifier: "bank_#{SecureRandom.hex}",
+).find_or_create_by!(slug: "expensicon23")
+
+OrganizerPositionInvite.create!(
+  event: non_transparent_event,
+  user:,
+  sender: user,
+)
+
+transparent_event = Event.create_with(
+  name: "Hack The Seas (Transparent Event)",
+  slug: "hack_the_seas",
+  can_front_balance: true,
+  point_of_contact: user,
+  sponsorship_fee: 0.00,
+  category: 7,
+  organization_identifier: "bank_#{SecureRandom.hex}",
+).find_or_create_by!(slug: "hack_the_seas")
+
+OrganizerPositionInvite.create!(
+  event: transparent_event,
+  user:,
+  sender: user,
+)
+
+# create incoming transactions for each org
+
+nte_non_pending_transaction = ::RawCsvTransactionService::Create.new(
+  unique_bank_identifier: "FSMAIN",
+  date: 3.days.ago.iso8601(3),
+  memo: "🏦 Donation from David Barrett",
+  amount: 8992898
+).run
+
+::TransactionEngine::HashedTransactionService::RawCsvTransaction::Import.new.run
+::TransactionEngine::CanonicalTransactionService::Import::All.new.run
+
+CanonicalEventMapping.create!({
+                                canonical_transaction_id: CanonicalTransaction.last.id,
+                                event_id: non_transparent_event.id,
+                                user_id: user.id
+                              })
+
+te_non_pending_transaction = ::RawCsvTransactionService::Create.new(
+  unique_bank_identifier: "FSMAIN",
+  date: 9.days.ago.iso8601(3),
+  memo: "🏦 Funding From HQ",
+  amount: 8100381
+).run
+
+::TransactionEngine::HashedTransactionService::RawCsvTransaction::Import.new.run
+::TransactionEngine::CanonicalTransactionService::Import::All.new.run
+
+CanonicalEventMapping.create!({
+                                canonical_transaction_id: CanonicalTransaction.last.id,
+                                event_id: transparent_event.id,
+                                user_id: user.id
+                              })
+
+# create non-pending transactions for each org
+
+nte_non_pending_transaction = ::RawCsvTransactionService::Create.new(
+  unique_bank_identifier: "FSMAIN",
+  date: 3.days.ago.iso8601(3),
+  memo: "🎤 George Clooney Speaking Fee",
+  amount: -892898
+).run
+
+::TransactionEngine::HashedTransactionService::RawCsvTransaction::Import.new.run
+::TransactionEngine::CanonicalTransactionService::Import::All.new.run
+
+CanonicalEventMapping.create!({
+                                canonical_transaction_id: CanonicalTransaction.last.id,
+                                event_id: non_transparent_event.id,
+                                user_id: user.id
+                              })
+
+te_non_pending_transaction = ::RawCsvTransactionService::Create.new(
+  unique_bank_identifier: "FSMAIN",
+  date: 9.days.ago.iso8601(3),
+  memo: "🚢 Cruise Ship Rental",
+  amount: -8181
+).run
+
+::TransactionEngine::HashedTransactionService::RawCsvTransaction::Import.new.run
+::TransactionEngine::CanonicalTransactionService::Import::All.new.run
+
+CanonicalEventMapping.create!({
+                                canonical_transaction_id: CanonicalTransaction.last.id,
+                                event_id: transparent_event.id,
+                                user_id: user.id
+                              })
+
+# create pending transactions for each org
+
+nte_non_pending_transaction = CanonicalPendingTransaction.create!(
+  date: 4.days.ago,
+  memo: "🍷 Wine, lots of wine.",
+  amount: -198614
+)
+
+CanonicalPendingEventMapping.create!({
+                                       canonical_pending_transaction_id: nte_non_pending_transaction.id,
+                                       event_id: non_transparent_event.id
+                                     })
+
+te_non_pending_transaction = CanonicalPendingTransaction.create!(
+  date: 1.days.ago,
+  memo: "📋 Overpriced Insurance Policy",
+  amount_cents: 140381,
+)
+
+CanonicalPendingEventMapping.create!({
+                                       canonical_pending_transaction_id: te_non_pending_transaction.id,
+                                       event_id: transparent_event.id
+                                     })
+
+puts "Done!"
