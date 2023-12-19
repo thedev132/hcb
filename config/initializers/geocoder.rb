f3b26a3cdb906fbc1195dbe6f7e9d38ae45db91d
Gary Tou <gary@garytou.com>
2023-12-18 17:33:22 -0800

    Configure Geocoder's Redis cache to work with Heroku (#4795)

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Geocoder.configure(
-  timeout: 2,
+  timeout: 15,
 
   ip_lookup: :ipinfo_io,
   ipinfo_io: {
     api_key: Rails.application.credentials.ipinfo_io[:api_key],
   },
 
-  cache: Redis.new,
+  cache: Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }),
   cache_options: {
     # ipinfo.io rate limits at 50,000/month with a free account (about 70
     # requests per hour)
