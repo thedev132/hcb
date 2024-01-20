1634a16292cc4f313e2f8a0c5dfc7191fcdf32e9
Caleb Denio <cjdenio44@gmail.com>
2024-01-20 17:32:11 -0500

    Limit money fields to 2 decimal places (#5077)
    Fixes #5052

---
+++ app/javascript/controllers/truncate_decimal_controller.js
@@ -0,0 +1,19 @@
+/*
+  Limits an <input type="number"> to a specified number of decimal places.
+*/
+
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static values = {
+    places: { type: Number, default: 2 },
+  }
+
+  truncate(e) {
+    const split = e.target.value.split('.')
+
+    if (split.length == 2 && split[1].length > this.placesValue) {
+      e.target.value = [split[0], split[1].slice(0, this.placesValue)].join('.')
+    }
+  }
+}
