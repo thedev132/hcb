43bf79ae7104fee6d76abf9172b620937ecbe5d7
Caleb Denio <cjdenio44@gmail.com>
2024-02-23 11:47:01 -0500

    API: improvements to invitations (#5335)

--- app/controllers/api/v4/invitations_controller.rb
+++ app/controllers/api/v4/invitations_controller.rb
@@ -32,6 +32,10 @@ module Api
 
       def set_invitation
         @invitation = authorize OrganizerPositionInvite.find_by_public_id(params[:id]) || OrganizerPositionInvite.friendly.find(params[:id])
+
+        if @invitation.cancelled? || @invitation.rejected? || @invitation.user != current_user
+          raise ActiveRecord::RecordNotFound
+        end
       end
 
     end
--- app/views/api/v4/invitations/_invitation.json.jbuilder
+++ app/views/api/v4/invitations/_invitation.json.jbuilder
@@ -1,4 +1,5 @@
 json.id invitation.public_id
 json.created_at invitation.created_at
+json.accepted invitation.accepted?
 json.sender { json.partial! "api/v4/users/user", user: invitation.sender }
 json.organization { json.partial! "api/v4/events/event", event: invitation.event }
