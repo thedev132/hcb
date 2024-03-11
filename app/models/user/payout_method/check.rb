1713fa1d4ea50a324ab4b2ececeaf464543bd399
Sam Poder <sampoder@berkeley.edu>
2024-03-11 01:49:12 -0700

    Add a default value for country on check payouts (#5439)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/models/user/payout_method/check.rb
+++ app/models/user/payout_method/check.rb
@@ -23,6 +23,7 @@ class User
       validates_presence_of :address_state, message: "Please select a state!"
       validates :address_state, inclusion: { in: ISO3166::Country["US"].states.keys, message: "This isn't a valid US state!", allow_blank: true }
       validates :address_postal_code, format: { with: /\A\d{5}(?:[-\s]\d{4})?\z/, message: "This isn't a valid ZIP code." }
+      attribute :address_country, :text, default: "US"
       def kind
         "check"
       end
