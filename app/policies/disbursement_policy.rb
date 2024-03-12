2a1c083febfb720b2c1bd50343d1a01fad2d492e
Ben <malted@malted.dev>
2024-03-12 00:37:42 +0000

    Fix disbursement policy checks (#5475)

--- app/policies/disbursement_policy.rb
+++ app/policies/disbursement_policy.rb
@@ -10,24 +10,16 @@ class DisbursementPolicy < ApplicationPolicy
   end
 
   def new?
-    return true if user.admin?
-
-    return false if !record.destination_event.nil? && !record.destination_event.users.include?(user)
-    return false if !record.source_event.nil? && !record.source_event.users.include?(user)
-    return false unless OrganizerPosition.find_by(user_id: user.id, event_id: record.source_event.id).manager?
-
-    return true
-
-
-    # FAIL if the destinaition is not null and the user is not in dest
-    # FAIL if the soruce  is not null AND the user is not in src
-    # FAIL if user is not manager
-
-    # user.admin? || (user_associated_with_events? && user_who_can_transfer?)
+    user&.admin? || (
+      (record.destination_event.nil? || record.destination_event.users.include?(user)) &&
+      (record.source_event?          || record.source_event.users.include?(user))
+    )
+    # return false unless OrganizerPosition.find_by(user_id: user.id, event_id: record.source_event.id).manager?
   end
 
   def create?
-    user.admin? || (!record.outernet_guild? && user_associated_with_events?) && user_who_can_transfer?
+    user&.admin? || (record.destination_event.users.include?(user) && record.source_event.users.include?(user))
+    # user.admin? || user_associated_with_events? && user_who_can_transfer?
   end
 
   def transfer_confirmation_letter?
@@ -62,11 +54,6 @@ class DisbursementPolicy < ApplicationPolicy
 
   def user_who_can_transfer?
     user&.admin? || EventPolicy.new(user, record.event).new_transfer?
-  end
-
-  def user_associated_with_events?
-    (record.nil? or record.users.include?(user)) and
-      Flipper.enabled?(:transfers_2022_04_21, user)
   end
 
 end
