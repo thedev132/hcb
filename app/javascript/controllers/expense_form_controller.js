8737f1388949d3e97eaa427ef972450760a7b33e
Sam Poder <sampoder@berkeley.edu>
2024-03-13 10:19:01 -0700

    Add a title to expenses (#5491)
    From the doc, "Hierarchy of information for expense":
    
    
    https://github.com/hackclub/hcb/assets/39828164/a81f1b4a-c805-4841-8197-d74cd6afea6a

--- app/javascript/controllers/expense_form_controller.js
+++ app/javascript/controllers/expense_form_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['field', 'button', 'form', 'move']
+  static targets = ['field', 'button', 'form', 'move', 'memo', 'memoInput']
   static values = {
     enabled: { type: Boolean, default: false },
     locked: { type: Boolean, default: false },
+    memo: { type: String, default: 'Untitled Expense' },
   }
 
   connect() {
@@ -25,12 +26,14 @@ export default class extends Controller {
 
     this.#buttons()
     this.#label()
+    this.#memo()
   }
 
   edit() {
     if (this.enabledValue || this.lockedValue) return
     this.enabledValue = true
 
+    this.#memo()
     this.#buttons()
     this.#label()
     this.moveTarget.style.display = 'none'
@@ -39,6 +42,11 @@ export default class extends Controller {
       field.readOnly = false
       this.#removeTooltip(field)
     }
+  }
+
+  updateMemo() {
+    this.memoValue = this.memoInputTarget.value
+    this.#memo()
   }
 
   #buttons() {
@@ -53,6 +61,16 @@ export default class extends Controller {
       this.enabledValue && !this.lockedValue
         ? 'Save edits'
         : 'Edit this expense'
+  }
+
+  #memo() {
+    this.memoTarget.innerText =
+      this.enabledValue && !this.lockedValue
+        ? `Editing: ${this.memoValue}`
+        : this.memoValue
+    if (this.enabledValue && !this.lockedValue) {
+      this.memoTarget.classList.remove('muted')
+    }
   }
 
   #addTooltip(field, label) {
