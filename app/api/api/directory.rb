e59529c571a897771dd0b878769b8d8e96bc7cfb
Caleb Denio <cjdenio44@gmail.com>
2024-03-15 15:29:30 -0400

    Climate Directory: allow `grant recipient` orgs (#5538)
    We have a few orgs classified as "grant recipients" that should still be
    able to be listed on the Climate Directory (flagged by Paul)

--- app/api/api/directory.rb
+++ app/api/api/directory.rb
@@ -15,15 +15,8 @@ module Api
         orgs = Event.indexable.includes(:event_tags) # Transparent organizations
                     .or(
                       # Tagged as okay to list in the Climate Directory
-                      Event.includes(:event_tags).where({ event_tags: { name: EventTag::Tags::CLIMATE, purpose: :directory } })
+                      Event.includes(:event_tags).where({ event_tags: { name: EventTag::Tags::CLIMATE, purpose: :directory } }),
                     )
-        disallowed_categories = [
-          "hardware grant",
-          "outernet guild",
-          "grant recipient",
-          "salary"
-        ]
-        orgs = orgs.where.not(category: disallowed_categories)
         orgs = Event.where(id: orgs.select(:id))
                     .includes(:event_tags)
                     .with_attached_logo.with_attached_background_image
