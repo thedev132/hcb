021aa75e2a3054d61e8acc0d7a3ebaffd3177657
yodalightsabr <yoda@yodacode.xyz>
2024-03-16 18:26:52 -0700

    Improvements to feedback form (#5600)
    ## Summary of the problem
    Feedback form doesn't give the engineering team much context on where it
    was used.
    
    ## Solution
    With this change, users can report bugs and other issues, and the
    engineering team will be able to see where they occurred.
    
    If the user chooses not to share their email, the request URL and the ID
    of the resource accessed will be hidden (the controller and method will
    still be shown).

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -180,13 +180,19 @@ class StaticPagesController < ApplicationController
 
   def feedback
     message = params[:message]
-    share_email = params[:share_email] || "1"
+    share_email = (params[:share_email] || "1") == "1"
+    url = share_email ? "#{request.base_url}#{params[:page_path]}" : ""
+
+    routing = Rails.application.routes.recognize_path(params[:page_path])
+    location = "#{routing[:controller]}##{routing[:action]} #{routing[:id] if routing[:id] && share_email}".strip
 
     feedback = {
       "Share your idea(s)" => message,
+      "URL"                => url,
+      "Location"           => location,
     }
 
-    if share_email == "1"
+    if share_email
       feedback["Name"] = current_user.name
       feedback["Email"] = current_user.email
       feedback["Organization"] = current_user.events.first&.name
