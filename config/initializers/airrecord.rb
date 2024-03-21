835613339feab24043b10f66afd3c6a83c6167f2
Sam Poder <sampoder@berkeley.edu>
2024-03-21 16:01:14 -0700

    Backfill Postal Codes (#5290)
    This works off of @manuthecoder's work in
    https://github.com/hackclub/hcb/pull/5191 to add a OneTimeJob we can use
    to backfill the data.
    
    Here's an example of an org with a postal code:
    https://hackclub.slack.com/files/USNPNJXNX/F06K7GM6DQE/screenshot_2024-02-18_at_1.45.56___pm.png.
    
    After running it locally with relatively recent data:
    
    <img width="576" alt="Screenshot 2024-02-18 at 1 51 47 PM"
    src="https://github.com/hackclub/hcb/assets/39828164/1cdf9056-c8eb-4582-b17c-272e233111b2">
    
    Which roughly matches
    
    <img width="585" alt="Screenshot 2024-02-18 at 1 53 57 PM"
    src="https://github.com/hackclub/hcb/assets/39828164/7d194dba-5a62-4bf0-bc5f-f2d7c9392e46">
    
    <img width="213" alt="Screenshot 2024-02-18 at 1 54 05 PM"
    src="https://github.com/hackclub/hcb/assets/39828164/8743ee6f-8ed8-414a-8076-403e50978ce1">
    
    ---------
    
    Co-authored-by: Manu G <manusvathgurudath@gmail.com>
    Co-authored-by: Gary Tou <gary@garytou.com>

--- config/initializers/airrecord.rb
+++ config/initializers/airrecord.rb
@@ -2,3 +2,4 @@
 
 Feedback = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "appEzv7w2IBMoxxHe", "tblOmqLjWtJZWXn4O")
 GWaitlistTable = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "appEzv7w2IBMoxxHe", "tbl9CkfZHKZYrXf1T")
+ApplicationsTable = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "apppALh5FEOKkhjLR", "tblctmRFEeluG4do7")
