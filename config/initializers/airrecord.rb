5ec865a8e3d30fa54f14d625425636206903559d
Sam Poder <sampoder@berkeley.edu>
2024-03-21 22:05:02 -0700

    Revert "Backfill Postal Codes (#5290)" (#5683)
    This reverts commit 835613339feab24043b10f66afd3c6a83c6167f2.
    
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- config/initializers/airrecord.rb
+++ config/initializers/airrecord.rb
@@ -2,4 +2,3 @@
 
 Feedback = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "appEzv7w2IBMoxxHe", "tblOmqLjWtJZWXn4O")
 GWaitlistTable = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "appEzv7w2IBMoxxHe", "tbl9CkfZHKZYrXf1T")
-ApplicationsTable = Airrecord.table(Rails.application.credentials.dig(:airtable, :pat), "apppALh5FEOKkhjLR", "tblctmRFEeluG4do7")
