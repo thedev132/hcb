009f2e7b3d1d3d74a3a4bf5029808920e8422f4c
Ben <malted@malted.dev>
2024-03-25 23:06:55 +0000

    Refactor disbursement create policy (#5485)
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/policies/disbursement_policy.rb
+++ app/policies/disbursement_policy.rb
@@ -14,12 +14,13 @@ class DisbursementPolicy < ApplicationPolicy
       (record.destination_event.nil? || record.destination_event.users.include?(user)) &&
       (record.source_event.nil?      || record.source_event.users.include?(user))
     )
-    # return false unless OrganizerPosition.find_by(user_id: user.id, event_id: record.source_event.id).manager?
   end
 
   def create?
-    user&.admin? || (record.destination_event.users.include?(user) && record.source_event.users.include?(user))
-    # user.admin? || user_associated_with_events? && user_who_can_transfer?
+    user&.admin? || (
+      record.destination_event.users.include?(user) &&
+      Pundit.policy(user, record.source_event).new_transfer?
+    )
   end
 
   def transfer_confirmation_letter?
@@ -50,10 +51,6 @@ class DisbursementPolicy < ApplicationPolicy
 
   def admin_or_user?
     user&.admin? || record.event.users.include?(user)
-  end
-
-  def user_who_can_transfer?
-    user&.admin? || EventPolicy.new(user, record.event).new_transfer?
   end
 
 end
