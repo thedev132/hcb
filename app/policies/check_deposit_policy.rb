522464d5234fda07a9d238e383e17a59e8a1ea91
Ben <malted@malted.dev>
2024-03-26 21:53:16 +0000

    [User Roles] Blur Check Deposit images for non-managers (#5743)
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/policies/check_deposit_policy.rb
+++ app/policies/check_deposit_policy.rb
@@ -9,10 +9,18 @@ class CheckDepositPolicy < ApplicationPolicy
     admin_or_user? && !record.event.demo_mode? && !record.event.outernet_guild?
   end
 
+  def view_image?
+    admin_or_manager?
+  end
+
   private
 
   def admin_or_user?
     user&.admin? || record.event.users.include?(user)
+  end
+
+  def admin_or_manager?
+    user&.admin? || OrganizerPosition.find_by(user:, event: record.event)&.manager?
   end
 
   def user_who_can_transfer?
