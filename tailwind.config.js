7163b56f61e40a5991820176c559e0e9f926d228
Caleb Denio <cjdenio44@gmail.com>
2024-04-11 11:36:13 -0400

    More Tailwind utilities + switch away from safelist (#5971)
    - replaced more basscss helpers with tailwind equivalents
    - switch `safelist` -> `content` and fix one layout bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // DO NOT UNCOMMENT YET— this will cause Tailwind to override some classes we already have
-  //
-  // content: [
-  //   './app/views/**/*.html.erb',
-  //   './app/helpers/**/*.rb',
-  //   './app/assets/stylesheets/**/*.css',
-  //   './app/javascript/**/*.js'
-  // ],
-  safelist: [
-    'relative',
-    'absolute',
-    'fixed',
-    'top-0',
-    'right-0',
-    'bottom-0',
-    'left-0',
-    'rounded-full',
-    'rounded-b',
-    'rounded-t',
-    'flex',
-    'inline-flex',
-    'flex-wrap',
-    'flex-none',
-    'flex-row',
-    'flex-col',
-    'align-top',
-    'align-middle',
-    'align-bottom',
-    'mx-auto',
-    'ml-auto',
-    'mr-auto',
-    'font-mono',
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
   ],
+  blocklist: ['container'],
   corePlugins: {
     preflight: false,
   },
