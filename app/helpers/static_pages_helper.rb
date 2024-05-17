3e89c9e61cb43252b2f1ba2ff34fe96811edceb1
Caleb Denio <cjdenio44@gmail.com>
2024-05-17 12:16:56 -0400

    Extract application form URL into helper (#6320)
    Before, we had 2 separate "New project" buttons, one of which didn't
    prefill all the same fields as the other.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -148,6 +148,10 @@ module StaticPagesHelper
     }
   end
 
+  def apply_form_url(user = current_user)
+    "https://hackclub.com/fiscal-sponsorship/apply/?#{URI.encode_www_form({ userEmail: user.email, firstName: user.first_name, lastName: user.last_name, userPhone: user.phone_number, userBirthday: user.birthday&.year }.compact)}"
+  end
+
   def render_permissions(permissions, depth = 0)
     capture do
       permissions.each_with_index do |(k, v), i|
