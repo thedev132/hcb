3183fc1a4f37e7b0dc679c79dc3c626698ff9885
Gary Tou <gary@garytou.com>
2024-05-23 17:30:12 -0700

    [Migrations] Comment out problematic data migrations (#6381)

--- db/migrate/20230714043346_create_event_tags_and_events.rb
+++ db/migrate/20230714043346_create_event_tags_and_events.rb
@@ -8,17 +8,20 @@ class CreateEventTagsAndEvents < ActiveRecord::Migration[7.0]
       t.index [:event_tag_id, :event_id], unique: true
     end
 
+    # [@garyhtou] These lines were later commented out, after running the
+    # migration in production, because they were causing an error.
+
     # Migrate data from old boolean columns to new EventTag model
-    hack_clubers = EventTag.find_or_initialize_by(name: EventTag::Tags::ORGANIZED_BY_HACK_CLUBBERS).save!(validate: false)
-    teenagers = EventTag.find_or_initialize_by(name: EventTag::Tags::ORGANIZED_BY_TEENAGERS).save!(validate: false)
-
-    Event.where(organized_by_hack_clubbers: true).find_each do |e|
-      e.event_tags << hack_clubers
-    end
-
-    Event.where(organized_by_teenagers: true).find_each do |e|
-      e.event_tags << teenagers
-    end
+    # hack_clubers = EventTag.find_or_initialize_by(name: EventTag::Tags::ORGANIZED_BY_HACK_CLUBBERS).save!(validate: false)
+    # teenagers = EventTag.find_or_initialize_by(name: EventTag::Tags::ORGANIZED_BY_TEENAGERS).save!(validate: false)
+    #
+    # Event.where(organized_by_hack_clubbers: true).find_each do |e|
+    #   e.event_tags << hack_clubers
+    # end
+    #
+    # Event.where(organized_by_teenagers: true).find_each do |e|
+    #   e.event_tags << teenagers
+    # end
 
   end
 
--- db/migrate/20230811160940_add_access_level_to_users.rb
+++ db/migrate/20230811160940_add_access_level_to_users.rb
@@ -4,11 +4,14 @@ class AddAccessLevelToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :access_level, :integer, default: 0, null: false
 
-    reversible do |dir|
-      dir.up do
-        User.update_all("access_level = CASE WHEN admin_at IS NOT NULL THEN 1 ELSE 0 END")
-      end
-    end
+    # [@garyhtou] These lines were later commented out after, running the
+    # migration in production, because they were causing an error.
+
+    # reversible do |dir|
+    #   dir.up do
+    #     User.update_all("access_level = CASE WHEN admin_at IS NOT NULL THEN 1 ELSE 0 END")
+    #   end
+    # end
   end
 
 end
