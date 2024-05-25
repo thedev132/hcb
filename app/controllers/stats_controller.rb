7f83e92552509019591c49c080cd4139dd77f8db
Sam Poder <sampoder@berkeley.edu>
2024-05-25 13:40:29 -0700

    Cache `/stats` (#6400)
    Cache it. Temporary fix. Tested locally, seems to work fine.
    
    ---------
    
    Co-authored-by: yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -26,54 +26,57 @@ class StatsController < ApplicationController
   end
 
   def stats
-    now = params[:date].present? ? Date.parse(params[:date]) : DateTime.current
-    year_ago = now - 1.year
-    qtr_ago = now - 3.months
-    month_ago = now - 1.month
-    week_ago = now - 1.week
+    json = Rails.cache.fetch("stats", expires_in: 20.minutes) do
+      now = params[:date].present? ? Date.parse(params[:date]) : DateTime.current
+      year_ago = now - 1.year
+      qtr_ago = now - 3.months
+      month_ago = now - 1.month
+      week_ago = now - 1.week
 
-    events_list = Event.not_omitted
-                       .where("created_at <= ?", now)
-                       .order(created_at: :desc)
-                       .limit(10)
-                       .pluck(:created_at)
-                       .map(&:to_i)
-                       .map { |time| { created_at: time } }
-
-    tx_all = CanonicalTransaction.where.not("hcb_code LIKE 'HCB-#{::TransactionGroupingEngine::Calculate::HcbCode::BANK_FEE_CODE}%'")
-                                 .included_in_stats
-                                 .where("date <= ?", now)
-
-    pending_tx_all = CanonicalPendingTransaction.where(raw_pending_bank_fee_transaction_id: nil)
-                                                .included_in_stats
-                                                .unsettled
-                                                .and(CanonicalPendingTransaction.outgoing.or(CanonicalPendingTransaction.fronted))
-
-    render json: {
-      date: now,
-      events_count: Event.not_omitted
-                         .not_hidden
-                         .not_demo_mode
-                         .approved
+      events_list = Event.not_omitted
                          .where("created_at <= ?", now)
-                         .count,
-      last_transaction_date: tx_all.order(:date).last.date.to_time.to_i,
+                         .order(created_at: :desc)
+                         .limit(10)
+                         .pluck(:created_at)
+                         .map(&:to_i)
+                         .map { |time| { created_at: time } }
 
-      # entire time period. this remains to prevent breaking changes to existing systems that use this endpoint
-      raised: tx_all.revenue.sum(:amount_cents) + pending_tx_all.incoming.sum(:amount_cents),
-      transactions_count: tx_all.size,
-      transactions_volume: tx_all.sum("@amount_cents") + pending_tx_all.sum("@amount_cents"),
+      tx_all = CanonicalTransaction.where.not("hcb_code LIKE 'HCB-#{::TransactionGroupingEngine::Calculate::HcbCode::BANK_FEE_CODE}%'")
+                                   .included_in_stats
+                                   .where("date <= ?", now)
 
-      # entire (all), year, quarter, and month time periods
-      all: CanonicalTransactionService::Stats::During.new.run,
-      last_year: CanonicalTransactionService::Stats::During.new(start_time: year_ago, end_time: now).run,
-      last_qtr: CanonicalTransactionService::Stats::During.new(start_time: qtr_ago, end_time: now).run,
-      last_month: CanonicalTransactionService::Stats::During.new(start_time: month_ago, end_time: now).run,
-      last_week: CanonicalTransactionService::Stats::During.new(start_time: week_ago, end_time: now).run,
+      pending_tx_all = CanonicalPendingTransaction.where(raw_pending_bank_fee_transaction_id: nil)
+                                                  .included_in_stats
+                                                  .unsettled
+                                                  .and(CanonicalPendingTransaction.outgoing.or(CanonicalPendingTransaction.fronted))
 
-      # events
-      events: events_list,
-    }
+      {
+        date: now,
+        events_count: Event.not_omitted
+                           .not_hidden
+                           .not_demo_mode
+                           .approved
+                           .where("created_at <= ?", now)
+                           .count,
+        last_transaction_date: tx_all.order(:date).last.date.to_time.to_i,
+
+        # entire time period. this remains to prevent breaking changes to existing systems that use this endpoint
+        raised: tx_all.revenue.sum(:amount_cents) + pending_tx_all.incoming.sum(:amount_cents),
+        transactions_count: tx_all.size,
+        transactions_volume: tx_all.sum("@amount_cents") + pending_tx_all.sum("@amount_cents"),
+
+        # entire (all), year, quarter, and month time periods
+        all: CanonicalTransactionService::Stats::During.new.run,
+        last_year: CanonicalTransactionService::Stats::During.new(start_time: year_ago, end_time: now).run,
+        last_qtr: CanonicalTransactionService::Stats::During.new(start_time: qtr_ago, end_time: now).run,
+        last_month: CanonicalTransactionService::Stats::During.new(start_time: month_ago, end_time: now).run,
+        last_week: CanonicalTransactionService::Stats::During.new(start_time: week_ago, end_time: now).run,
+
+        # events
+        events: events_list,
+      }
+    end
+    render json:
   end
 
   def admin_receipt_stats # secret api endpoint for the tv in the bank office
