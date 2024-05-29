35ab60e13900b6f07d8403c4787ccebbdaba9068
Sam Poder <sampoder@berkeley.edu>
2024-05-29 10:02:41 -0700

    Update the `for_user` scope to support `Event` recipients (#6454)

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -2,7 +2,9 @@
 
 class PublicActivity::Activity
   scope :for_user, ->(user) {
-    where("recipient_type = 'User' AND recipient_id = ?", user.id).or(where(event_id: user.events.pluck(:id)))
+    where(recipient_type: "User", recipient_id: user.id)
+      .or(where(event_id: user.events.pluck(:id)))
+      .or(where(recipient_type: "Event", recipient_id: user.events.pluck(:id)))
   }
 
 end
