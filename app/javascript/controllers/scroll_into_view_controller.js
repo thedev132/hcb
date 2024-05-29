c55faf9e33e4c0bab4f0f78e33c82a93d2098290
Caleb Denio <cjdenio44@gmail.com>
2024-05-29 11:53:43 -0400

    Reimbursement expense partial: use Stimulus for `scrollIntoView` (#6449)

--- app/javascript/controllers/scroll_into_view_controller.js
+++ app/javascript/controllers/scroll_into_view_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static values = {
+    alignment: { type: String, default: 'nearest' },
+  }
+
   connect() {
-    this.element.scrollIntoView({ inline: 'nearest', block: 'nearest' })
+    this.element.scrollIntoView({
+      inline: this.alignmentValue,
+      block: this.alignmentValue,
+    })
   }
 }
