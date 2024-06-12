213fe31b2c9f9342064d53b5d4c41fa4ab2dfe4d
Sam Poder <sampoder@berkeley.edu>
2024-06-11 20:42:23 -0700

    Make the activity log "live" and real-time (#6522)

---
+++ app/javascript/controllers/timestamp_controller.js
@@ -0,0 +1,66 @@
+import { Controller } from '@hotwired/stimulus'
+
+// example implementation: data-controller="timestamp" data-timestamp-target="label" data-time="<%= activity.created_at.to_i * 1000 %>"
+// to_i gets you seconds since epoch! JS needs milliseconds.
+
+export default class extends Controller {
+  static targets = ['label']
+  connect() {
+    setInterval(() => {
+      this.labelTarget.innerText = DateHelper.time_ago_in_words(
+        this.labelTarget.dataset.time
+      )
+    }, 60000)
+  }
+}
+
+// sourced from https://gist.github.com/deadkarma/1989808
+// thank you @deadkarma!
+
+let DateHelper = {
+  // Takes a timestamp and converts it to a relative time
+  // DateHelper.time_ago_in_words(1331079503000)
+  time_ago_in_words: function (from) {
+    return this.distance_of_time_in_words(new Date(), from)
+  },
+
+  distance_of_time_in_words: function (to, from) {
+    var distance_in_seconds = (to - from) / 1000
+    var distance_in_minutes = Math.floor(distance_in_seconds / 60)
+    var tense = distance_in_seconds < 0 ? ' from now' : ' ago'
+    distance_in_minutes = Math.abs(distance_in_minutes)
+    console.log(distance_in_minutes)
+    if (distance_in_minutes == 0) {
+      return 'less than a minute' + tense
+    }
+    if (distance_in_minutes == 1) {
+      return 'a minute' + tense
+    }
+    if (distance_in_minutes < 45) {
+      return distance_in_minutes + ' minutes' + tense
+    }
+    if (distance_in_minutes < 90) {
+      return 'about an hour' + tense
+    }
+    if (distance_in_minutes < 1440) {
+      return 'about ' + Math.floor(distance_in_minutes / 60) + ' hours' + tense
+    }
+    if (distance_in_minutes < 2880) {
+      return 'a day' + tense
+    }
+    if (distance_in_minutes < 43200) {
+      return Math.floor(distance_in_minutes / 1440) + ' days' + tense
+    }
+    if (distance_in_minutes < 86400) {
+      return 'about a month' + tense
+    }
+    if (distance_in_minutes < 525960) {
+      return Math.floor(distance_in_minutes / 43200) + ' months' + tense
+    }
+    if (distance_in_minutes < 1051199) {
+      return 'about a year' + tense
+    }
+
+    return 'over ' + Math.floor(distance_in_minutes / 525960) + ' years'
+  },
+}
--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -7,6 +7,43 @@ class PublicActivity::Activity
       .or(where(recipient_type: "Event", recipient_id: user.events.pluck(:id)))
   }
 
+  include Turbo::Broadcastable
+
+  # after_create_commit -> {
+  #  streams = []
+
+  #  if event_id
+  #    Event.find(event_id).users.each do |user|
+  #      streams << [user, "activities"]
+  #    end
+  #  end
+
+  #  if recipient.is_a?(User)
+  #    streams << [recipient, "activities"]
+  #  end
+
+  #  if recipient.is_a?(Event)
+  #    recipient.users.each do |user|
+  #      streams << [user, "activities"]
+  #    end
+  #  end
+
+  #  User.admin.each do |user|
+  #    streams << [user, "activities"]
+  #  end
+
+  #  streams.uniq.each do |stream|
+  #    broadcast_action_later_to(
+  #      stream,
+  #      action: :prepend,
+  #      target: "activities-1",
+  #      partial: "public_activity/activity",
+  #      locals: { activity: self, current_user: streams.first }
+  #    )
+
+  #  end
+  # }
+
   validate do
     owner.nil? || owner_type == User.name
   end
