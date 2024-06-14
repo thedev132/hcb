249e77388c06563ed5a895b8a93982281be91920
Ben <malted@malted.dev>
2024-06-14 18:52:48 -0400

    Add comments to JSON ledger export (#6645)
    Closes https://github.com/hackclub/hcb/issues/6627

--- app/services/export_service/json.rb
+++ app/services/export_service/json.rb
@@ -27,7 +27,8 @@ module ExportService
         date: ct.date,
         memo: ct.local_hcb_code.memo,
         amount_cents: @public_only && ct.likely_account_verification_related? ? 0 : ct.amount_cents,
-        tags: ct.local_hcb_code.tags.filter { |tag| tag.event_id == @event_id }.pluck(:label).join(", ")
+        tags: ct.local_hcb_code.tags.filter { |tag| tag.event_id == @event_id }.pluck(:label).join(", "),
+        comments: @public_only ? [] : ct.local_hcb_code.comments.not_admin_only.pluck(:content)
       }
     end
 
