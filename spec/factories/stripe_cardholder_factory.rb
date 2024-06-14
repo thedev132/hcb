0b4b775d4e995d99a93b28980144e997b1d03fb9
Sam Poder <sampoder@berkeley.edu>
2024-06-14 09:17:37 -0700

    Upgrade Stripe to 10.15.0 (#6722)
    No breaking changes associated with this release, see
    https://github.com/stripe/stripe-ruby/blob/master/CHANGELOG.md#900---2023-08-16.
    
    The next version has breaking changes so I'm going to handle that
    separately.

--- spec/factories/stripe_cardholder_factory.rb
+++ spec/factories/stripe_cardholder_factory.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :stripe_cardholder do
     association :user
 
+    stripe_id { "" }
     stripe_billing_address_line1 { "8605 Santa Monica Blvd #86294" }
     stripe_billing_address_city { "West Hollywood" }
     stripe_billing_address_state { "CA" }
