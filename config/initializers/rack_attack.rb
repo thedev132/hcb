406872aa321d0e30a157c75c43141f67b42b9f72
Sam Poder <sampoder@berkeley.edu>
2024-06-16 15:54:42 -0700

    Increase Rack Attack Limit to 1000 (#6762)

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -25,7 +25,7 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle("req/ip", limit: 600, period: 5.minutes) do |req|
+  throttle("req/ip", limit: 1000, period: 5.minutes) do |req|
     req.ip unless req.path.start_with?("/assets") ||
                   req.path.start_with?("/admin") ||
                   req.path.start_with?("/stats")
