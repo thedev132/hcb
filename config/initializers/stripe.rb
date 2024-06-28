f4fffa3a51b3e8ae4446233cc73be3e3670a7a20
Sam Poder <sampoder@berkeley.edu>
2024-06-28 13:06:54 -0700

    Revert to 2019-11-05 Stripe (#6974)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,5 @@
 stripe_environment = Rails.env.production? ? :live : :test
 
 # update as needed, we specify explicitly in code to avoid inter-branch API version conflicts
-Stripe.api_version = "2024-06-20"
+Stripe.api_version = "2019-11-05"
 Stripe.api_key = Rails.application.credentials.stripe[stripe_environment][:secret_key]
