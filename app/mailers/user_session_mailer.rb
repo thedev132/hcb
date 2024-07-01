8b0df905eb52ed65f83a1aac7df9df59e44f03a6
Caleb Denio <cjdenio44@gmail.com>
2024-07-01 19:38:50 -0400

    Patch `UserSessionMailer` (#7001)
    Fixes #7002

--- app/mailers/user_session_mailer.rb
+++ app/mailers/user_session_mailer.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class UserSessionMailer < ApplicationMailer
-  def new_login(session:)
-    @session = session
-    @user = session.user
+  def new_login(user_session:)
+    @session = user_session
+    @user = user_session.user
 
     mail to: @user.email, subject: "New login to your HCB account"
   end
--- spec/mailers/previews/user_session_mailer_preview.rb
+++ spec/mailers/previews/user_session_mailer_preview.rb
@@ -2,9 +2,9 @@
 
 class UserSessionMailerPreview < ActionMailer::Preview
   def new_login
-    session = UserSession.where.not(ip: "127.0.0.1").where.not(device_info: "").where.not(os_info: "").where.not(latitude: nil).last
+    user_session = UserSession.where.not(ip: "127.0.0.1").where.not(device_info: "").where.not(os_info: "").where.not(latitude: nil).last
 
-    UserSessionMailer.new_login(session:)
+    UserSessionMailer.new_login(user_session:)
   end
 
 end
