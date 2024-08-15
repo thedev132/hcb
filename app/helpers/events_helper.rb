135ea0c4959ac0665c1375e7d44009414b80c7e6
Ben <malted@malted.dev>
2024-08-14 20:54:59 -0700

    [Navbars] Make the dock item icons smaller (#7478)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,10 +10,10 @@ module EventsHelper
     ) do
       (content_tag :div, class: "line-height-0 relative" do
         if async_badge
-          inline_icon(icon, size: 32) +
+          inline_icon(icon, size: 24) +
           turbo_frame_tag(async_badge, src: async_badge, data: { controller: "cached-frame", action: "turbo:frame-render->cached-frame#cache" })
         else
-          inline_icon(icon, size: 32)
+          inline_icon(icon, size: 24)
         end
       end) + content_tag(:span, name.html_safe, class: "line-height-3")
     end
