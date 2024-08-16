202538af8102b459ad8d4267c8f3c0c6b529a9d9
Sam Poder <sampoder@berkeley.edu>
2024-08-15 20:17:03 -0700

    Send it to the author of a transaction if they have `my_threads` (#7549)

--- app/models/hcb_code.rb
+++ app/models/hcb_code.rb
@@ -490,6 +490,7 @@ class HcbCode < ApplicationRecord
     users += self.comments.map(&:user)
     users += self.comments.flat_map(&:mentioned_users)
     users += self.events.flat_map(&:users).reject(&:my_threads?)
+    users += [author] if author
 
     if comment.admin_only?
       users += self.events.map(&:point_of_contact)
