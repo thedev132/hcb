c2c61f785b1ca7e058c5b5338ac57aef3231eb13
Sam Poder <sampoder@berkeley.edu>
2024-08-21 18:31:50 -0600

    Handle successful disputes & automatically create payouts (#7612)
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -298,4 +298,27 @@ class StripeController < ActionController::Base
   alias_method :handle_issuing_personalization_design_rejected, :handle_issuing_personalization_design_updated
   alias_method :handle_issuing_personalization_design_deactivated, :handle_issuing_personalization_design_updated
 
+  def handle_issuing_dispute_funds_reinstated
+    dispute = event.data.object
+    transaction = Stripe::Issuing::Transaction.retrieve(dispute["transaction"])
+    hcb_code = RawPendingStripeTransaction.find_by!(stripe_transaction_id: transaction["authorization"]).canonical_pending_transaction.local_hcb_code
+    if dispute["status"] == "won" && dispute["currency"] == "USD"
+      StripeService::Payout.create(
+        amount: dispute["amount"],
+        currency: dispute["currency"],
+        statement_descriptor: "HCB-#{hcb_code.short_code}",
+        source_balance: "issuing",
+        metadata: {
+          dispute_id: dispute["id"],
+          authorization_id: transaction["authorization"],
+          hcb_code: hcb_code.hcb_code
+        }
+      )
+    elsif dispute["status"] != "won"
+      Airbrake.notify("Dispute with funds reinstated but without a win: #{dispute["id"]}")
+    elsif dispute["status"] != "USD"
+      Airbrake.notify("Dispute with funds reinstated but non-USD currency. Must be manually handled.")
+    end
+  end
+
 end
