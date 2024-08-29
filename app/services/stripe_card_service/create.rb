ba56371872c89572b8a6fee46ad57ab468a6fe36
Sam Poder <sampoder@berkeley.edu>
2024-08-29 14:18:13 -0700

    Set the `second_line` on Stripe Cards to the event's name (#7770)

--- app/services/stripe_card_service/create.rb
+++ app/services/stripe_card_service/create.rb
@@ -76,6 +76,7 @@ module StripeCardService
       attrs = {
         cardholder: stripe_cardholder.stripe_id,
         type: @card_type,
+        second_line: event.name,
         currency: "usd",
         status: "active",
         spending_controls: {
