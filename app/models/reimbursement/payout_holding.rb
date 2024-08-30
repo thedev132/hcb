5c07130b48706d1182d195a0d70393cc704e951c
Sam Poder <sampoder@berkeley.edu>
2024-08-30 01:16:50 -0700

    Unfront & decline CPTs associated with reversed `PayoutHolding`s (#7791)

--- app/models/reimbursement/payout_holding.rb
+++ app/models/reimbursement/payout_holding.rb
@@ -93,6 +93,9 @@ module Reimbursement
 
         mark_reversed!
 
+        canonical_pending_transaction.update(fronted: false)
+        canonical_pending_transaction.decline!
+
         # these are reversed because this is reverse!
         sender_bank_account_id = ColumnService::Accounts.id_of(book_transfer_receiving_account)
         receiver_bank_account_id = ColumnService::Accounts.id_of(book_transfer_originating_account)
