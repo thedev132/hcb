efcb28d06282394f94eea9680b653976e6f8d6e1
Vedant Singhal <88609765+Ved235@users.noreply.github.com>
2024-09-03 20:55:22 +0530

    Fix command bar guide colors (#7813)
    Co-authored-by: Sam Poder <sam.r.poder@gmail.com>

--- app/javascript/components/command_bar/search/guide.js
+++ app/javascript/components/command_bar/search/guide.js
@@ -5,74 +5,76 @@ import React from 'react'
 export function Guide() {
   return (
     <>
-      Use the{' '}
-      <kbd
-        style={{
-          borderRadius: '3px',
-          backgroundColor: 'var(--command-bar-user)',
-          outline: '2px solid var(--command-bar-user)',
-          paddingLeft: '3px',
-          paddingRight: '3px',
-          marginRight: '1.5px',
-          color: 'black',
-        }}
-      >
-        @user
-      </kbd>
-      ,{' '}
-      <kbd
-        style={{
-          borderRadius: '3px',
-          backgroundColor: 'var(--command-bar-transaction)',
-          outline: '2px solid var(--command-bar-transaction)',
-          paddingLeft: '3px',
-          paddingRight: '3px',
-          marginRight: '1.5px',
-          color: 'black',
-        }}
-      >
-        @transaction
-      </kbd>
-      ,{' '}
-      <kbd
-        style={{
-          borderRadius: '3px',
-          backgroundColor: 'var(--command-bar-organization)',
-          outline: '2px solid var(--command-bar-organization)',
-          paddingLeft: '3px',
-          paddingRight: '3px',
-          color: 'black',
-        }}
-      >
-        @organization
-      </kbd>
-      ,{' '}
-      <kbd
-        style={{
-          borderRadius: '3px',
-          backgroundColor: 'var(--command-bar-reimbursement)',
-          outline: '2px solid var(--command-bar-reimbursement)',
-          paddingLeft: '3px',
-          paddingRight: '3px',
-          color: 'black',
-        }}
-      >
-        @reimbursement
-      </kbd>{' '}
-      and{' '}
-      <kbd
-        style={{
-          borderRadius: '3px',
-          backgroundColor: 'var(--command-bar-card)',
-          outline: '2px solid var(--command-bar-card)',
-          paddingLeft: '3px',
-          paddingRight: '3px',
-          color: 'black',
-        }}
-      >
-        @card
-      </kbd>{' '}
-      tags to narrow down your search. To search, click enter.
+      <span style={{ color: 'var(--kbar-foreground)' }}>
+        Use the{' '}
+        <kbd
+          style={{
+            borderRadius: '3px',
+            backgroundColor: 'var(--command-bar-user)',
+            outline: '2px solid var(--command-bar-user)',
+            paddingLeft: '3px',
+            paddingRight: '3px',
+            marginRight: '1.5px',
+            color: 'black',
+          }}
+        >
+          @user
+        </kbd>
+        ,{' '}
+        <kbd
+          style={{
+            borderRadius: '3px',
+            backgroundColor: 'var(--command-bar-transaction)',
+            outline: '2px solid var(--command-bar-transaction)',
+            paddingLeft: '3px',
+            paddingRight: '3px',
+            marginRight: '1.5px',
+            color: 'black',
+          }}
+        >
+          @transaction
+        </kbd>
+        ,{' '}
+        <kbd
+          style={{
+            borderRadius: '3px',
+            backgroundColor: 'var(--command-bar-organization)',
+            outline: '2px solid var(--command-bar-organization)',
+            paddingLeft: '3px',
+            paddingRight: '3px',
+            color: 'black',
+          }}
+        >
+          @organization
+        </kbd>
+        ,{' '}
+        <kbd
+          style={{
+            borderRadius: '3px',
+            backgroundColor: 'var(--command-bar-reimbursement)',
+            outline: '2px solid var(--command-bar-reimbursement)',
+            paddingLeft: '3px',
+            paddingRight: '3px',
+            color: 'black',
+          }}
+        >
+          @reimbursement
+        </kbd>{' '}
+        and{' '}
+        <kbd
+          style={{
+            borderRadius: '3px',
+            backgroundColor: 'var(--command-bar-card)',
+            outline: '2px solid var(--command-bar-card)',
+            paddingLeft: '3px',
+            paddingRight: '3px',
+            color: 'black',
+          }}
+        >
+          @card
+        </kbd>{' '}
+        tags to narrow down your search. To search, click enter.
+      </span>
     </>
   )
 }
