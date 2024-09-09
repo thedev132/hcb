9da0bac0ba7d658ecb10988f5a25c99c3b9166db
Sam Poder <sampoder@berkeley.edu>
2024-09-09 14:01:20 -0700

    Fix ledger audits (#7864)

--- app/models/admin/ledger_audit.rb
+++ app/models/admin/ledger_audit.rb
@@ -14,7 +14,7 @@ module Admin
   class LedgerAudit < ApplicationRecord
     self.table_name = "admin_ledger_audits"
     has_many :admin_ledger_audit_tasks, class_name: "Admin::LedgerAudit::Task", foreign_key: "admin_ledger_audit_id", inverse_of: "admin_ledger_audit"
-    alias_attribute :tasks, :admin_ledger_audit_tasks
+    alias_method :tasks, :admin_ledger_audit_tasks
     has_many :hcb_codes, through: :admin_ledger_audit_tasks
     scope :pending, -> { joins(:admin_ledger_audit_tasks).where("admin_ledger_audit_tasks.status" => "pending").distinct }
 
