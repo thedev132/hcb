ff2c135b3eaf08d52b8273c0383a2c9b8a1c93a2
Sam Poder <sampoder@berkeley.edu>
2024-09-22 18:54:49 -0700

    [Donations] Skip verify authorized for Stripe connection key (#7971)

--- app/controllers/api/v4/stripe_terminal_controller.rb
+++ app/controllers/api/v4/stripe_terminal_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V4
     class StripeTerminalController < ApplicationController
+      skip_after_action :verify_authorized, only: [:connection_token]
+
       def connection_token
         render json: {
           terminal_connection_token: Stripe::Terminal::ConnectionToken.create
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_202224) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_234449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
