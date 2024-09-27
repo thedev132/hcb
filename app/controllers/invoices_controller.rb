9e7a1a776d705c49fa1a85563050edee7dfc7d85
Sam Poder <sampoder@berkeley.edu>
2024-09-27 10:50:47 -0700

    Fix typo in invoices controller (#8015)

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -220,7 +220,7 @@ class InvoicesController < ApplicationController
 
     authorize @invoice
 
-    if invoice.canonical_transactions.any?
+    if @invoice.canonical_transactions.any?
       ::InvoiceService::Refund.new(invoice_id: @invoice.id, amount: Monetize.parse(params[:amount]).cents).run
       redirect_to hcb_code_path(@hcb_code.hashid), flash: { success: "The refund process has been queued for this invoice." }
     else
