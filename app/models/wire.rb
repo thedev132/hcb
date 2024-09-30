88820fb391f400b845ce749452fa4f785b2d7c5e
Sam Poder <sampoder@berkeley.edu>
2024-09-29 17:22:11 -0700

    Extend the country-specific fields on wires (#8020)

--- app/models/wire.rb
+++ app/models/wire.rb
@@ -149,13 +149,129 @@ class Wire < ApplicationRecord
   def self.information_required_for(country) # country can be null, in which case, only the general fields will be returned.
     fields = []
     case country
-    when "IN"
-      fields << { type: :text_field, key: "ifsc_code", label: "IFSC code", description: "An Indian Financial System Code is the 11 digit character that you can usually see on an Indian bank’s cheque leaves." }
-    when "MX"
-      fields << { type: :text_field, key: "clabe_code", label: "CLABE code", description: "18-digit standard for bank account numbers in Mexico." }
+    when "BR"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+      fields << { type: :text_field, key: "email", label: "Email address associated with account" }
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "11-digit CPF for individuals, or 14-digit CNPJ for corporations/NGO/organizations" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "CL"
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "9-digit RUT tax ID" }
+    when "CO"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+      fields << { type: :text_field, key: "email", label: "Email address associated with account" }
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "7-11 digits Cédulas for individuals, or 10-digit NIT for corporations/NGO/organizations" }
+    when "DO"
+      fields << { type: :text_field, key: "account_type", label: "Account type" }
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "11-digit Cedula or passport number for individuals, or 7+ digits tax ID or 9+ digits Registro Mercantil for corporations/NGO/organizations" }
+    when "HN"
+      fields << { type: :text_field, key: "account_type", label: "Account type" }
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "13-digit Tarjeta de Identidad for individuals, or 14-digit Registro Tributario Nacional for corporations/NGO/organizations" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "For payments from corporations/organizations to individuals, include a detailed purpose of payment (especially for salaries)" }
+    when "KZ"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "12-digit Business Identification Number (BIN) or Individual Identification Number (IIN)" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "MD"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "MY"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+    when "PK"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "Should be prepended with CNIC, SNIC, Passport, or NTN depending on the ID type." }
+    when "PY"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "Cedula de Identidad for individuals, or RUC for corporations" }
+    when "AM"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "AR"
+      fields << { type: :text_field, key: "email", label: "Email address associated with account" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "11-digit CUIL for individuals, or 11-digit CUIT for corporations" }
+    when "AE"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "AL"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "For utility payments: client name, bill month, and contract number of the subscriber. For tax payments: FDP (payment order document generated by Tax Office system). For fee payments: NIPT (tax ID)." }
+    when "AU"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "6-digit BSB code" }
+    when "AZ"
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "10-digit TIN/VOEN" }
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "6-digit BIK code" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified, especially for charitable purposes to avoid income tax" }
+    when "BA"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "If the beneficiary belongs to a government organization, Budget Organization Code, Profit Type (6-digit) and Citation Number (municipality, 3-digit) are required" }
+    when "BG"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "For tax payments: 6-digit payment type defined by the Ministry of Finance and local regulation, and one of the following: BULSTAT (Bulgarian Identification Tax Number, 6-digit for corporations), EGN (Bulgarian citizen ID), PNF (foreign citizen ID), or IZL (name of legal entity or individual)" }
+    when "BD"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "9-digit bank routing code" }
+    when "BY"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "3-9 digits MFO bank code" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "Tax ID" }
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "BZ"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "5-digit branch code" }
+    when "BS"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Transit Number is required if the beneficiary bank is RBC Bahamas" }
     when "CA"
-      # TBC, waiting for Mel atm!
-      fields << { type: :text_area, key: "instructions", label: "Country-specific instructions", description: "Use this space to include specific details required to send to this country." }
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "9-digit Canadian Payments Association Routing Number" }
+    when "CM"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "CR"
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "9-12 digits Cedula Juridica" }
+      fields << { type: :text_field, key: "local_account_number", label: "Local account number", description: "17-digit Cuenta Cliente" }
+    when "DZ"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "For invoices: reason for the invoice (e.g., invoice for health services). Otherwise provide a general reason for payment." }
+    when "GY"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "8-digit Transit Code is mandatory (format: TRANSIT CODE: XXXXXXXX). Funds paid to the Guyana Revenue Authority requires a reference (format: YYMMDD/RRRRRRRRRRRR), which can be obtained from the Guyana Revenue Authority." }
+    when "ID"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "IN"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "11-character IFSC codes" }
+    when "JO"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "KG"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "6-digit BIK code" }
+    when "KR"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "10-digit Business Registration Number (for corporations)" }
+    when "MU"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "MX"
+      fields << { type: :text_field, key: "clabe_code", label: "CLABE code", description: "18-digit standard for bank account numbers in Mexico" }
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "Nostro Account Number" }
+    when "MZ"
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "9-digit NUIT: Taxpayer Single ID Number" }
+    when "NE"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "NP"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "9-digit Permanent Account Number (PAN) is required for (i) payments related to social media content and software development by individuals or corporations or equivalent and (ii) payments related to any consultancy services would apply to individual only." }
+    when "NZ"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "6-digit NZ Clearing Code" }
+    when "PE"
+      fields << { type: :text_field, key: "legal_type", label: "Legal status of receiving entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "11-digit RUC number for corporations, or 8-digit DNI for individuals" }
+    when "TG"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "TW"
+      fields << { type: :text_field, key: "phone", label: "Phone number associated with account" }
+    when "UA"
+      fields << { type: :text_field, key: "type", label: "Type of entity" }
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "10-digit tax ID for individuals, or 8-digit tax ID for corporations/NGO/organizations" }
+    when "UG"
+      fields << { type: :text_field, key: "legal_id", label: "Legal ID of receiving entity", description: "13-digit PRN tax ID" }
+    when "SA"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "Payment purpose must be clearly identified" }
+    when "ZM"
+      fields << { type: :text_field, key: "local_bank_code", label: "Local bank code", description: "6-digit branch code" }
+    when "ZA"
+      fields << { type: :text_area, key: "remittance_info", label: "Remittance information", description: "For Tax payments, include unique 19 character Payment Reference Number(PRN) (e.g., /PRN/xxxxxxxxxxxxxxxxxxx)" }
     else
       fields << { type: :text_area, key: "instructions", label: "Country-specific instructions", description: "Use this space to include specific details required to send to this country." }
     end
