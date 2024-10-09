f39ac4db8d7273051fa947398b5e5454b264bf0d
Gary Tou <gary@garytou.com>
2024-10-08 18:11:15 -0700

    [Google Workspace] Refactor DNS checks in controller to service (#8106)
    Co-authored-by: rluo.dev <34289582+rluodev@users.noreply.github.com>

--- app/services/g_suite_service/verify.rb
+++ app/services/g_suite_service/verify.rb
@@ -13,9 +13,23 @@ module GSuiteService
     end
 
     def run
-      unless mx_valid? && spf_valid? && verification_key_valid?
+      unless mx_primary_valid? && spf_valid? && verification_key_valid?
         g_suite.mark_verification_error!
       end
+    end
+
+    def results(skip_g_verify: false)
+      {
+        mx_primary: mx_primary_valid?,
+        mx_alt1: mx_alt1_valid?,
+        mx_alt2: mx_alt2_valid?,
+        mx_alt3: mx_alt3_valid?,
+        mx_alt4: mx_alt4_valid?,
+        spf: spf_valid?,
+        verification_key: verification_key_valid?(skip_g_verify:),
+        dkim: dkim_valid?,
+        dmarc: dmarc_valid?,
+      }
     end
 
     private
@@ -28,9 +42,18 @@ module GSuiteService
       g_suite.domain
     end
 
-    def mx_valid?
+    def mx_primary_valid?
       check_dns Resolv::DNS::Resource::IN::MX do |records|
         return records.map(&:exchange).map(&:to_s).map(&:downcase).include?(VALID_MX)
+      end
+    end
+
+    (1..4).each do |i|
+      domain = "alt#{i}.aspmx.l.google.com"
+      define_method "mx_alt#{i}_valid?" do
+        check_dns Resolv::DNS::Resource::IN::MX do |records|
+          return records.map(&:exchange).map(&:to_s).map(&:downcase).include?(domain)
+        end
       end
     end
 
@@ -42,13 +65,33 @@ module GSuiteService
       end
     end
 
-    def verification_key_valid?
-      res = Faraday.get do |req|
-        req.url G_VERIFY_DOMAIN + domain
-        req.options.timeout = 60 # it may take heroku up to 30 seconds to cold start
-        req.headers["Authorization"] = Rails.application.credentials.g_verify_api_key
+    def dkim_valid?
+      return nil if g_suite.dkim_key.blank?
+
+      check_dns Resolv::DNS::Resource::IN::TXT do |records|
+        return records.map(&:data).any? do |record|
+          record.include?(g_suite.dkim_key)
+        end
       end
-      return true if res.success?
+    end
+
+    def dmarc_valid?
+      check_dns Resolv::DNS::Resource::IN::TXT do |records|
+        return records.map(&:data).any? do |record|
+          record.include?("v=DMARC1;")
+        end
+      end
+    end
+
+    def verification_key_valid?(skip_g_verify: false)
+      unless skip_g_verify
+        res = Faraday.get do |req|
+          req.url G_VERIFY_DOMAIN + domain
+          req.options.timeout = 60 # it may take heroku up to 30 seconds to cold start
+          req.headers["Authorization"] = Rails.application.credentials.g_verify_api_key
+        end
+        return true if res.success?
+      end
 
       # Some old G Suite domains did not have their verification key generated
       # using G-Verify. This means that G-Verify is unable to successfully
