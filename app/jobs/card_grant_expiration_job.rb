e549e85ecf3ff32053cd8df2046dd510d21cb7c8
rluo.dev <34289582+rluodev@users.noreply.github.com>
2024-10-15 15:26:22 -0400

    [Card Grant] Only attempt to expire active card grants (#8152)

--- app/jobs/card_grant_expiration_job.rb
+++ app/jobs/card_grant_expiration_job.rb
@@ -3,7 +3,7 @@
 class CardGrantExpirationJob < ApplicationJob
   queue_as :low
   def perform
-    CardGrant.joins(:card_grant_setting).where("card_grants.created_at + (card_grant_settings.expiration_preference * interval '1 day') < NOW()").find_each do |card_grant|
+    CardGrant.joins(:card_grant_setting).active.where("card_grants.created_at + (card_grant_settings.expiration_preference * interval '1 day') < NOW()").find_each do |card_grant|
       card_grant.expire!
     end
   end
