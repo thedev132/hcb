52244faed5b602548135866299c59cb334189c8b
Sam Poder <sampoder@berkeley.edu>
2024-10-29 22:25:43 -0700

    Create `Event::Plan::SalaryAccount` (#8371)

---
+++ app/models/event/plan/salary_account.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: event_plans
+#
+#  id         :bigint           not null, primary key
+#  aasm_state :string
+#  plan_type  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#
+# Indexes
+#
+#  index_event_plans_on_event_id  (event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#
+class Event
+  class Plan
+    class SalaryAccount < FeeWaived
+      def label
+        "salary account"
+      end
+
+    end
+
+  end
+
+end
