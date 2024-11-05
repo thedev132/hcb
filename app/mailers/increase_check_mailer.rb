16a8dc7777b4d51284f4e037f7cab38fc4b189c9
Manu G <manusvathgurudath@gmail.com>
2024-11-04 22:41:50 -0800

    [IncreaseCheckMailer] Set reply_to for notify_recipient and remind_recipient (#8355) (#8420)
    ### See [`#8355`](https://github.com/hackclub/hcb/issues/8355)
    
    This pull request includes updates to the `IncreaseCheckMailer` class to
    enhance the email functionalities by adding a `reply_to` field.
    
    Enhancements to email functionalities:
    
    *
    [`app/mailers/increase_check_mailer.rb`](diffhunk://#diff-399bdfa7e2f4380cb926af7b71650f42a1529605c4b36ea60a73b4056b785340L7-R19):
    Added a `reply_to` field in the `notify_recipient` and
    `remind_recipient` methods to include the email addresses of event
    organizers with the role of manager.
    
    ---------
    
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/mailers/increase_check_mailer.rb
+++ app/mailers/increase_check_mailer.rb
@@ -4,13 +4,19 @@ class IncreaseCheckMailer < ApplicationMailer
   def notify_recipient
     @check = params[:check]
 
-    mail to: @check.recipient_email, subject: "Your check from #{@check.event.name} is in transit", from: email_address_with_name("hcb@hackclub.com", "#{@check.event.name} via HCB")
+    mail to: @check.recipient_email,
+         subject: "Your check from #{@check.event.name} is in transit",
+         from: email_address_with_name("hcb@hackclub.com", "#{@check.event.name} via HCB"),
+         reply_to: @check.event.organizer_positions.where(role: :manager).includes(:user).map(&:user).map(&:email_address_with_name)
   end
 
   def remind_recipient
     @check = params[:check]
 
-    mail to: @check.recipient_email, subject: "[Action Required] You haven't deposited your check from #{@check.event.name}", from: email_address_with_name("hcb@hackclub.com", "#{@check.event.name} via HCB")
+    mail to: @check.recipient_email,
+         subject: "[Action Required] You haven't deposited your check from #{@check.event.name}",
+         from: email_address_with_name("hcb@hackclub.com", "#{@check.event.name} via HCB"),
+         reply_to: @check.event.organizer_positions.where(role: :manager).includes(:user).map(&:user).map(&:email_address_with_name)
   end
 
 end
