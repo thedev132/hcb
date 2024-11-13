200420bb3280705a19a4f58d6391d2c756084277
Sam Poder <sampoder@berkeley.edu>
2024-11-12 21:30:15 -0800

    [YellowPages] Use `missing_merchant_reporter` (#8503)
    Closes https://github.com/hackclub/hcb/issues/8088

--- config/initializers/yellow_pages.rb
+++ config/initializers/yellow_pages.rb
@@ -2,7 +2,6 @@
 
 # when we added icons to the homepage, this became pretty costly.
 
-# YellowPages.missing_merchant_reporter = ->(network_id) do
-#  ahoy = Ahoy::Tracker.new
-#  ahoy.track("Merchant Network ID not found", network_id:)
-# end
+YellowPages.missing_merchant_reporter = ->(network_id) do
+  StatsD.event("MerchantNotFound", network_id)
+end
