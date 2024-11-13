90bd7afa5b4b466fc5b4a0b39a456e09d5fa1b84
Sam Poder <sampoder@berkeley.edu>
2024-11-12 23:48:12 -0800

    Drop `plan_type` on `Event::Plan` (#8509)
    Only merge after the ignore column PR has been in production for a bit
    
    Closes https://github.com/hackclub/hcb/issues/8482

--- app/models/event/plan/standard.rb
+++ app/models/event/plan/standard.rb
@@ -7,7 +7,6 @@
 #  id          :bigint           not null, primary key
 #  aasm_state  :string
 #  inactive_at :datetime
-#  plan_type   :string
 #  type        :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_055749) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_13_064317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
@@ -822,7 +822,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_055749) do
   end
 
   create_table "event_plans", force: :cascade do |t|
-    t.string "plan_type"
     t.string "aasm_state"
     t.bigint "event_id", null: false
     t.datetime "created_at", null: false
