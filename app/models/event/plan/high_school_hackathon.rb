28674947e42c6f407f50ae1c70c23ab5443e8fea
Sam Poder <sampoder@berkeley.edu>
2024-11-12 19:21:59 -0800

    Create a `HighSchoolHackathon` plan (#8495)

---
+++ app/models/event/plan/high_school_hackathon.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: event_plans
+#
+#  id          :bigint           not null, primary key
+#  aasm_state  :string
+#  inactive_at :datetime
+#  plan_type   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  event_id    :bigint           not null
+#
+# Indexes
+#
+#  index_event_plans_on_event_id  (event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#
+class Event
+  class Plan
+    class HighSchoolHackathon < FeeWaived
+      def label
+        "high school hackathon"
+      end
+
+    end
+
+  end
+
+end
