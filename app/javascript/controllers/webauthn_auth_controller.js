7a3c06491cadf2dad449b982a3297429d14da4c5
Sam Poder <sampoder@berkeley.edu>
2024-11-26 13:06:29 -0800

    Add error logging to Webauthn JS controller (#8695)
    Impossible to debug production-only bugs without this.

--- app/javascript/controllers/webauthn_auth_controller.js
+++ app/javascript/controllers/webauthn_auth_controller.js
@@ -3,6 +3,7 @@ import { get } from '@github/webauthn-json'
 import UAParser from 'ua-parser-js'
 import FingerprintJS from '@fingerprintjs/fingerprintjs'
 import submitForm from '../common/submitForm'
+import airbrake from '../airbrake'
 
 export default class extends Controller {
   static targets = [
@@ -86,6 +87,9 @@ export default class extends Controller {
         this.continueButtonTarget.value = 'Continue'
 
         this.errorTarget.classList.remove('display-none')
+
+        console.error(e)
+        airbrake?.notify(e)
       }
     }
   }
