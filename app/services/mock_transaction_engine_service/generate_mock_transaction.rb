2abdf1eb9913ca00c25a53a7882a34232a02c8da
Sam Poder <sampoder@berkeley.edu>
2024-11-28 19:55:27 -0800

    Make `amount` on a mock transaction a `Money` object (#8732)
    `render_money` assumes any number inputted as cents. so when we had a
    $10.37 mock transaction it was being rendered as 10 cents.
    
    closes https://github.com/hackclub/hcb/issues/8722

--- app/services/mock_transaction_engine_service/generate_mock_transaction.rb
+++ app/services/mock_transaction_engine_service/generate_mock_transaction.rb
@@ -41,7 +41,7 @@ module MockTransactionEngineService
     def run
       generate_mock_transaction_list.map do |trans|
         OpenStruct.new(
-          amount: trans[:amount].round(2),
+          amount: Money.new(trans[:amount].round(2) * 100),
           amount_cents: (trans[:amount].round(2) * 100).to_i,
           fee_payment?: trans[:desc].include?("💰 Fiscal sponsorship fee"),
           date: trans[:date],
