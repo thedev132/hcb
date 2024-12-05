e9dc622494bafc958eaa39505a2f2b5d46221f42
yodalightsabr <yoda@yodacode.xyz>
2024-12-05 12:31:23 -0800

    [HCB Wrapped] Add reimbursement and card grant metrics (#8643)
    ## Summary of the problem
    HCB Wrapped needs additional metrics for 2024
    
    
    ## Describe your changes
    Adds the following metrics (with maybe more to come):
    - Reimbursement count
    - Reimbursement amount
    - Card grant count
    - Card grant amount
    
    ---------
    
    Co-authored-by: Gary Tou <gary@garytou.com>

---
+++ app/models/metric/user/reimbursement_amount.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: metrics
+#
+#  id           :bigint           not null, primary key
+#  metric       :jsonb
+#  subject_type :string
+#  type         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  subject_id   :bigint
+#
+# Indexes
+#
+#  index_metrics_on_subject  (subject_type,subject_id)
+#
+class Metric
+  module User
+    class ReimbursementAmount < Metric
+      include Subject
+
+      def calculate
+        user.reimbursement_reports.sum(&:amount_cents)
+      end
+
+    end
+  end
+
+end
