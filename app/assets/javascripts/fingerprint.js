a9685ad5c392699c419e1c1d81fdd5df30ec13e2
Sam Poder <sampoder@berkeley.edu>
2024-12-18 01:00:29 -0800

    Fix browser fingerprinting (#8988)
    Closes https://github.com/hackclub/hcb/issues/8958 or at least is the
    result of that investigation..

--- app/assets/javascripts/fingerprint.js
+++ app/assets/javascripts/fingerprint.js
@@ -6,21 +6,14 @@
     token = csrfTokenSelector.content
   }
 
-  const fp = await import('https://openfpcdn.io/fingerprintjs/v3').then(
+  const fp = await import('https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3.X/+esm').then(
     FingerprintJS => FingerprintJS.load()
   )
   const result = await fp.get()
-
-  // eslint-disable-next-line no-undef
-  const parser = new UAParser()
   const visitorId = result.visitorId
-  parser.setUA(navigator.userAgent)
-  const uaParserResult = parser.getResult()
 
   document.querySelector('#fingerprint').value = visitorId
-  document.querySelector('#device_info').value =
-    uaParserResult.browser.name + ' ' + uaParserResult.browser.version
-  document.querySelector('#os_info').value =
-    uaParserResult.os.name + ' ' + uaParserResult.os.version
+  document.querySelector('#device_info').value = $.ua.browser.name + ' ' + $.ua.version
+  document.querySelector('#os_info').value = $.ua.os.name + ' ' + $.ua.os.version
   document.querySelector('#timezone').value = result.components.timezone.value
 })()
