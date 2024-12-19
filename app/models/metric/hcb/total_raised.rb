4936326a4d1b274b4c9d1c501eb0b642e2176394
Gary Tou <gary@garytou.com>
2024-12-18 16:24:47 -0800

    [`Metric::Hcb::TotalRaised`] Update SQL (#9006)
    based on Sam's HCB operating budget blazer dashboard

--- app/models/metric/hcb/total_raised.rb
+++ app/models/metric/hcb/total_raised.rb
@@ -22,10 +22,33 @@ class Metric
       include AppWide
 
       def calculate
-        CanonicalTransaction.included_in_stats
-                            .where(date: Date.new(2024, 1, 1)..Date.new(2024, 12, 31))
-                            .revenue
-                            .sum(:amount_cents)
+        result = ActiveRecord::Base.connection.execute(<<~SQL)
+          SELECT SUM(amount_cents) / 100 as amount FROM "canonical_transactions"
+          LEFT JOIN "canonical_event_mappings" ON canonical_transactions.id = canonical_event_mappings.canonical_transaction_id
+          LEFT JOIN "events" ON canonical_event_mappings.event_id = events.id
+          LEFT JOIN "event_plans" ON event_plans.event_id = events.id AND event_plans.aasm_state = 'active'
+          LEFT JOIN "disbursements" ON canonical_transactions.hcb_code = CONCAT('HCB-500-', disbursements.id)
+          WHERE amount_cents > 0
+          AND date_part('year', date) = date_part('year', now())
+          AND event_plans.type != 'Event::Plan::HackClubAffiliate'
+          AND event_plans.type != 'Event::Plan::Internal'
+          AND event_plans.type != 'Event::Plan::CardsOnly'
+          AND event_plans.type != 'Event::Plan::SalaryAccount'
+          AND (disbursements.id IS NULL or disbursements.should_charge_fee = true)
+          AND NOT (
+            canonical_transactions.hcb_code ILIKE 'HCB-300%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-310%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-350%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-400%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-401%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-600%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-601%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-710%' OR
+            canonical_transactions.hcb_code ILIKE 'HCB-712%'
+          )
+        SQL
+
+        result.first["amount"]
       end
 
     end
