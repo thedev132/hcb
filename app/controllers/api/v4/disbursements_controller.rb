7a404efa7914ccd516fb38fa234cfeea43d78b8a
Sam Poder <sampoder@berkeley.edu>
2025-01-01 09:00:56 -0800

    Fix pundit policy calls in api 4's disbursements controller (#9095)
    Closes https://github.com/hackclub/hcb/issues/9087

--- app/controllers/api/v4/disbursements_controller.rb
+++ app/controllers/api/v4/disbursements_controller.rb
@@ -6,9 +6,9 @@ module Api
       def create
         @source_event = Event.find_by_public_id(params[:event_id]) || Event.friendly.find(params[:event_id])
         @destination_event = Event.find_by_public_id(params[:to_organization_id]) || Event.friendly.find(params[:to_organization_id])
+        @disbursement = Disbursement.new(destination_event: @destination_event, source_event: @source_event)
 
-        authorize @source_event, policy_class: ::DisbursementPolicy
-        authorize @destination_event, policy_class: ::DisbursementPolicy
+        authorize @disbursement
 
         begin
           @disbursement = DisbursementService::Create.new(
