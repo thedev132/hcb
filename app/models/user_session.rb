cd65abcc0b986166320ee5024d85d27653b36f90
Sam Poder <sampoder@berkeley.edu>
2025-01-04 02:58:23 -0800

    Ignore "deleted_at", "peacefully_expired" on UserSession (#9139)
    Progress towards https://github.com/hackclub/hcb/issues/8950

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -42,7 +42,7 @@ class UserSession < ApplicationRecord
   has_encrypted :session_token
   blind_index :session_token
 
-  acts_as_paranoid
+  self.ignored_columns = ["deleted_at", "peacefully_expired"]
 
   belongs_to :user
   belongs_to :impersonated_by, class_name: "User", optional: true
@@ -53,9 +53,9 @@ class UserSession < ApplicationRecord
 
   scope :impersonated, -> { where.not(impersonated_by_id: nil) }
   scope :not_impersonated, -> { where(impersonated_by_id: nil) }
-  scope :expired, -> { with_deleted.where.not(deleted_at: nil).or(with_deleted.where("expiration_at <= ?", Time.now)) }
-  scope :not_expired, -> { where(deleted_at: nil).where("expiration_at > ?", Time.now) }
-  scope :recently_expired_within, ->(date) { expired.where("coalesce(deleted_at, expiration_at) >= ?", date) }
+  scope :expired, -> { where("expiration_at <= ?", Time.now) }
+  scope :not_expired, -> { where("expiration_at > ?", Time.now) }
+  scope :recently_expired_within, ->(date) { expired.where("expiration_at >= ?", date) }
 
   after_create_commit do
     if fingerprint.present? && user.user_sessions.excluding(self).where(fingerprint:).none?
@@ -79,6 +79,10 @@ class UserSession < ApplicationRecord
     return if last_seen_at&.after? LAST_SEEN_AT_COOLDOWN.ago # prevent spamming writes
 
     update_columns(last_seen_at: Time.now)
+  end
+
+  def expired?
+    expiration_at <= Time.now
   end
 
   private
