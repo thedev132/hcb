4e0b966759f625cbb84ec11fd87b5c5ee5e44856
Sam Poder <sampoder@berkeley.edu>
2025-01-16 18:30:59 -0800

    Remove use of `EXCLUDE_PENDING_FEES_FROM_AMPLE_BALANCE` environment variable in AmpleBalance (#9243)
    Closes https://github.com/hackclub/hcb/issues/9242

--- app/services/shared/ample_balance.rb
+++ app/services/shared/ample_balance.rb
@@ -3,12 +3,7 @@
 module Shared
   module AmpleBalance
     def self.ample_balance?(amount_cents = @amount_cents, event = @event)
-      if ENV["EXCLUDE_PENDING_FEES_FROM_AMPLE_BALANCE"] == "true"
-        event.balance_v2_cents >= amount_cents
-      else
-        # includes pending fees
-        event.balance_available_v2_cents >= amount_cents
-      end
+      event.balance_available_v2_cents >= amount_cents # includes pending fees
     end
 
     def ample_balance?(...)
