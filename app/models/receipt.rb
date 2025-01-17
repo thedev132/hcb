9787ce286338ff01ed2481c818a04cf81d4eb9a0
Sam Poder <sampoder@berkeley.edu>
2025-01-16 19:59:58 -0800

    Remove email & sms methods from `Receipt::SYNCHRONOUS_SUGGESTION_UPLOAD_METHODS` (#9251)
    Because these methods didn't use direct uploads, HCB needs a little more
    time before it can extract the receipt text. Because uploads are
    guaranteed to be done after a commit.
    
    https://github.com/hackclub/hcb/issues/4067#issuecomment-2591701863

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -73,7 +73,7 @@ class Receipt < ApplicationRecord
     end
   end
 
-  SYNCHRONOUS_SUGGESTION_UPLOAD_METHODS = %w[quick_expense email_receipt_bin email_hcb_code email_reimbursement sms_reimbursement].freeze
+  SYNCHRONOUS_SUGGESTION_UPLOAD_METHODS = %w[quick_expense].freeze
 
   after_create_commit do
     # Queue async job to extract text from newly upload receipt
