f99b54ea4c609ae01b89d9d46c809e864f4c8d12
Sam Poder <sampoder@berkeley.edu>
2025-01-25 22:26:56 -0800

    Rescue `Stripe::InvalidRequestError` for card grants (#9354)
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    This legal name is not valid
    
    Did I mention this closes https://github.com/hackclub/hcb/issues/9353?

--- app/controllers/card_grants_controller.rb
+++ app/controllers/card_grants_controller.rb
@@ -92,6 +92,8 @@ class CardGrantsController < ApplicationController
     @card_grant.create_stripe_card(current_session)
 
     redirect_to @card_grant
+  rescue Stripe::InvalidRequestError => e.message
+    redirect_to @card_grant, flash: { error: "This card could not be activated: #{e.message}" }
   end
 
   def cancel
