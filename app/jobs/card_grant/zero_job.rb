c4f03106af3984c60d1ece2fb915e8f6a8bbbe2f
Sam Poder <sampoder@berkeley.edu>
2025-02-03 14:27:03 -0800

    Don't zero recently cancelled grants + stop auto-topups (#9421)
    https://github.com/hackclub/hcb/issues/9419 <- related

--- app/jobs/card_grant/zero_job.rb
+++ app/jobs/card_grant/zero_job.rb
@@ -6,9 +6,13 @@ class CardGrant
     def perform
 
       CardGrant.where.not(status: :active).find_each do |card_grant|
-        next if card_grant.balance.zero?
+        next if card_grant.last_time_change_to(status: :canceled) > 1.week.ago
 
-        card_grant.zero!(allow_topups: true)
+        Airbrake.notify("#{card_grant.hashid} is a negative card grant that was canceled more than a week ago") if card_grant.balance.negative?
+
+        next unless card_grant.balance.positive?
+
+        card_grant.zero!
       end
 
     end
