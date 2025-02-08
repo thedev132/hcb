d383a02684504ff99ea0285117814931422b2b9f
Gary Tou <gary@garytou.com>
2025-02-07 18:11:25 -0800

    [AirbrakeSubscriber] Re-enable & prevent infinite recursion (#9514)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    When calling `Airbrake.notify`, we passed `context` (provided to
    `AirbrakeSubscriber#report` as a parameter). When an error is raised
    during a web request, Rails includes the `controller` instance inside
    the `context` object.
    
    Inside `Airbrake.notify`, it attempts to call `context.as_json`. The
    `controller` instance, however, will infinitely call `as_json` on
    itself. I'm assuming there's some circular referencing happening in
    there.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    As a solution, I'm deleting `controller` from context and only passing
    along specific attributes from the controller.
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- config/initializers/airbrake_subscriber.rb
+++ config/initializers/airbrake_subscriber.rb
@@ -2,11 +2,23 @@
 
 class AirbrakeSubscriber
   def report(error, handled:, severity:, context:, source: nil)
-    Airbrake.notify(error, { context:, handled:, severity:, source: })
-  rescue
-    nil
+    airbrake_context = extract_context(context.dup)
+
+    Airbrake.notify(error, { context: airbrake_context, handled:, severity:, source: })
+  end
+
+  def extract_context(context)
+    controller = context.delete(:controller)
+
+    context.merge!({
+                     controller_name: controller&.controller_name,
+                     action_name: controller&.action_name,
+                     current_user_id: controller&.current_user&.id,
+                     current_user_email: controller&.current_user&.email,
+                     current_session_id: controller&.session&.id,
+                   })
   end
 
 end
 
-# Rails.error.subscribe(AirbrakeSubscriber.new)
+Rails.error.subscribe(AirbrakeSubscriber.new)
