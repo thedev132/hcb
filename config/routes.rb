a3e83c7fd90efece03a65cbca4136bfc4be75d5c
Gary Tou <gary@garytou.com>
2025-02-08 22:22:52 -0800

    [Airbrake] Remove Ruien's error testing controller (#9516)
    After https://github.com/hackclub/hcb/pull/9514 deploys and we verify it
    errors are reporting to both AppSignal and Airbrake, we can merge this
    PR.
    
    Co-authored-by: rluo.dev <34289582+rluodev@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ require "sidekiq/web"
 require "sidekiq/cron/web"
 
 Rails.application.routes.draw do
-  # to be removed after testing rails error reporter
-  get "/ruien-test", to: "ruien_test#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   constraints AdminConstraint do
