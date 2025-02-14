6a5f2c76f69197ea0ef8bfaafdce3255974d80f1
Sam Poder <sampoder@berkeley.edu>
2025-02-13 16:08:11 -0800

    Fix slug exposure issue in `EventsController#show` (#9568)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -55,7 +55,11 @@ class EventsController < ApplicationController
 
   # GET /events/1
   def show
-    authorize @event
+    begin
+      authorize @event
+    rescue Pundit::NotAuthorizedError
+      return redirect_to root_path, flash: { error: "We couldn’t find that organization!" }
+    end
 
     if !Flipper.enabled?(:event_home_page_redesign_2024_09_21, @event) && !(params[:event_home_page_redesign_2024_09_21] && admin_signed_in?) || @event.demo_mode?
       redirect_to event_transactions_path(@event.slug)
