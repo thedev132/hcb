a5a3312b71424b47cd396a00b2f0a5300e2b9e7c
Sam Poder <sampoder@berkeley.edu>
2025-02-18 12:56:41 -0800

    Create `StripeWebhookJob` (#9583)
    Closes https://github.com/hackclub/hcb/issues/9570

---
+++ app/jobs/stripe_webhook_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class StripeWebhookJob < ApplicationJob
+  queue_as :default
+  def perform
+    events = StripeService::Event.list({ limit: 100, created: { gte: Time.now.to_i - 5 * 60 }, delivery_success: false }).data
+    if events.any?
+      if events.count == 100
+        Airbrake.notify("🚨 100+ Stripe webhooks failed in the past five minutes.")
+      else
+        Airbrake.notify("🚨 #{events.count} Stripe webhooks failed in the past five minutes.")
+      end
+    end
+  end
+
+end
