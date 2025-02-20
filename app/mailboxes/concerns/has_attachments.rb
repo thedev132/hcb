03e453cb5c4332a827b80d5525c41b1a381a1328
nora <163450896+24c02@users.noreply.github.com>
2025-02-20 16:41:59 -0500

    [mailboxes] convert .eml message parts to PDFs (#9603)
    select a bunch of emailed receipts.
    right click > forward as attachment to your receipt bin email.
    boom.
    
    this would let me ingest a whole inbox's worth of receipts into hcb
    easily!
    which... would be nice.....because i have an inbox full of receipts :pf:
    
    pls save me from having to forward them individually!

--- app/mailboxes/concerns/has_attachments.rb
+++ app/mailboxes/concerns/has_attachments.rb
@@ -6,13 +6,29 @@ module HasAttachments
   included do
     private
 
-    def set_attachments(include_body: true)
+    def set_attachments(include_body: true, convert_emls: true)
       files = mail.attachments.select { |a| valid_content_type(a.content_type) }.map do |atta|
         {
           io: StringIO.new(atta.decoded),
           content_type: atta.content_type,
           filename: atta.filename,
         }
+      end
+
+      if convert_emls
+        emls = mail.parts.select { |a| a.content_type.start_with?('message/rfc822' ) || a.filename&.end_with?('.eml') }
+        emls.each do |eml|
+          decoded = Mail.read_from_string(eml.decoded)
+          content = decoded.html_part&.body&.decoded || decoded.text_part&.body&.decoded || decoded.body&.decoded
+
+          next unless content
+
+          files << {
+            io: StringIO.new(WickedPdf.new.pdf_from_string(content, encoding: "UTF-8")),
+            content_type: "application/pdf",
+            filename: "EML_#{(decoded.subject || Time.now.strftime("%Y%m%d%H%M")).gsub(/[^0-9A-Za-z]/, '').slice(0, 30)}.pdf"
+          }
+        end
       end
 
       return @attachments = files if files.any?
