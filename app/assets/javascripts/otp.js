eee504164d32e953437a8d5027f9e11ca3481b31
Sam Poder <sampoder@berkeley.edu>
2025-02-22 00:06:14 -0800

    Make the TOTP input separate boxes but one input (#9625)
    https://github.com/user-attachments/assets/7d16ac81-6a21-4446-8db5-738d8837f2d4

---
+++ app/assets/javascripts/otp.js
@@ -0,0 +1,27 @@
+function setupOTP() {
+  $('[data-behavior=otp_input]').on('keydown', (e) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            e.target.style.setProperty('--_otp-digit', Math.max(0, e.target.selectionStart - 1))
+            break;
+        case "ArrowRight":
+            e.target.style.setProperty('--_otp-digit', Math.min(e.target.value.length, e.target.selectionStart + 1))
+            break;
+    }
+  })
+  
+  $('[data-behavior=otp_input]').on('input', (e) => {
+    e.target.style.setProperty('--_otp-digit', e.target.selectionStart)
+    if(e.target.value.length == 6){
+      $(e.target).closest('form').submit();
+    }
+  })
+  
+  $('[data-behavior=otp_input]').on('click', (e) => 
+    e.target.style.setProperty('--_otp-digit', e.target.selectionStart)
+  )
+}
+
+setupOTP()
+
+document.addEventListener('turbo:load', setupOTP)
