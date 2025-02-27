b3e605660aa323f8925cc18a385270da7cfde4e4
yodalightsabr <yoda@yodacode.xyz>
2025-02-27 08:57:32 -0800

    Remove Counterspell Ottawa from login page (#9711)
    https://hackclub.slack.com/archives/GBXBFEED9/p1740673787813469
    
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/helpers/logins_helper.rb
+++ app/helpers/logins_helper.rb
@@ -7,12 +7,6 @@ module LoginsHelper
       time: "Summer 2022",
       slug: "assemble",
       background: "linear-gradient(180deg, rgb(0 0 0 / 20%) 0%, rgba(0 0 0 / 40%) 100%), url('https://cloud-cuely0z02-hack-club-bot.vercel.app/0assemble__2_.jpg')"
-    },
-    {
-      name: "Counterspell Ottawa",
-      time: "Fall 2024",
-      slug: "counterspell-ottawa",
-      background: "linear-gradient(180deg, rgb(0 0 0 / 30%) 0%, rgba(0 0 0 / 50%) 100%), url('https://cloud-cuely0z02-hack-club-bot.vercel.app/10batch1photos-3112__1_.jpg')"
     }
   ].map do |hackathon|
     hackathon[:url] = "/#{hackathon[:slug]}"
