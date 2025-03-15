07ccef54c77750392c2f30e5b57235619d6e0a8c
Sam Poder <sampoder@berkeley.edu>
2025-03-15 01:45:37 -0700

    Fix Webauthn by properly setting `config.origin` (#9843)
    Thanks to https://gitlab.com/gitlab-org/gitlab/-/issues/348294 for
    helping me with this one!

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -5,8 +5,10 @@ WebAuthn.configure do |config|
     # Use the Heroku review app's origin
     heroku_app_name = ENV["HEROKU_APP_NAME"]
     config.origin = "https://#{heroku_app_name}.herokuapp.com"
+  elsif Rails.env.production?
+    config.origin = Credentials.fetch(:LIVE_URL_HOST)
   else
-    config.origin = Rails.application.routes.default_url_options[:host]
+    config.origin = Credentials.fetch(:TEST_URL_HOST)
   end
 
   config.rp_name = "Hack Club Bank"
