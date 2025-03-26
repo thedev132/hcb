9bf3765c6ace3518e8eb672982d562086b26b46b
yodalightsabr <yoda@yodacode.xyz>
2025-03-26 17:13:05 -0500

    Remove non-friendly ID usage in transparency API (#9981)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    ---------
    
    Co-authored-by: Sam Poder <sam.r.poder@gmail.com>

--- app/api/api/v3.rb
+++ app/api/api/v3.rb
@@ -23,7 +23,7 @@ module Api
           begin
             id = params[:organization_id]
             event ||= Event.transparent.find_by_public_id id # by public id (ex. org_1234). Will NOT error if not found
-            event ||= Event.transparent.friendly.find id # by slug or numeric id. Will error if not found
+            event ||= Event.transparent.friendly.find_by_friendly_id id # by slug. Will error if not found
           end
       rescue ActiveRecord::RecordNotFound
         error!({ message: "Organization not found." }, 404)
