86b5136279e9941646a09df843ce9edebc621697
Sam Poder <sampoder@berkeley.edu>
2025-03-27 06:54:05 -0700

    Ignore NoEvent on CheckBalanceJob (#9986)

--- app/jobs/check_balance_job.rb
+++ app/jobs/check_balance_job.rb
@@ -3,6 +3,8 @@
 class CheckBalanceJob < ApplicationJob
   queue_as :low
   def perform(event:)
+    return if event.id == EventMappingEngine::EventIds::NOEVENT
+
     Airbrake.notify("#{event.name} has a negative balance: #{ApplicationController.helpers.render_money event.balance}") if event.balance.negative?
   end
 
