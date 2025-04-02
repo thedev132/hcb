e6fb836c921c4e1ad1a85a09ea730b83df5e52c6
Sam Poder <sampoder@berkeley.edu>
2025-04-01 19:37:58 -0700

    Add more contract-related mailers (#10032)
    I love sending email programmatically.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/controllers/docuseal_controller.rb
+++ app/controllers/docuseal_controller.rb
@@ -5,15 +5,18 @@ class DocusealController < ActionController::Base
 
   def webhook
     ActiveRecord::Base.transaction do
-      @contract = OrganizerPosition::Contract.find_by(external_id: params[:data][:submission_id])
-      return render json: { success: true } unless @contract # sometimes contracts are sent using Docuseal that aren't in HCB
+      contract = OrganizerPosition::Contract.find_by(external_id: params[:data][:submission_id])
+      return render json: { success: true } unless contract # sometimes contracts are sent using Docuseal that aren't in HCB
+
+      signee = contract.docuseal_document["submitters"].select { |r| r["role"] == "Cosigner" }
+      cosigneer = contract.docuseal_document["submitters"].select { |r| r["role"] == "Contract Signee" }
 
       if params[:event_type] == "form.completed" && params[:data][:submission][:status] == "completed"
-        return render json: { success: true } if @contract.signed?
+        return render json: { success: true } if contract.signed?
 
         document = Document.new(
-          event: @contract.organizer_position_invite.event,
-          name: "Fiscal sponsorship contract with #{@contract.organizer_position_invite.user.name}"
+          event: contract.organizer_position_invite.event,
+          name: "Fiscal sponsorship contract with #{contract.organizer_position_invite.user.name}"
         )
 
         response = Faraday.get(params[:data][:documents][0][:url]) do |req|
@@ -25,12 +28,18 @@ class DocusealController < ActionController::Base
           filename: "#{params[:data][:documents][0][:name]}.pdf"
         )
 
-        document.user = User.find_by(email: params[:data][:email]) || @contract.organizer_position_invite.event.point_of_contact
+        document.user = User.find_by(email: params[:data][:email]) || contract.organizer_position_invite.event.point_of_contact
         document.save!
-        @contract.update(document:)
-        @contract.mark_signed!
+        contract.update(document:)
+        contract.mark_signed!
       elsif params[:event_type] == "form.declined"
-        @contract.mark_voided!
+        contract.mark_voided!
+      elsif cosigner.present? && cosigner["status"] != "completed"
+        # send email about cosigner needing to pay
+        OrganizerPosition::ContractsMailer.with(contract:).pending_cosigner.deliver_later
+      elsif signee["status"] == "completed" && (cosigner.nil? || cosigner["status"] == "completed")
+        # send email about hcb needing to sign
+        OrganizerPosition::ContractsMailer.with(contract:).pending_hcb.deliver_later
       end
     end
   rescue => e
--- app/mailers/organizer_position/contracts_mailer.rb
+++ app/mailers/organizer_position/contracts_mailer.rb
@@ -14,6 +14,17 @@ class OrganizerPosition
       mail to: @contract.cosigner_email, subject: "You've been invited to sign a contract for #{@contract.organizer_position_invite.event.name} on HCB 📝"
     end
 
+    def pending_cosigner
+      @contract = params[:contract]
+
+      mail to: @contract.organizer_position_invite.user.email_address_with_name, subject: "We're waiting for your cosigner to sign your HCB contract 📝"
+    end
+
+    def pending_hcb
+      @contract = params[:contract]
+
+      mail to: @contract.organizer_position_invite.user.email_address_with_name, subject: "We're processing your HCB contract 📝"
+    end
 
   end
 
