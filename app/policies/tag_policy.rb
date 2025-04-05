ddb69f51bbb2f3f97186a8f79dfe83532b9f0340
Sam Poder <sampoder@berkeley.edu>
2025-04-04 22:15:03 -0700

    Fix `TagPolicy` (#10078)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -2,15 +2,15 @@
 
 class TagPolicy < ApplicationPolicy
   def create?
-    OrganizerPosition.role_at_least?(user, record.event, :member)
+    OrganizerPosition.role_at_least?(user, record, :member)
   end
 
   def destroy?
-    OrganizerPosition.role_at_least?(user, record.event, :member)
+    OrganizerPosition.role_at_least?(user, record, :member)
   end
 
   def toggle_tag?
-    OrganizerPosition.role_at_least?(user, record.event, :member)
+    OrganizerPosition.role_at_least?(user, record, :member)
   end
 
 end
