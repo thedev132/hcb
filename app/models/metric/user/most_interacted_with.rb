a77f3124f7a6b6209dbc6a878cb20de5cc032035
Sam Poder <sampoder@berkeley.edu>
2025-04-14 01:08:30 -0700

    Fix 402s in Metric (#10165)

--- app/models/metric/user/most_interacted_with.rb
+++ app/models/metric/user/most_interacted_with.rb
@@ -28,7 +28,7 @@ class Metric
           JOIN "hcb_codes" on commentable_type = 'HcbCode' and hcb_codes.id = commentable_id
           LEFT JOIN "ach_transfers" on hcb_codes.hcb_code = CONCAT('HCB-300-', ach_transfers.id)
           LEFT JOIN "checks" on hcb_codes.hcb_code = CONCAT('HCB-400-', checks.id)
-          LEFT JOIN "increase_checks" on hcb_codes.hcb_code = CONCAT('HCB-402-', increase_checks.id)
+          LEFT JOIN "increase_checks" on hcb_codes.hcb_code = CONCAT('HCB-401-', increase_checks.id)
           LEFT JOIN "disbursements" on hcb_codes.hcb_code = CONCAT('HCB-500-', disbursements.id)
           LEFT JOIN "canonical_transactions" on hcb_codes.hcb_code = canonical_transactions.hcb_code
           LEFT JOIN "raw_stripe_transactions" on canonical_transactions.transaction_source_type = 'RawStripeTransaction' and canonical_transactions.transaction_source_id = raw_stripe_transactions.id
