6252252230472966ca607485db4a4454d429af68
Sam Poder <sampoder@berkeley.edu>
2025-04-18 14:50:59 -0700

    Fix `company_name` to "HCB (Hack Club)" (#10206)

--- app/models/ach_transfer.rb
+++ app/models/ach_transfer.rb
@@ -173,7 +173,7 @@ class AchTransfer < ApplicationRecord
   before_validation { self.recipient_name = recipient_name.presence&.strip }
 
   before_validation do
-    self.company_name = event.short_name if company_name.blank?
+    self.company_name = "HCB (Hack Club)" # https://hackclub.slack.com/archives/C065957QPC7/p1745005224201699
   end
 
   # Eagerly create HcbCode object
