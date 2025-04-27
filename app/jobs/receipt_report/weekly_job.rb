451ffd082931170482bc6d6a04ea525432d53e42
Sam Poder <sampoder@berkeley.edu>
2025-04-27 16:49:49 -0700

    Fix receipt weekly reports (#10247)

--- app/jobs/receipt_report/weekly_job.rb
+++ app/jobs/receipt_report/weekly_job.rb
@@ -5,7 +5,7 @@ module ReceiptReport
     queue_as :low
     def perform
       User.receipt_report_weekly.find_each(batch_size: 100) do |user|
-        Send.perform_later(user.id)
+        SendJob.perform_later(user.id)
       end
     end
 
