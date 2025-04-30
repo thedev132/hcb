6e1304dd5709f6d3f68fbdff59465e7f345f4fa6
Sam Poder <sampoder@berkeley.edu>
2025-04-30 00:16:58 -0700

    Fix short code mapping (#10278)

--- app/services/event_mapping_engine/map/hcb_codes/short.rb
+++ app/services/event_mapping_engine/map/hcb_codes/short.rb
@@ -15,15 +15,15 @@ module EventMappingEngine
             hcb_code = ::HcbCode.find_by(short_code:)
             next unless hcb_code
 
-            next unless guessed_event_id
+            next unless guess_event_id(hcb_code)
 
             ActiveRecord::Base.transaction do
               ct.update_column(:hcb_code, hcb_code.hcb_code)
 
               attrs = {
                 canonical_transaction_id: ct.id,
-                event_id: guessed_event_id,
-                subledger_id: guessed_subledger_id
+                event_id: guess_event_id(hcb_code),
+                subledger_id: guess_subledger_id(hcb_code)
               }
               ::CanonicalEventMapping.create!(attrs)
             end
@@ -36,7 +36,7 @@ module EventMappingEngine
           ::CanonicalTransaction.unmapped.with_short_code.order("date asc")
         end
 
-        def guessed_event_id
+        def guess_event_id(hcb_code)
           return hcb_code.event.try(:id) if hcb_code.events.length == 1
 
           if hcb_code.disbursement?
@@ -50,7 +50,7 @@ module EventMappingEngine
           raise ArgumentError, "attempted to map a transaction with HCB short codes to a multi-event HCB code"
         end
 
-        def guessed_subledger_id
+        def guess_subledger_id(hcb_code)
           return hcb_code.ct&.canonical_event_mapping&.subledger_id if hcb_code.events.length == 1
 
           if hcb_code.disbursement?
