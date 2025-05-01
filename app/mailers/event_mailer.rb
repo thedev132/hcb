9d2add5af3246fa31fe63eafd5f1daf7671f6775
Sam Poder <sampoder@berkeley.edu>
2025-04-30 19:10:54 -0700

    Don't email if no emails (#10291)

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -10,6 +10,7 @@ class EventMailer < ApplicationMailer
     @donations = @event.donations.where(aasm_state: [:in_transit, :deposited], created_at: Time.now.last_month.beginning_of_month..).order(:created_at)
 
     return if @donations.none?
+    return if @emails.none?
 
     @total = @donations.sum(:amount)
 
