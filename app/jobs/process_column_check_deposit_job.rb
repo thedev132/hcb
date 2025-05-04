a4e47ab6c85e54fa50fa4ea332f0b766cdd7d99f
Sam Poder <sampoder@berkeley.edu>
2025-05-04 14:58:06 -0700

    Clarify that we are depositing checks to Column (#10322)

--- app/jobs/process_column_check_deposit_job.rb
+++ app/jobs/process_column_check_deposit_job.rb
@@ -36,7 +36,7 @@ class ProcessColumnCheckDepositJob < ApplicationJob
                                                                            micr_line: front["micr_line"],
                                                                            image_front: front["image_front"],
                                                                            image_back: back["image_back"],
-                                                                           idempotency_key: check_deposit.id.to_s)
+                                                                           idempotency_key: "check_deposit_#{check_deposit.id}")
 
     check_deposit.update!(column_id: column_check_deposit["id"], status: :submitted)
 
