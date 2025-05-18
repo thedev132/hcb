af3b4279e670236df5aeeb530c95c13d618e4412
Sam Poder <sampoder@berkeley.edu>
2025-05-17 17:03:09 -0700

    Allow topups on the ZeroJob (#10448)

--- app/jobs/card_grant/zero_job.rb
+++ app/jobs/card_grant/zero_job.rb
@@ -10,11 +10,7 @@ class CardGrant
         changed_at = card_grant.last_time_change_to(status: :canceled) || card_grant.last_time_change_to(status: :expired) || card_grant.updated_at
         next if changed_at > 1.week.ago
 
-        Airbrake.notify("#{card_grant.hashid} is a negative card grant that was canceled more than a week ago") if card_grant.balance.negative?
-
-        next unless card_grant.balance.positive?
-
-        card_grant.zero!
+        card_grant.zero!(allow_topups: true)
       end
 
     end
