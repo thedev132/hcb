c39feb419078aa7050704a9649c18ca2a294afa3
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-06-05 18:40:00 -0400

    [Card Grants] Hide actions for members (#10495)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Members don't have permissions to use the actions so they should see the
    regular view and not the organizer view.
    
    manager/admin: 
    
    ![image](https://github.com/user-attachments/assets/b50b6472-ba53-4401-af42-44a356a55fbc)
    
    member:
    
    ![image](https://github.com/user-attachments/assets/13a0a97a-704a-49a5-9865-089113ae96a6)
    
    
    reader: 
    
    ![image](https://github.com/user-attachments/assets/018e2d18-9421-4933-b3d4-95739710e1f9)
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Use card grant policy.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/policies/card_grant_policy.rb
+++ app/policies/card_grant_policy.rb
@@ -22,7 +22,19 @@ class CardGrantPolicy < ApplicationPolicy
   end
 
   def cancel?
-    admin_or_user? || record.user == user
+    (admin_or_manager? || record.user == user) && record.active?
+  end
+
+  def convert_to_reimbursement_report?
+    (admin_or_manager? || record.user == user) && record.active? && record.card_grant_setting.reimbursement_conversions_enabled?
+  end
+
+  def edit?
+    admin_or_manager? && record.active?
+  end
+
+  def toggle_one_time_use?
+    admin_or_manager? && record.active?
   end
 
   def topup?
@@ -34,14 +46,6 @@ class CardGrantPolicy < ApplicationPolicy
   end
 
   def update?
-    admin_or_manager?
-  end
-
-  def convert_to_reimbursement_report?
-    (admin_or_manager? || record.user == user) && record.card_grant_setting.reimbursement_conversions_enabled?
-  end
-
-  def toggle_one_time_use?
     admin_or_manager? && record.active?
   end
 
