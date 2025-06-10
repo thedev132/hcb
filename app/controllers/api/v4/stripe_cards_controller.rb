fb3b662c054ddde7c070113d8073cd9735cf2283
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-06-09 20:00:30 -0400

    [API V4] Check policy before updating card. (#10598)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/controllers/api/v4/stripe_cards_controller.rb
+++ app/controllers/api/v4/stripe_cards_controller.rb
@@ -76,33 +76,39 @@ module Api
         @stripe_card = authorize StripeCard.find_by_public_id!(params[:id])
 
         if params[:status] == "frozen"
+          return render json: { error: "not_authorized" }, status: :forbidden unless policy(@stripe_card).freeze?
+
           if @stripe_card.canceled?
-            return render json: { error: "Card has been cancelled, it can't be frozen" }, status: :unprocessable_entity
+            return render json: { error: "Card is canceled." }, status: :unprocessable_entity
           end
 
           @stripe_card.freeze!
         elsif params[:status] == "active"
           if @stripe_card.initially_activated?
+            return render json: { error: "not_authorized" }, status: :forbidden unless policy(@stripe_card).defrost?
+
             if @stripe_card.stripe_status == "active"
-              return render json: { error: "Card is already active" }, status: :unprocessable_entity
+              return render json: { error: "Card is already active." }, status: :unprocessable_entity
             end
 
             @stripe_card.defrost!
-            return render json: { success: "Card activated!" }
+            return render json: { success: "Card defrosted!" }
           end
 
+          return render json: { error: "not_authorized" }, status: :forbidden unless policy(@stripe_card).activate?
+
           if params[:last4].blank?
-            return render json: { error: "Last four digits are required" }, status: :unprocessable_entity
+            return render json: { error: "Last four digits are required." }, status: :unprocessable_entity
           end
 
           # Find the correct card based on it's last4
           card = current_user.stripe_cardholder&.stripe_cards&.find_by(last4: params[:last4])
           if card.nil? || card.id != @stripe_card.id
-            return render json: { error: "Last four digits are incorrect" }, status: :unprocessable_entity
+            return render json: { error: "Last four digits are incorrect." }, status: :unprocessable_entity
           end
 
           if @stripe_card.canceled?
-            return render json: { error: "Card has been cancelled, it can't be activated." }, status: :unprocessable_entity
+            return render json: { error: "Card is canceled." }, status: :unprocessable_entity
           end
 
           # If this replaces another card, attempt to cancel the old card.
