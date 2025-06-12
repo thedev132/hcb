3185c3fb75ea3bb30dd9289a1008432212aeb355
Sam Poder <sampoder@berkeley.edu>
2025-06-12 17:54:42 -0400

    Fix ACH transfer reversing (#10631)

--- app/models/ach_transfer.rb
+++ app/models/ach_transfer.rb
@@ -252,7 +252,7 @@ class AchTransfer < ApplicationRecord
   def reverse!(reason)
     raise ArgumentError, "must have been sent" unless column_id
 
-    ColumnService.post "/transfers/ach/#{column_id}/reverse", reason:, idempotency_key: self.id.to_s
+    ColumnService.post "/transfers/ach/#{column_id}/reverse", reason:, idempotency_key: "reverse_#{self.id}"
   end
 
   def pending_expired?
