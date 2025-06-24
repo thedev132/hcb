85bdbe647df6d4dc40e1b3375aff0258989f3468
David Cornu <davidc@hackclub.com>
2025-06-24 06:19:31 -0400

    Expire impersonated sessions after 1 hour (#10667)
    ## Summary of the problem
    
    Admin users have the ability to impersonate other users for debugging
    and support purposes. However while thinking through the implications of
    implementing a sudo mode (see
    https://github.com/hackclub/hcb/pull/10653#discussion_r2155544529) we
    realized these impersonated sessions live for as long as the
    impersonated user's `session_duration_seconds`, which could be much
    longer than necessary.
    
    ## Describe your changes
    
    - `spec/rails_helper.rb` now eagerly-loads all files in `spec/support`
    so we can put shared code in there
    - Added `SessionSupport` module to make writing controller tests easier
    - Updated the `:user_session` factory to populate the `session_token`
    field
    - Updated the logic in `SessionHelper#sign_in` to cap impersonated
    sessions to one hour
    - Added a test for the above
    
    ---------
    
    Co-authored-by: Manu G <manusvathgurudath@gmail.com>

--- spec/factories/user_session_factory.rb
+++ spec/factories/user_session_factory.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :user_session do
     association :user
-    expiration_at { Time.now + 7.days.to_i }
+    expiration_at { 7.days.from_now }
+    session_token { SecureRandom.urlsafe_base64 }
   end
 end
