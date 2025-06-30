3dbf2fcc7e9318170d20d6c69d420909ed0d4bb3
Samuel Fernandez <me@sfernandez.dev>
2025-06-30 17:14:53 -0400

    Handle rate limiting in donation summary sending  (#10546)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    > Net::SMTPServerBusy: 454 Throttling failure: Maximum sending rate
    exceeded.
    
    This error was raised a couple days ago when the monthly donation
    summaries were sent since the job does not currently handle rate limits
    from SES.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Changed the monthly donation summary job to be a long-running job that
    will use exponential backoff when it is rate limited by SES.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    ---------
    
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/jobs/monthly_donation_summary_job.rb
+++ app/jobs/monthly_donation_summary_job.rb
@@ -3,9 +3,11 @@
 class MonthlyDonationSummaryJob < ApplicationJob
   queue_as :default
   def perform
+    # Rate limit is 14/s, but putting 12 here to be safe and allow for other emails to be sent
+    queue = Limiter::RateQueue.new(12, interval: 1)
     Event.includes(:donations).where("donations.created_at > ?", 1.month.ago).references(:donations).find_each do |event|
-      mailer = EventMailer.with(event: event)
-      mailer.monthly_donation_summary.deliver_later
+      queue.shift
+      EventMailer.with(event: event).monthly_donation_summary.deliver_now
     end
   end
 
