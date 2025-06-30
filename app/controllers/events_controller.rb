7e695a19a8fc5acf3fc07a97b4ac7d86a609d15c
Scooter <92922224+scooterthedev@users.noreply.github.com>
2025-06-30 11:21:17 -0400

    [Admin] Filter active teenagers within the last 30 days on the team page (#10615)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    https://hackclub.slack.com/archives/C068U0JMV19/p1749575821982219 +
    @leowilkin ;)
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Added a filter for admins only to see active teenagers for a specific
    org on the teams page.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -265,6 +265,8 @@ class EventsController < ApplicationController
       @filter = "manager"
     when "readers"
       @filter = "reader"
+    when "active_teens"
+      @filter = "active_teens"
     end
 
     @q = params[:q] || ""
@@ -274,10 +276,13 @@ class EventsController < ApplicationController
 
     @all_positions = @event.organizer_positions
                            .joins(:user)
-    @all_positions = @all_positions.where(role: @filter) if @filter
     @all_positions = @all_positions.where(organizer_signed_in? ? "users.full_name ILIKE :query OR users.email ILIKE :query" : "users.full_name ILIKE :query", query: "%#{User.sanitize_sql_like(@q)}%")
                                    .order(created_at: :desc)
-
+    if @filter == "active_teens"
+      @all_positions = @all_positions.select { |op| op.user.teenager? && op.user.active? } # select if user is a teenager and active (stole from the other code ;))
+    elsif @filter
+      @all_positions = @all_positions.where(role: @filter)
+    end
     @positions = Kaminari.paginate_array(@all_positions).page(params[:page]).per(params[:per] || @view == "list" ? 20 : 10)
 
     @pending = @event.organizer_position_invites.pending.includes(:sender)
