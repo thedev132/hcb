d910d8afe7bbd7f97292d8e91ec77f629a116137
Mohamad Mortada <mmortada685@gmail.com>
2025-07-03 15:33:04 -0400

    readd comments v4

--- config/routes.rb
+++ config/routes.rb
@@ -583,7 +583,7 @@ Rails.application.routes.draw do
           resources :card_grants, only: [:index, :create]
           resources :transactions, only: [:show, :update] do
             resources :receipts, only: [:index]
-            resources :comments, only: [:index]
+            resources :comments, only: [:index, :create]
 
             member do
               get "memo_suggestions"
