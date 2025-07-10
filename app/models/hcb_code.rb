e2e7f1618580e857964adea7ba44e91e64ae1d62
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-07-09 23:02:14 -0400

    Small grant and HCB code fixes (#10840)
    - Fix amount cents on card grant transactions
    - Change link to stripe card to link to grant card (fixes bug for
    admins)
    - Add button to get to grant from stripe card
    - Check for tags before rendering tags
    - Remove amount from disbursement body

--- app/models/hcb_code.rb
+++ app/models/hcb_code.rb
@@ -136,6 +136,9 @@ class HcbCode < ApplicationRecord
   end
 
   def amount_cents_by_event(event)
+    return stripe_atm_fee ? amount_cents.abs - stripe_atm_fee : amount_cents.abs if stripe_card?
+    return invoice.item_amount if invoice?
+
     if canonical_transactions.any?
       return canonical_transactions
              .includes(:canonical_event_mapping)
