56127e52af1e91618ce4853f9e978959491eaea2
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-07-11 11:16:20 -0400

    Small changes to memos (#10908)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Standardize dates / format of memos.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    The order on `outgoing_fee_reimbursement_memo` may be wrong.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/models/hcb_code/memo.rb
+++ app/models/hcb_code/memo.rb
@@ -61,7 +61,7 @@ class HcbCode
 
       def bank_fee_memo
         if bank_fee.amount_cents.negative? && bank_fee.fee_revenue.present?
-          return "Fiscal sponsorship for #{bank_fee.fee_revenue.start.strftime("%-m/%-d")} to #{bank_fee.fee_revenue.end.strftime("%-m/%-d")}"
+          return "Fiscal sponsorship fee for #{bank_fee.fee_revenue.start.strftime("%-m/%-d")} to #{bank_fee.fee_revenue.end.strftime("%-m/%-d")}"
         elsif bank_fee.amount_cents.negative?
           return "Fiscal sponsorship"
         else
@@ -86,11 +86,11 @@ class HcbCode
       end
 
       def fee_revenue_memo
-        "Fee revenue from #{fee_revenue.start.strftime("%b %e")} to #{fee_revenue.end.strftime("%b %e")}"
+        "Fee revenue for #{fee_revenue.start.strftime("%-m/%-d")} to #{fee_revenue.end.strftime("%-m/%-d")}"
       end
 
       def outgoing_fee_reimbursement_memo
-        "🗂️ Stripe fee reimbursements for week of #{ct.date.beginning_of_week.strftime("%-m/%-d")}"
+        "🗂️ Stripe fee reimbursements for #{ct.date.beginning_of_week.strftime("%-m/%-d")} to #{ct2.date.beginning_of_week.strftime("%-m/%-d")}"
       end
 
       def reimbursement_payout_holding_memo
