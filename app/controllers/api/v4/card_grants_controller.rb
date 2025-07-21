537b52f636cf76da9b5365264d7d4f70427adb0d
nora <163450896+24c02@users.noreply.github.com>
2025-07-21 14:12:24 -0400

    [Card Grants/V4] permit preauth `instructions` parameter on card grants (#11026)
    let catboy maid tell our users what to do.
    LET CATBOY MAID TELL OUR USERS WHAT TO DO!

--- app/controllers/api/v4/card_grants_controller.rb
+++ app/controllers/api/v4/card_grants_controller.rb
@@ -16,7 +16,7 @@ module Api
       def create
         @event = Event.find_by_public_id(params[:event_id]) || Event.friendly.find(params[:event_id])
 
-        @card_grant = @event.card_grants.build(params.permit(:amount_cents, :email, :merchant_lock, :category_lock, :keyword_lock, :purpose, :one_time_use, :pre_authorization_required).merge(sent_by: current_user))
+        @card_grant = @event.card_grants.build(params.permit(:amount_cents, :email, :merchant_lock, :category_lock, :keyword_lock, :purpose, :one_time_use, :pre_authorization_required, :instructions).merge(sent_by: current_user))
 
         authorize @card_grant
 
@@ -45,7 +45,7 @@ module Api
 
         authorize @card_grant
 
-        @card_grant.update!(params.permit(:merchant_lock, :category_lock, :keyword_lock, :purpose, :one_time_use))
+        @card_grant.update!(params.permit(:merchant_lock, :category_lock, :keyword_lock, :purpose, :one_time_use, :instructions))
 
         render :show
       end
