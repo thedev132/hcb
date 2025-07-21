407fde5636f81e8457f8fd3933e7f02d32b04d4b
Samuel Fernandez <me@sfernandez.dev>
2025-07-21 04:31:06 -0400

    Improve frontend AppSignal logging (#10999)
    ## Summary of the problem
    <!-- Why these changes are being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Frontend errors can be hard to debug!
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Adds the path (to include the page the user was on when an error
    happens) and console (to include console log breadcrumbs) plugins to
    appsignal.
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/javascript/appsignal.js
+++ app/javascript/appsignal.js
@@ -1,7 +1,12 @@
 /* global APPSIGNAL_FRONTEND */
 
 import Appsignal from '@appsignal/javascript'
+import { plugin as pathPlugin } from '@appsignal/plugin-path-decorator'
+import { plugin as consolePlugin } from '@appsignal/plugin-breadcrumbs-console'
 
 export const appsignal = new Appsignal({
   key: APPSIGNAL_FRONTEND,
 })
+
+appsignal.use(pathPlugin())
+appsignal.use(consolePlugin())
