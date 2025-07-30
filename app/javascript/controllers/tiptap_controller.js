95e1da2453c864bb1bd4129e9b47c86c44290523
Samuel Fernandez <me@sfernandez.dev>
2025-07-30 14:21:02 -0400

    [Announcement] Confirm publishing an announcement from the create form (#11150)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Currently we have `turbo_confirm` on publishing draft announcements, but
    there's no confirmation when publishing an announcement straight from
    the creation form!
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Adds a confirmation dialog to the creation form when publishing an
    announcement for the first time.
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/javascript/controllers/tiptap_controller.js
+++ app/javascript/controllers/tiptap_controller.js
@@ -18,6 +18,8 @@ export default class extends Controller {
     content: String,
     announcementId: Number,
     autosave: Boolean,
+    followers: Number,
+    published: Boolean,
   }
 
   editor = null
@@ -82,6 +84,19 @@ export default class extends Controller {
   }
 
   submit(autosave) {
+    if (autosave !== true && !this.publishedValue) {
+      const data = new FormData(this.formTarget)
+      const draft = data.get('announcement[draft]')
+
+      if (draft === 'false') {
+        let confirmed = confirm(
+          `Are you sure you would like to publish this announcement and notify ${this.followersValue} follower${this.followersValue === 1 ? '' : 's'}?`
+        )
+
+        if (!confirmed) return
+      }
+    }
+
     this.autosaveInputTarget.value = autosave === true ? 'true' : 'false'
     this.contentInputTarget.value = JSON.stringify(this.editor.getJSON())
     this.formTarget.requestSubmit()
