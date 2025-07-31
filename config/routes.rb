63bb8a9456cc6d87358dc5edd1120b76c844efcf
David Cornu <davidc@hackclub.com>
2025-07-31 15:02:00 -0400

    [DX] Make `/schema` accessible to auditors (#11167)
    ## Summary of the problem
    
    Follow up to #11045, as requested by @Luke-Oldenburg
    (https://hackclub.slack.com/archives/C047Y01MHJQ/p1753985372569149?thread_ts=1753734366.653599&cid=C047Y01MHJQ)
    
    ## Describe your changes
    
    Moves the endpoint from `AdminConstraint` to `AuditorConstraint`

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
     mount Audits1984::Engine => "/console"
     mount Sidekiq::Web => "/sidekiq"
     mount Flipper::UI.app(Flipper), at: "flipper", as: "flipper"
-    mount SchemaEndpoint.instance => "/schema"
   end
   constraints AuditorConstraint do
     mount Blazer::Engine, at: "blazer"
+    mount SchemaEndpoint.instance => "/schema"
   end
   get "/sidekiq", to: redirect("users/auth") # fallback if adminconstraint fails, meaning user is not signed in
   if Rails.env.development?
