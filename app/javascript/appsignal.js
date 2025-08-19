c0fd280fcc62c605f782644856d575719fcc9151
Sam Poder <sampoder@berkeley.edu>
2025-08-18 22:08:16 -0400

    Ignore the error thrown when Webauth permission is declined (#11363)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/javascript/appsignal.js
+++ app/javascript/appsignal.js
@@ -6,6 +6,9 @@ import { plugin as consolePlugin } from '@appsignal/plugin-breadcrumbs-console'
 
 export const appsignal = new Appsignal({
   key: APPSIGNAL_FRONTEND,
+  ignoreErrors: [
+    /The request is not allowed by the user agent or the platform in the current context, possibly because the user denied permission\./,
+  ],
 })
 
 appsignal.use(pathPlugin())
