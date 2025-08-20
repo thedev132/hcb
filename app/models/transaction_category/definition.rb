5885f5c0e8600398843d60e1a46936db7b389b43
David Cornu <davidc@hackclub.com>
2025-08-20 12:49:21 -0400

    [Categories] Import MCC mappings (#11371)
    ## Summary of the problem
    
    Follow-up to #11298 
    
    ## Describe your changes
    
    - Imports stripe merchant categories mappings from
    https://docs.google.com/spreadsheets/d/1I7TSqE6OxplCvS2YbJCBS5gNgY0bHIFQy4gVBWyqpWY/edit?gid=341089138#gid=341089138
    - Adds test to make sure `db/data/transaction_categories.json` is valid

--- app/models/transaction_category/definition.rb
+++ app/models/transaction_category/definition.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TransactionCategory
-  Definition = Struct.new(:slug, :label, keyword_init: true) do
+  Definition = Struct.new(:slug, :label, :stripe_merchant_categories, keyword_init: true) do
     def self.load_all
       path = Rails.root.join("db/data/transaction_categories.json")
       JSON.parse(File.read(path)).to_h do |slug, attributes|
@@ -10,6 +10,7 @@ class TransactionCategory
           Definition.new(
             slug:,
             label: attributes.fetch("label"),
+            stripe_merchant_categories: attributes.fetch("stripe_merchant_categories", []),
           ).freeze
         ]
       end.freeze
---
+++ spec/models/transaction_category/definition_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TransactionCategory::Definition do
+  describe "db/data/transaction_categories.json" do
+    it "is correctly structured" do
+      contents = JSON.parse(File.read(Rails.root.join("db/data/transaction_categories.json")))
+
+      expect(contents).to(be_a(Hash), "is not a JSON object")
+      expect(contents.keys).to(eq(contents.keys.sort), "top-level keys are not sorted")
+
+      # Keep a running list of all the stripe categories we see so we can detect
+      # duplicates
+      all_stripe_categories = Set.new
+
+      contents.each do |slug, attributes|
+        expect(attributes).to(be_a(Hash), "#{slug.inspect} property is not an object")
+
+        attributes.each_key do |key|
+          expect(["label", "stripe_merchant_categories"]).to(
+            include(key),
+            "#{slug.inspect} has unsupported key #{key.inspect}"
+          )
+        end
+
+        expect(attributes["label"]).to(be_a(String).and(be_present), "#{slug.inspect} is missing a label")
+
+        stripe_categories = attributes["stripe_merchant_categories"]
+        next if stripe_categories.nil?
+
+        expect(stripe_categories).to(
+          be_a(Array).and(be_present),
+          "stripe_categories for #{slug.inspect} is not a populated array"
+        )
+        expect(stripe_categories).to(
+          all(be_a(String).and(be_present)),
+          "#{slug.inspect} must only contain strings in stripe_categories"
+        )
+        expect(stripe_categories).to(
+          eq(stripe_categories.sort.uniq),
+          "#{slug.inspect} must have sorted and unique stripe categories"
+        )
+
+        stripe_categories.each do |stripe_category|
+          expect(YellowPages::Category.categories_by_key[stripe_category]).to(
+            be_present,
+            "stripe category #{stripe_category.inspect} for #{slug.inspect} must exist in YellowPages"
+          )
+
+          expect(all_stripe_categories.add?(stripe_category)).to(
+            be_present,
+            "stripe category #{stripe_category.inspect} for #{slug.inspect} is included in another category"
+          )
+        end
+      end
+    end
+  end
+end
