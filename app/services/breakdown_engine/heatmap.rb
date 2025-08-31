d08b33a8bc625d588a75a56baeac1a7bad4322fc
Samuel Fernandez <me@sfernandez.dev>
2025-08-31 02:38:15 -0400

    Improve transaction heatmap (#10544)

--- app/services/breakdown_engine/heatmap.rb
+++ app/services/breakdown_engine/heatmap.rb
@@ -8,34 +8,30 @@ module BreakdownEngine
 
     def run
       heatmap = {}
-      transactions_count = 0
 
       start = 1.year.ago - (1.year.ago.to_date.wday % 7).day
 
       range = start.to_date...6.days.from_now.to_date
 
       range.each do |date|
-        heatmap[date.to_s] = { negative: 0, positive: 0 }
+        heatmap[date.to_s] = 0
       end
 
-      transactions = TransactionGroupingEngine::Transaction::All.new(event_id: @event.id).run
+      settled_transactions = TransactionGroupingEngine::Transaction::All.new(event_id: @event.id).run
+      pending_transactions = PendingTransactionEngine::PendingTransaction::All.new(event_id: @event.id).run
 
-      transactions.reverse.select { |transaction| heatmap.key?(transaction.date) }.each do |transaction|
-        if transaction.amount > 0
-          heatmap[transaction.date][:positive] += transaction.amount_cents
-        elsif transaction.amount < 0
-          heatmap[transaction.date][:negative] += transaction.amount_cents
-        end
-        transactions_count += 1
+      all_transactions = settled_transactions.concat(pending_transactions)
+      transactions_in_range = all_transactions.select { |transaction| heatmap.key?(transaction.date.to_s) }
+
+      transactions_in_range.each do |transaction|
+        heatmap[transaction.date.to_s] += transaction.amount_cents
       end
-
-      last_date = heatmap.keys.max.to_date
 
       {
         heatmap:,
-        transactions_count:,
-        maximum_positive_change: heatmap.values.map { |change| change[:positive] }.max || 0,
-        maximum_negative_change: heatmap.values.map { |change| change[:negative] }.min || 0
+        transactions_count: transactions_in_range.size,
+        maximum_positive_change: heatmap.values.max || 0,
+        maximum_negative_change: heatmap.values.min || 0
       }
     end
 
