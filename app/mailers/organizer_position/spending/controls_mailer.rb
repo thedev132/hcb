d2e917d6bc71303e2eb1fcf3eba3ef1d731132df
Samuel Fernandez <me@sfernandez.dev>
2025-09-01 00:07:48 -0400

    Notify organizers when they have a new spending allowance (#11514)

--- app/mailers/organizer_position/spending/controls_mailer.rb
+++ app/mailers/organizer_position/spending/controls_mailer.rb
@@ -9,6 +9,13 @@ class OrganizerPosition
         mail to: @control.organizer_position.user.email_address_with_name, subject: "Your spending balance on #{@control.organizer_position.event.name} is getting low"
       end
 
+      def new_allowance
+        @allowance = params[:allowance]
+        @control = @allowance.control
+
+        mail to: @control.organizer_position.user.email_address_with_name, subject: "Your spending balance on #{@control.organizer_position.event.name} has #{@allowance.amount_cents > 0 ? "increased" : "decreased"}"
+      end
+
     end
   end
 
