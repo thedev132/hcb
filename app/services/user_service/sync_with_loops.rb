8e29573ffaccf27dc4be5e8dede2c4e684b5dbec
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-09-03 17:32:47 -0700

    Fix usage of Rails.error.report (#11558)
    oops, i did it again
    
    ('it', in this case, would be me using the wrong argument type for the
    `Rails.error.report` call)

--- app/services/user_service/sync_with_loops.rb
+++ app/services/user_service/sync_with_loops.rb
@@ -69,7 +69,7 @@ module UserService
 
         JSON[resp.body][0]
       rescue => e
-        Rails.error.report("Received exception #{e.full_message} while attempting to get contact details for email #{@user.email} from Loops.", handled: false, severity: :error, context: "service")
+        Rails.error.report(Exception.new("Received exception #{e.full_message} while attempting to get contact details for email #{@user.email} from Loops."), handled: false, severity: :error, context: "service")
         raise e
       end
     end
