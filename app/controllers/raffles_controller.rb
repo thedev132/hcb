d31de1a7484bd15c0d6eb28c9e2861bf44963bda
Samuel Fernandez <me@sfernandez.dev>
2025-09-12 18:17:04 -0400

    Fix refresh canceling raffle submission (#11661)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    The page was immediately refreshing when the raffle was submitted,
    causing the request to not send.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Removed the event handler on the frontend to refresh the page and
    instead used Turbo streams to respond with a refresh stream.

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -9,12 +9,23 @@ class RafflesController < ApplicationController
   def create
     if Raffle.where(user: current_user, program: params[:program]).any?
       flash[:error] = "You are already entered in this raffle."
-      redirect_to root_path
+
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.refresh(request_id: nil) }
+        format.html do
+          redirect_to root_path
+        end
+      end
     else
       raffle = Raffle.new(user: current_user, program: params[:program])
       if raffle.save
         flash[:success] = "Raffle joined!"
-        redirect_to root_path
+        respond_to do |format|
+          format.turbo_stream { render turbo_stream: turbo_stream.refresh(request_id: nil) }
+          format.html do
+            redirect_to root_path
+          end
+        end
       else
         flash[:error] = raffle.errors.full_messages.to_sentence
         redirect_to new_raffle_path
